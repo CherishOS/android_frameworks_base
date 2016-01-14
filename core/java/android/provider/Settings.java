5dc60a091f25c8af7d597e88f04a1ff92bed40d6
beanstown106 <nbenis106@gmail.com>
2016-01-14 05:56:50 -0500

    Fingerprint authentication vibration [1/2]
    had a few requests for this, as most devices only vibrate when fingerprint authentication is unsucesful so made it configurable
    
    Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6773,6 +6773,13 @@ public final class Settings {
         public static final String QS_FOOTER_DATA_USAGE = "qs_footer_data_usage";
 
         /**
+         * whether to enable or disable vibration on succesful fingerprint auth
+         *
+         * @hide
+         */
+        public static final String FINGERPRINT_SUCCESS_VIB = "fingerprint_success_vib";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
