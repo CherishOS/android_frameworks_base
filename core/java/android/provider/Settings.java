fda1651a26e6ab56f2f2a011c1d64c1bcc9e2dcd
LorDClockaN <davor@losinj.com>
2016-01-21 23:05:00 +0200

    FWB: Power/reboot menu transparency (1/2)
    SDB: adapt for A12 as GlobalActionDialog got a sibling, GlobalActionsDialogLite.
    
    Change-Id: Ib703c2574aca9bc3c534a155d165f07b603c0535
    Signed-off-by: SuperDroidBond <superdroidbond@yahoo.com>
    Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6399,6 +6399,18 @@ public final class Settings {
         public static final String ANIM_TILE_INTERPOLATOR = "anim_tile_interpolator";
 
         /**
+         * Transparent power menu and dialogs
+         * @hide
+         */
+        public static final String TRANSPARENT_POWER_MENU = "transparent_power_menu";
+
+        /**
+         * Dim amount around power/reboot menu dialogs
+         * @hide
+         */
+        public static final String TRANSPARENT_POWER_DIALOG_DIM = "transparent_power_dialog_dim";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
@@ -6542,6 +6554,8 @@ public final class Settings {
             PRIVATE_SETTINGS.add(HIGH_TOUCH_SENSITIVITY_ENABLE);
             PRIVATE_SETTINGS.add(STATUSBAR_CLOCK_CHIP);
             PRIVATE_SETTINGS.add(FORCE_FULLSCREEN_CUTOUT_APPS);
+            PRIVATE_SETTINGS.add(TRANSPARENT_POWER_MENU);
+            PRIVATE_SETTINGS.add(TRANSPARENT_POWER_DIALOG_DIM);
         }
 
         /**
