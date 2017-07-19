2014208833ba7f52a4ee65f655ce71c6db442aa4
SagarMakhar <sagarmakhar@gmail.com>
2017-07-19 23:46:45 +0530

    SystemUI: Introduce dynamic VoLTE & VoWiFi icons
    @/neobuddy89: Clean up unrequired derps. Adapted for A13 QPR2.
    
    Co-authored-by: Adithya R <gh0strider.2k18.reborn@gmail.com>
    Co-authored-by: Jyotiraditya Panda <jyotiraditya@aospa.co>
    Co-authored-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: SuperDroidBond <superdroidbond@yahoo.com>
    Signed-off-by: RakeshBatra <raakesh.batra@rediffmail.com>
    Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBarIconHolder.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBarIconHolder.java
@@ -24,6 +24,7 @@ import android.os.UserHandle;
 
 import com.android.internal.statusbar.StatusBarIcon;
 import com.android.systemui.statusbar.phone.PhoneStatusBarPolicy.BluetoothIconState;
+import com.android.systemui.statusbar.connectivity.ImsIconState;
 import com.android.systemui.statusbar.phone.StatusBarSignalPolicy.CallIndicatorIconState;
 import com.android.systemui.statusbar.pipeline.mobile.ui.viewmodel.MobileIconViewModel;
 
@@ -60,20 +61,25 @@ public class StatusBarIconHolder {
     @Deprecated
     public static final int TYPE_WIFI_NEW = 4;
 
-    public static final int TYPE_BLUETOOTH = 5;
+    public static final int TYPE_IMS = 5;
+
+    public static final int TYPE_BLUETOOTH = 6;
 
     @IntDef({
             TYPE_ICON,
             TYPE_MOBILE_NEW,
             TYPE_WIFI_NEW,
-            TYPE_BLUETOOTH
+            TYPE_BLUETOOTH,
+            TYPE_IMS
     })
     @Retention(RetentionPolicy.SOURCE)
     @interface IconType {}
 
-    public static final int TYPE_NETWORK_TRAFFIC = 6;
+    public static final int TYPE_NETWORK_TRAFFIC = 7;
 
     private StatusBarIcon mIcon;
+    private ImsIconState mImsState;
+
     private BluetoothIconState mBluetoothState;
 
     private @IconType int mType = TYPE_ICON;
@@ -103,6 +109,14 @@ public class StatusBarIconHolder {
     public static StatusBarIconHolder forNewWifiIcon() {
         StatusBarIconHolder holder = new StatusBarIconHolder();
         holder.mType = TYPE_WIFI_NEW;
+        return holder;
+    }
+
+    /** */
+    public static StatusBarIconHolder fromImsIconState(ImsIconState state) {
+        StatusBarIconHolder holder = new StatusBarIconHolder();
+        holder.mImsState = state;
+        holder.mType = TYPE_IMS;
         return holder;
     }
 
@@ -170,6 +184,14 @@ public class StatusBarIconHolder {
         mBluetoothState = state;
     }
 
+    public ImsIconState getImsState() {
+        return mImsState;
+    }
+
+    public void setImsState(ImsIconState state) {
+        mImsState = state;
+    }
+
     public boolean isVisible() {
         switch (mType) {
             case TYPE_ICON:
@@ -179,6 +201,8 @@ public class StatusBarIconHolder {
                 // The new pipeline controls visibilities via the view model and view binder, so
                 // this is effectively an unused return value.
                 return true;
+            case TYPE_IMS:
+                return mImsState.visible;
             case TYPE_BLUETOOTH:
                 return mBluetoothState.visible;
             default:
@@ -203,7 +227,9 @@ public class StatusBarIconHolder {
                 // The new pipeline controls visibilities via the view model and view binder, so
                 // ignore setVisible.
                 break;
-
+            case TYPE_IMS:
+                mImsState.visible = visible;
+                break;
             case TYPE_BLUETOOTH:
                 mBluetoothState.visible = visible;
                 break;
