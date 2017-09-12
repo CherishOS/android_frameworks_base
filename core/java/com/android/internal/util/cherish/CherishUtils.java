be061bfddcf2b475a75de076007bdf52806c37d1
bigrushdog <randall.rushing@gmail.com>
2017-09-12 14:14:35 +0800

    Add api to take screenshots abc: extracted from DUI
    Change-Id: I2ac0bf9cf6641a475ea9a3c9b714550bc55ee13b
    Signed-off-by: DennySPB <dennyspb@gmail.com>
    Signed-off-by: DennySPb <dennyspb@gmail.com>
    Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>

--- core/java/com/android/internal/util/cherish/CherishUtils.java
+++ core/java/com/android/internal/util/cherish/CherishUtils.java
@@ -48,6 +48,9 @@ import com.android.internal.statusbar.IStatusBarService;
 
 public class CherishUtils {
     private static OverlayManager mOverlayService;
+	
+	public static final String INTENT_SCREENSHOT = "action_handler_screenshot";
+    public static final String INTENT_REGION_SCREENSHOT = "action_handler_region_screenshot";
 
     public static void switchScreenOff(Context ctx) {
         PowerManager pm = (PowerManager) ctx.getSystemService(Context.POWER_SERVICE);
@@ -100,6 +103,15 @@ public class CherishUtils {
                         InputManager.INJECT_INPUT_EVENT_MODE_ASYNC);
             }
         }, 20);
+    }
+	
+	public static void takeScreenshot(boolean full) {
+        IWindowManager wm = WindowManagerGlobal.getWindowManagerService();
+        try {
+            wm.sendCustomAction(new Intent(full? INTENT_SCREENSHOT : INTENT_REGION_SCREENSHOT));
+        } catch (RemoteException e) {
+            e.printStackTrace();
+        }
     }
 
     private static final class FireActions {
