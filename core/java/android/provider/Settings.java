eb820a8d9c888b5246bf49debeb303d879b0ed04
bigrushdog <randall.rushing@gmail.com>
2018-01-12 12:35:28 -0800

    Screen off animations [1/2]
    Fade (default), CRT, and Scale
    
    Credit and respect to @xplodwild for paving the way
    back in the KitKat days!
    
    History of the ElectronBeam class can be found here
    https://github.com/DirtyUnicorns/android_frameworks_base/commits/kitkat/services/java/com/android/server/power/ElectronBeam.java
    
    Adapted to 10 by @eyosen
    
    Change-Id: I58d269d44c901a8c0471807e3cf05c2054205d28
    Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5191,6 +5191,12 @@ public final class Settings {
         public static final String HEADS_UP_BLACKLIST_VALUES = "heads_up_blacklist_values";
 
         /**
+         * Defines the screen-off animation to display
+         * @hide
+         */
+        public static final String SCREEN_OFF_ANIMATION = "screen_off_animation";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
@@ -5261,6 +5267,7 @@ public final class Settings {
             PUBLIC_SETTINGS.add(VIBRATE_WHEN_RINGING);
             PUBLIC_SETTINGS.add(HEADS_UP_STOPLIST_VALUES);
             PUBLIC_SETTINGS.add(HEADS_UP_BLACKLIST_VALUES);
+            PUBLIC_SETTINGS.add(SCREEN_OFF_ANIMATION);
         }
 
         /**
@@ -5346,6 +5353,7 @@ public final class Settings {
             PRIVATE_SETTINGS.add(ANIM_TILE_INTERPOLATOR);
             PRIVATE_SETTINGS.add(HEADS_UP_STOPLIST_VALUES);
             PRIVATE_SETTINGS.add(HEADS_UP_BLACKLIST_VALUES);
+            PRIVATE_SETTINGS.add(SCREEN_OFF_ANIMATION);
         }
 
         /**
