e67e7935cc7da275b258b3472806d503269c8931
Pranav Vashi <neobuddy89@gmail.com>
2018-09-29 13:05:52 +0530

    Use FORCE_SHOW_NAVBAR to actually toggle navbar
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/CentralSurfacesImpl.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/CentralSurfacesImpl.java
@@ -957,17 +957,6 @@ public class CentralSurfacesImpl implements
         mTunerService.addTunable(this, QS_TRANSPARENCY);
         mTunerService.addTunable(this, FORCE_SHOW_NAVBAR);
 
-        mNeedsNavigationBar = mContext.getResources().getBoolean(
-                com.android.internal.R.bool.config_showNavigationBar);
-        // Allow a system property to override this. Used by the emulator.
-        // See also hasNavigationBar().
-        String navBarOverride = SystemProperties.get("qemu.hw.mainkeys");
-        if ("1".equals(navBarOverride)) {
-            mNeedsNavigationBar = false;
-        } else if ("0".equals(navBarOverride)) {
-            mNeedsNavigationBar = true;
-        }
-
         mWindowManager = (WindowManager) mContext.getSystemService(Context.WINDOW_SERVICE);
 
         mDisplay = mContext.getDisplay();
@@ -4222,10 +4211,10 @@ public class CentralSurfacesImpl implements
             case FORCE_SHOW_NAVBAR:
                 if (mDisplayId != Display.DEFAULT_DISPLAY || mWindowManagerService == null)
                     return;
-                boolean forcedVisibility = mNeedsNavigationBar ||
-                        TunerService.parseIntegerSwitch(newValue, false);
+                boolean mNavbarVisible =
+                        TunerService.parseIntegerSwitch(newValue, ActionUtils.hasNavbarByDefault(mContext));
                 boolean hasNavbar = getNavigationBarView() != null;
-                if (forcedVisibility) {
+                if (mNavbarVisible) {
                     if (!hasNavbar) {
                         mNavigationBarController.onDisplayReady(mDisplayId);
                     }
