90a2f7fc77608b6d175225e529e8323f14ce6241
LorDClockaN <davor@losinj.com>
2018-11-21 15:49:47 +0100

    Volume dialog timeout [1/2]
    @/neobuddy89:
    * Rewrite to avoid called getInt everytime changing volume! Really?
    * Removed validators - handle range in the settings.
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6434,6 +6434,12 @@ public final class Settings {
         public static final String SCREENSHOT_SHUTTER_SOUND = "screenshot_shutter_sound";
 
         /**
+         * Volume dialog timeout. Value in seconds
+         * @hide
+         */
+        public static final String VOLUME_DIALOG_TIMEOUT = "volume_dialog_timeout";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
