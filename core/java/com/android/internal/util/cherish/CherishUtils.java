08db3d1cecbd9f6954b546a481d03fa1d1d1c259
Alex Cruz <du.alexcruz@gmail.com>
2019-01-19 17:30:24 -0600

    CherishUtils: Add method to determine battery temp
    Change-Id: Id0b8addc460afae030eac0a2bf064fb21aa2c4dc
    Signed-off-by: Saikiran <bjsaikiran@gmail.com>
    Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>

--- core/java/com/android/internal/util/cherish/CherishUtils.java
+++ core/java/com/android/internal/util/cherish/CherishUtils.java
@@ -20,6 +20,7 @@ import android.content.Intent;
 import android.hardware.input.InputManager;
 import android.os.Looper;
 import android.content.Context;
+import android.content.IntentFilter;
 import android.content.pm.PackageManager;
 import android.os.PowerManager;
 import java.util.List;
@@ -29,6 +30,7 @@ import android.os.SystemClock;
 import android.os.SystemProperties;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
+import android.os.BatteryManager;
 import android.text.format.Time;
 import android.content.res.Resources;
 import android.view.IWindowManager;
@@ -104,6 +106,19 @@ public class CherishUtils {
 
     public static void toggleCameraFlash() {
         FireActions.toggleCameraFlash();
+    }
+	
+	public static String batteryTemperature(Context context, Boolean ForC) {
+        Intent intent = context.registerReceiver(null, new IntentFilter(
+                Intent.ACTION_BATTERY_CHANGED));
+        float  temp = ((float) (intent != null ? intent.getIntExtra(
+                BatteryManager.EXTRA_TEMPERATURE, 0) : 0)) / 10;
+        // Round up to nearest number
+        int c = (int) ((temp) + 0.5f);
+        float n = temp + 0.5f;
+        // Use boolean to determine celsius or fahrenheit
+        return String.valueOf((n - c) % 2 == 0 ? (int) temp :
+                ForC ? c * 9/5 + 32 + "°F" :c + "°C");
     }
 
     public static void sendKeycode(int keycode) {
