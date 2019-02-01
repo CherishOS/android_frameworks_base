84afeb420161875aa13f9126cfa4130ba253ee45
Alex <du.alexcruz@gmail.com>
2019-02-01 16:47:41 -0600

    CherishUtils: Introduce method to determine countries that use Fahrenheit
    This one is a weird one because up until a few days ago, I didn't think
    I would need. Basically what this does is check the device's MCC and bounce
    it off an array of MCC's that belong to countries that use fahrenheit.
    
    - If no MCC is found (whether is a WiFi only tablet or the user has their phone
    in airplane mode), the method returns false (Celsius). Celsius is used by 99 %
    of all people so is not a bad option.
    
    Inspired by Henrique Silva <jhenrique09.mcz@hotmail.com> and his commit below
    https://gerrit.pixelexperience.org/#/c/frameworks_base/+/1606/
    
    We're going to end up using this for two things, our battery temp thingy in
    Settings and also for what Henrique used it for, the keyguard weather default.
    
    Change-Id: I49c7a397c794e7852ab0be4bd3182710fa3b19b0
    Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>

--- core/java/com/android/internal/util/cherish/CherishUtils.java
+++ core/java/com/android/internal/util/cherish/CherishUtils.java
@@ -28,6 +28,8 @@ import android.os.RemoteException;
 import android.os.ServiceManager;
 import android.os.SystemClock;
 import android.os.SystemProperties;
+import android.telephony.TelephonyManager;
+import android.text.TextUtils;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.BatteryManager;
@@ -48,6 +50,8 @@ import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager.NameNotFoundException;
 
 import com.android.internal.statusbar.IStatusBarService;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Locale;
 public class CherishUtils {
     private static OverlayManager mOverlayService;
@@ -118,7 +122,23 @@ public class CherishUtils {
         float n = temp + 0.5f;
         // Use boolean to determine celsius or fahrenheit
         return String.valueOf((n - c) % 2 == 0 ? (int) temp :
-                ForC ? c * 9/5 + 32 + "°F" :c + "°C");
+                ForC ? c * 9/5 + 32:c);
+    }
+
+    // Method to detect countries that use Fahrenheit
+    public static boolean mccCheck(Context context) {
+        // MCC's belonging to countries that use Fahrenheit
+        String[] mcc = {"364", "552", "702", "346", "550", "376", "330",
+                "310", "311", "312", "551"};
+
+        TelephonyManager tel = (TelephonyManager) context.getSystemService(
+                Context.TELEPHONY_SERVICE);
+        String networkOperator = tel.getNetworkOperator();
+
+        // Check the array to determine celsius or fahrenheit.
+        // Default to celsius if can't access MCC
+        return !TextUtils.isEmpty(networkOperator) && Arrays.asList(mcc).contains(
+                networkOperator.substring(0, /*Filter only 3 digits*/ 3));
     }
 
     public static void sendKeycode(int keycode) {
