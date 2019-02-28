2a967685da51ebc20b2b42d6d9062702b92a7c30
ezio84 <brabus84@gmail.com>
2019-02-27 21:13:01 -0600

    ActionUtils: Reverse actions for QS and notification panel
    This just makes sense, so lets do it!
    
    - Update method names in PWM
    
    hungphan2001: Adpart to A13 QPR2
    
    Change-Id: Ic08bee12c90dc55265dea3c3972f4087b93d2826
    Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/CommandQueue.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/CommandQueue.java
@@ -172,6 +172,7 @@ public class CommandQueue extends IStatusBar.Stub implements
     private static final int MSG_TOGGLE_CAMERA_FLASH  = 73 << MSG_SHIFT;
     private static final int MSG_SCREEN_PINNING_STATE_CHANGED = 74 << MSG_SHIFT;
     private static final int MSG_LEFT_IN_LANDSCAPE_STATE_CHANGED = 75 << MSG_SHIFT;
+    private static final int MSG_TOGGLE_SETTINGS_PANEL = 76 << MSG_SHIFT;
 
     public static final int FLAG_EXCLUDE_NONE = 0;
     public static final int FLAG_EXCLUDE_SEARCH_PANEL = 1 << 0;
@@ -218,6 +219,7 @@ public class CommandQueue extends IStatusBar.Stub implements
         default void animateExpandNotificationsPanel() { }
         default void animateCollapsePanels(int flags, boolean force) { }
         default void togglePanel() { }
+        default void toggleSettingsPanel() { }
         default void animateExpandSettingsPanel(String obj) { }
 
         /**
@@ -673,6 +675,13 @@ public class CommandQueue extends IStatusBar.Stub implements
         synchronized (mLock) {
             mHandler.removeMessages(MSG_TOGGLE_PANEL);
             mHandler.obtainMessage(MSG_TOGGLE_PANEL, 0, 0).sendToTarget();
+        }
+    }
+
+    public void toggleSettingsPanel() {
+        synchronized (mLock) {
+            mHandler.removeMessages(MSG_TOGGLE_SETTINGS_PANEL);
+            mHandler.obtainMessage(MSG_TOGGLE_SETTINGS_PANEL, 0, 0).sendToTarget();
         }
     }
 
@@ -1423,6 +1432,11 @@ public class CommandQueue extends IStatusBar.Stub implements
                 case MSG_TOGGLE_PANEL:
                     for (int i = 0; i < mCallbacks.size(); i++) {
                         mCallbacks.get(i).togglePanel();
+                    }
+                    break;
+                case MSG_TOGGLE_SETTINGS_PANEL:
+                    for (int i = 0; i < mCallbacks.size(); i++) {
+                        mCallbacks.get(i).toggleSettingsPanel();
                     }
                     break;
                 case MSG_EXPAND_SETTINGS:
--- packages/SystemUI/src/com/android/systemui/statusbar/phone/CentralSurfacesImpl.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/CentralSurfacesImpl.java
@@ -2084,6 +2084,14 @@ public class CentralSurfacesImpl implements CoreStartable, CentralSurfaces,
         mMessageRouter.sendMessage(MSG_OPEN_SETTINGS_PANEL);
     }
 
+    public void toggleSettingsPanel() {
+        if (mPanelExpanded) {
+            mShadeController.animateCollapseShade();
+        } else {
+            animateExpandSettingsPanel(null);
+        }
+    }
+
     @Override
     public boolean isPanelExpanded() {
         return mPanelExpanded;
