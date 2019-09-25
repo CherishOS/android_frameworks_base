d6c41fbf42c91850674b3b975ef92d72e082cdc8
maxwen <max.weninger@gmail.com>
2019-09-25 16:19:49 +0200

    base: navigation mode settings [1/2]
    to configure the height of the back gesture
    
    Change-Id: I6ebe0039e250f3b8f0b91b232df5941b58026499
    Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5710,6 +5710,9 @@ public final class Settings {
          */
         public static final String FULLSCREEN_GESTURES = "fullscreen_gestures";
 
+        /** @hide */
+        public static final String BACK_GESTURE_HEIGHT = "back_gesture_height";
+
         /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
