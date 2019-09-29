46bf1ec94b3162a214859c37637976a9c0c446db
paphonb <paphonb@gmail.com>
2019-09-29 19:35:36 +0700

    base: Add new font overlays
    Change-Id: I9e24321cde5cdf569e149616e18920ee922ab0c3
    Signed-off-by: Anirudh Gupta <anirudhgupta109@gmail.com>
    Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>

--- packages/overlays/Android.mk
+++ packages/overlays/Android.mk
@@ -33,6 +33,10 @@ LOCAL_REQUIRED_MODULES := \
         FontRosemarySourceOverlay \
         FontSonySketchSourceOverlay \
         FontSurferSourceOverlay \
+	FontArbutusSourceOverlay \
+	FontArvoLatoOverlay \
+	FontNotoSerifSourceOverlay \
+	FontRubikRubikOverlay \
 	IconPackCircularAndroidOverlay \
 	IconPackCircularLauncherOverlay \
 	IconPackCircularSettingsOverlay \
