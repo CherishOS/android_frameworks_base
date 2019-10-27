60e99d81dec97b5c8dbfd1e37429d803ebcfe3b2
neobuddy89 <neobuddy89@gmail.com>
2019-10-27 17:40:56 +0100

    Add battery styles and customizations for Android 11 [1/2]
    neobuddy89:
    * Handle QS battery modes
    * Convert ThemedBatteryDrawable from kotlin to java with extra goodies of setting percentage.
    * Treat ThemedBatteryDrawable and BatteryMeterDrawableBase separately.
    * Forward port styles by Ian and Danesh & improvements by LOS team.
    
    hungphan2001: port to Cherish R
    Change-Id: I316d7f31f070343406611fe55b8900118401c730
    Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4776,7 +4776,7 @@ public final class Settings {
          *    1 - Show percentage
          * @hide
          */
-        public static final String SHOW_BATTERY_PERCENT = "status_bar_show_battery_percent";
+        public static final String SHOW_BATTERY_PERCENT = "dummy_show_battery_percent";
 
         /**
          * The time in ms to keep the button backlight on after pressing a button.
@@ -5108,6 +5108,33 @@ public final class Settings {
          * @hide
          */
         public static final String QS_BRIGHTNESS_POSITION_BOTTOM = "qs_brightness_position_bottom";
+		
+		/**
+         * Battery style
+         * @hide
+         */
+        public static final String STATUS_BAR_BATTERY_STYLE = "status_bar_battery_style";
+
+        /**
+         * QS Battery mode
+         * @hide
+         */
+        public static final String QS_BATTERY_MODE = "qs_battery_mode";
+
+        /**
+          * SStatusbar Battery %
+          * 0: Hide the battery percentage
+          * 1: Display the battery percentage inside the icon
+          * 2: Display the battery percentage next to the icon
+          * @hide
+          */
+        public static final String STATUS_BAR_SHOW_BATTERY_PERCENT = "status_bar_show_battery_percent";
+
+        /**
+         * Show battery percentage when charging
+         * @hide
+         */
+        public static final String STATUS_BAR_BATTERY_TEXT_CHARGING = "status_bar_battery_text_charging";
 
         /**
          * Keys we no longer back up under the current schema, but want to continue to
@@ -5226,7 +5253,6 @@ public final class Settings {
             PRIVATE_SETTINGS.add(POINTER_SPEED);
             PRIVATE_SETTINGS.add(LOCK_TO_APP_ENABLED);
             PRIVATE_SETTINGS.add(EGG_MODE);
-            PRIVATE_SETTINGS.add(SHOW_BATTERY_PERCENT);
             PRIVATE_SETTINGS.add(DISPLAY_COLOR_MODE);
             PRIVATE_SETTINGS.add(BATTERY_LIGHT_ENABLED);
             PRIVATE_SETTINGS.add(BATTERY_LIGHT_ALLOW_ON_DND);
