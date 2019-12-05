27eace686971b1539c7b1529bdc75556dd1da699
AnkitGourav <Ankitgourav035@gmail.com>
2019-12-05 23:46:00 +0530

    SystemUI: Show keyguard charging animation on AOD
    Signed-off-by: AnkitGourav <Ankitgourav035@gmail.com>
    Signed-off-by: spezi77 <spezi7713@gmx.net>
    Signed-off-by: Joey Huab <joey@evolution-x.org>
    Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/KeyguardIndicationController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/KeyguardIndicationController.java
@@ -589,7 +589,7 @@ public class KeyguardIndicationController implements StateListener,
     }
 
     private void updateChargingIndication() {
-        if (mChargingIndication > 0 && !mDozing && mPowerPluggedIn) {
+        if (mChargingIndication > 0 && mPowerPluggedIn) {
             mChargingIndicationView.setVisibility(View.VISIBLE);
             mChargingIndicationView.playAnimation();
         } else {
