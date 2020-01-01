92339209ebc3134283079ddab0138ba5c3d42ab9
Pranav Vashi <neobuddy89@gmail.com>
2020-01-01 23:30:08 +0530

    Add option to select VoLTE icon [1/2]
    * Inspired by https://github.com/Project-Xtended/frameworks_base/commit/8351ba85af149361d54a70a5d2a5d5a75e494440
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6622,6 +6622,12 @@ public final class Settings {
         public static final String CLICK_PARTIAL_SCREENSHOT = "click_partial_screenshot";
 
         /**
+         * Whether to display VoLTE icon when VoLTE present
+         * @hide
+         */
+        public static final String VOLTE_ICON_STYLE = "volte_icon_style";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
