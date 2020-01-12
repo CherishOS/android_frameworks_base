8f26c1d140a119885f7c4aed565f813bf85aad96
Pranav Vashi <neobuddy89@gmail.com>
2020-01-12 10:09:14 +0530

    Add Comfortaa font [1/2]
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>

--- packages/overlays/Android.mk
+++ packages/overlays/Android.mk
@@ -40,6 +40,7 @@ LOCAL_REQUIRED_MODULES := \
 	FontFucekSourceOverlay \
 	FontGoogleSansMediumSourceOverlay \
 	FontLemonMilkSourceOverlay \
+	FontComfortaaSourceOverlay \
 	FontComicSansSourceOverlay \
 	FontGoogleSansSourceOverlay \
 	FontNotoSerifSourceOverlay \
