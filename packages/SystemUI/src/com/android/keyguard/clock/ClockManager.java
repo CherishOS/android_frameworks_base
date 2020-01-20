168550b17d82a76ca71ea34cc1517ae49bb08b34
rohan <purohit.rohan@gmail.com>
2020-01-20 23:48:18 +0530

    Add Custom Spectrum Analog Clock
    Thanks to mohancm
    Thanks to ROGERdotT for thumbnail.
    Change-Id: I6cca8ce309edd40cd2072f557778c95a28933ded
    Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>

--- packages/SystemUI/src/com/android/keyguard/clock/ClockManager.java
+++ packages/SystemUI/src/com/android/keyguard/clock/ClockManager.java
@@ -156,6 +156,7 @@ public final class ClockManager {
         addBuiltinClock(() -> new BubbleClockController(res, layoutInflater, colorExtractor));
         addBuiltinClock(() -> new AnalogClockController(res, layoutInflater, colorExtractor));
         addBuiltinClock(() -> new SpideyClockController(res, layoutInflater, colorExtractor));
+        addBuiltinClock(() -> new SpectrumClockController(res, layoutInflater, colorExtractor));
         addBuiltinClock(() -> new TypeClockController(res, layoutInflater, colorExtractor));
         addBuiltinClock(() -> new TypeClockAltController(res, layoutInflater, colorExtractor));
 
