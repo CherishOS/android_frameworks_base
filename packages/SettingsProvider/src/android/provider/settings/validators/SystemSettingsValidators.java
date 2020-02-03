41544863806b42cb5e04dea7fd03aac5e075df15
rituj <ritujbeniwal@gmail.com>
2020-02-03 09:26:45 +0530

    base: Add vibration patterns from OOS [1/3]
    idoybh edits: Add settings value to backup array
    
    Change-Id: I9622c17a1969598af71bcee573a576b752b96c5e
    Signed-off-by: rituj <ritujbeniwal@gmail.com>
    Signed-off-by: aswin7469 <aswinas@pixysos.com>
    Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>

--- packages/SettingsProvider/src/android/provider/settings/validators/SystemSettingsValidators.java
+++ packages/SettingsProvider/src/android/provider/settings/validators/SystemSettingsValidators.java
@@ -220,5 +220,6 @@ public class SystemSettingsValidators {
         VALIDATORS.put(System.LOCKSCREEN_BATTERY_INFO, BOOLEAN_VALIDATOR);
         VALIDATORS.put(System.NAV_BAR_COMPACT_LAYOUT, BOOLEAN_VALIDATOR);
         VALIDATORS.put(System.FORCE_FULLSCREEN_CUTOUT_APPS, ANY_STRING_VALIDATOR);
+        VALIDATORS.put(System.RINGTONE_VIBRATION_PATTERN, new InclusiveIntegerRangeValidator(0, 4));
     }
 }
