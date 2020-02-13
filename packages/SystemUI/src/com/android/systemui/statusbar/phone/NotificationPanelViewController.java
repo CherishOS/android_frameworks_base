470068de333ad497737b98d5522ecf297f0a59e3
Alex <du.alexcruz@gmail.com>
2020-02-12 20:10:07 -0500

    Keyguard: ReInflate views when density or font scale change
    Previously the keyguard's statusbar was not fully adapting to the density or
    font scale changes. Either the battery percent wouldn't stick or another icon
    would not change. This was fixed by telling the user to just restart systemui
    but that wasn't a real solution. This ensures that the statusbar view is indeed
    adapting accordingly.
    
    Test:
    - Go into Settings/Display
    - Change Font size or display size
    - Turn your screen off and then on
    - Look at the statusbar system icons
    
    Change-Id: I435fdad1dada3bb0674e9fd5ee530e71e0516af0

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelViewController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelViewController.java
@@ -3812,7 +3812,7 @@ public class NotificationPanelViewController extends PanelViewController {
     private class ConfigurationListener implements ConfigurationController.ConfigurationListener {
         @Override
         public void onDensityOrFontScaleChanged() {
-            updateShowEmptyShadeView();
+            reInflateViews();
         }
 
         @Override
