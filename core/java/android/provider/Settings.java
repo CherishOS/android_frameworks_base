8d67ad72ffb356c3d61e8121151a2073903d61fe
AnkitGourav <Ankitgourav035@gmail.com>
2020-02-25 00:33:21 +0530

    SystemUI: Ambient Edge Pulse style [1/2]
    - option to choose between faded & solid view
    
    @SKULSHADY: Update for Android 11
    
    Signed-off-by: Anushek Prasal <anushekprasal@gmail.com>
    Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6486,6 +6486,12 @@ public final class Settings {
         public static final String AMBIENT_LIGHT_REPEAT_DIRECTION = "ambient_light_repeat_direction";
 
         /**
+         * Ambient Edge Light Layout
+         * @hide
+         */
+        public static final String AMBIENT_LIGHT_LAYOUT = "ambient_light_layout";
+
+        /**
          * Battery style
          * @hide
          */
