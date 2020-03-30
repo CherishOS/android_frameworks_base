9d70abbc049746de43d634c3f59151e924b8d9f2
Kshitij Gupta <kshitijgm@gmail.com>
2020-03-30 16:36:14 +0530

    base: Disco mode!
    Change-Id: I263c6d2818d6615fc47e831d34b31ec5f4742a0e
    Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>

--- core/java/android/content/res/AccentUtils.java
+++ core/java/android/content/res/AccentUtils.java
@@ -6,6 +6,8 @@ import android.util.Log;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Calendar;
+import java.util.Date;
 
 /** @hide */
 public class AccentUtils {
@@ -48,6 +50,11 @@ public class AccentUtils {
     }
 
     private static int getAccentColor(int defaultColor, String property) {
+        Calendar cal = Calendar.getInstance();
+        cal.setTime(new Date());
+        if (cal.get(Calendar.MONTH) == 3 && cal.get(Calendar.DAY_OF_MONTH) == 1) {
+            return ColorUtils.genRandomAccentColor(property == ACCENT_COLOR_PROP);
+        }
         try {
             String colorValue = SystemProperties.get(property, "-1");
             return "-1".equals(colorValue)
