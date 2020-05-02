0aec09be6b9831228a8ea58fc7fe124bb8eb0ace
Advaith Bhat <advaithbhat9@gmail.com>
2020-05-02 03:56:52 +0000

    base: Use Selectable Network Traffic Fonts [1/2]
    Signed-off-by: Aston-Martinn <advaithbhat9@gmail.com>
    Change-Id: I5e2a828fe54f47097bf1b388ebe6df79d44113ff
    Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5773,6 +5773,12 @@ public final class Settings {
          */
          public static final String NETWORK_TRAFFIC_VIEW_LOCATION = "network_traffic_view_location";
 
+	/**
+         * Network traffic font style
+         * @hide
+         */
+        public static final String NETWORK_TRAFFIC_FONT_STYLE = "network_traffic_font_style";
+
         /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
