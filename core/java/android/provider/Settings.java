1c95e299b3c80306004d4bb799d62057b68a1b0e
Jesse Chan <jc@lineageos.org>
2020-05-17 22:41:42 +0800

    Implement click to take partial screenshot [1/2]
    Default to the old long press behaviour when the feature is enabled.
    
    Change-Id: I5fc37f818b06db2bd2d315cffee2041a740781ed
    Signed-off-by: Jesse Chan <jc@lineageos.org>
    Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6633,6 +6633,12 @@ public final class Settings {
         public static final String AMBIENT_CUSTOM_IMAGE = "ambient_custom_image";
 
         /**
+         * Whether to take partial screenshot with volume down + power click.
+         * @hide
+         */
+        public static final String CLICK_PARTIAL_SCREENSHOT = "click_partial_screenshot";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
