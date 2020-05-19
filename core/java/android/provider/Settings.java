f5a9fe830e9cb1847f939f2ffc29d8d0c95f2757
Jesse Chan <jc@lineageos.org>
2020-05-19 16:05:23 +0800

    SystemUI: Implement hide gestural navigation hint bar
    Author: Jesse Chan <jc@lineageos.org>
    Date:   Tue Jun 2 13:59:52 2020 +0800
    
        SystemUI: Set no hint overlay to highest priority
    
        As overlays are also used to apply navigation mode, it is needed to set
        our customization overlay to highest priority to ensure it is applied.
    
        Change-Id: I41ca4b7ea99fea9f62d2e8ece5114f682e8e7fe9
        Signed-off-by: Jesse Chan <jc@lineageos.org>
    
    Change-Id: Ic6695dfe929db443f169d59f153e14a6de249590
    Signed-off-by: Jesse Chan <jc@lineageos.org>
    Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6552,6 +6552,12 @@ public final class Settings {
         public static final String NAV_BAR_COMPACT_LAYOUT = "navigation_bar_compact_layout";
 
         /**
+         * boolean value. toggles swipe up hint in gestural nav mode
+         * @hide
+         */
+        public static final String NAVIGATION_BAR_HINT = "navigation_bar_hint";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
