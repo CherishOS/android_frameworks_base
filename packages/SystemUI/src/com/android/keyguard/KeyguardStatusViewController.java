8685300b2b35d332ec1771695aa15f46df12435a
TikkiTikki <radstylemusic@gmail.com>
2020-05-23 23:36:01 -0300

    Add Ambient Customizations [1/2]
    This adds the option to add custom text and a custom image on the ambient screen. This option is still in development, so be patient, the bugs that arise will be resolved.
    Based on OmniROM Edge Lighting and Custom Headers.
    
    [SuperDroidBond: adapt for R + A13 & put it behind notification lights]
    
    Good Work by @/TikkiX2
    
    Change-Id: I8bbda7e908ef4bae892c59fdbcf6032a799b3f5b
    
    fixup! Ambient Customizations
    
    SDB:
    1. Fix Text Animation
    2. Fix Pulse Lights not visible if Ambient Image activated
    3. Catch NPE in KeyguardVisibilityHelper
    4. Misc. fixes for Ambient Text & Images
    
    Change-Id: Icbc66effd362d5e7d4e77d977960778bfc19dc7a
    
    Co-authored-by: TikkiTikki <radstylemusic@gmail.com>
    Co-authored-by: SuperDroidBond <superdroidbond@yahoo.com>
    Signed-off-by: SuperDroidBond <superdroidbond@yahoo.com>
    Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>

--- packages/SystemUI/src/com/android/keyguard/KeyguardStatusViewController.java
+++ packages/SystemUI/src/com/android/keyguard/KeyguardStatusViewController.java
@@ -31,6 +31,7 @@ import com.android.systemui.statusbar.phone.DozeParameters;
 import com.android.systemui.statusbar.phone.ScreenOffAnimationController;
 import com.android.systemui.statusbar.policy.ConfigurationController;
 import com.android.systemui.statusbar.policy.KeyguardStateController;
+
 import com.android.systemui.util.ViewController;
 
 import javax.inject.Inject;
