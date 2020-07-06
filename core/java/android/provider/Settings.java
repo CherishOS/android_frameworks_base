d931538b4a65cf2a461c8869b280c12784b6e6bf
Anushek Prasal <anushekprasal@gmail.com>
2020-07-07 00:19:10 +0530

    base: Allow to show daily data usage [1/2]
    Signed-off-by: Anushek Prasal <anushekprasal@gmail.com>
    Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6636,6 +6636,14 @@ public final class Settings {
         };
 
         /**
+         * Data usage period
+         * 0: Daily
+         * 1: Monthly
+         * @hide
+         */
+        public static final String DATA_USAGE_PERIOD = "data_usage_period";
+
+        /**
          * These are all public system settings
          *
          * @hide
