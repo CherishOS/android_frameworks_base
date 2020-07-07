a2e69780d4af16b817f5cf31710aa70086ae28d7
Pranav Vashi <neobuddy89@gmail.com>
2020-07-08 03:30:42 +0530

    Add switch to select SBC HD by default [1/3]
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6608,6 +6608,12 @@ public final class Settings {
         public static final String QS_RUNNING_SERVICES_TOGGLE = "qs_running_services_toggle";
 
         /**
+         * SBC HD priority - whether to use dual channel for SBC by default
+         * @hide
+         */
+        public static final String SBC_HD_PRIORITY = "sbc_hd_priority";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
