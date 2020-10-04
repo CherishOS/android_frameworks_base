feadf96d10309901dfe2f188d3e0017cd325ab14
jhonboy121 <alfredmathew05@gmail.com>
2020-10-04 12:49:28 -0700

    SystemUI: allow changing the length of gesture navbar [1/2]
    * forward ported from 11 and improved a lot by delegating all the dirty work to controllers and using LayoutParams for changing the width instead of screwing around in onDraw.
    
    Signed-off-by: jhonboy121 <alfredmathew05@gmail.com>
    Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -10863,6 +10863,14 @@ public final class Settings {
         public static final String TETHERING_ALLOW_VPN_UPSTREAMS = "tethering_allow_vpn_upstreams";
 
         /**
+         * Gesture navbar length mode.
+         * Supported modes: 0 for normal length, 1 for medium and 2 for long.
+         * Default 0.
+         * @hide
+         */
+        public static final String GESTURE_NAVBAR_LENGTH_MODE = "gesture_navbar_length_mode";
+
+        /**
          * These entries are considered common between the personal and the managed profile,
          * since the managed profile doesn't get to change them.
          */
