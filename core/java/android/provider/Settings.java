1d34a537d7af7bfa51be94d04cd21c6bb6d1d24f
Ido Ben-Hur <idoybh2@gmail.com>
2020-11-01 17:28:34 +0200

    base: Allow customizing volume dialog timeout [1/2]
    Also reduce show & hide animation by a bit
    
    Change-Id: Idb9e9147d999996dac5d78996f4492d7aa1c515d
    Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5693,6 +5693,13 @@ public final class Settings {
          * @hide
          */
         public static final String VOWIFI_ICON_STYLE = "vowifi_icon_style";
+		
+		/**
+         * Volume dialog timeout. Value in seconds
+         * @hide
+         */
+        @Readable
+        public static final String VOLUME_DIALOG_TIMEOUT = "volume_dialog_timeout";
 
         /**
          * Keys we no longer back up under the current schema, but want to continue to
