65c3ad3d155445f95e4a4c60bbaf9471bf862b06
DennySPb <dennyspb@gmail.com>
2020-11-03 14:49:54 +0300

    Legacy WFD output video mode settings[1/3]
    Change-Id: I57fed841d91f2c48f070e154538a58cd98116ed6
    Signed-off-by: DennySPb <dennyspb@gmail.com>
    Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5822,6 +5822,11 @@ public final class Settings {
         public static final String BACK_GESTURE_HAPTIC = "back_gesture_haptic";
 
         /**
+         * @hide
+         */
+        public static final String WFD_LEGACY_VIDEO_MODE = "wfd_legacy_video_mode";
+        
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
