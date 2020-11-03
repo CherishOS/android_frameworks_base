f6dcf7375f156a41c758497353a4cd204ab943e6
timschumi <timschumi@users.noreply.github.com>
2020-11-03 08:47:23 +0000

    SystemUI: Enable NFC tile
    Author: Thecrazyskull <anaskarbila@gmail.com>
    Date:   Tue Jan 17 10:56:39 2017 -0600
    
        SystemUI: Enable NFC tile
    
        Change-Id: Ib21f927d336fcb5e460552786828b79551b235fa
    
    Author: Luca Stefani <luca.stefani.ge1@gmail.com>
    Date:   Tue Sep 4 10:07:17 2018 +0200
    
        SystemUI: Update NFCTile to match P style
    
        Change-Id: I8309eda6b2dbb28156dd463e667ff60545b37b7d
    
    Change-Id: Ia06b923ee15e91a9c84a806d4c69755478df2063
    Signed-off-by: Hung Phan <phandinhhungvp2001@gmail.com>

--- packages/SystemUI/src/com/android/systemui/qs/tiles/NfcTile.java
+++ packages/SystemUI/src/com/android/systemui/qs/tiles/NfcTile.java
@@ -41,6 +41,8 @@ import javax.inject.Inject;
 /** Quick settings tile: Enable/Disable NFC **/
 public class NfcTile extends QSTileImpl<BooleanState> {
 
+    private final Icon mIcon = ResourceIcon.get(R.drawable.ic_qs_nfc);
+
     private NfcAdapter mAdapter;
     private BroadcastDispatcher mBroadcastDispatcher;
 
@@ -138,8 +140,7 @@ public class NfcTile extends QSTileImpl<BooleanState> {
         state.state = getAdapter() == null
                 ? Tile.STATE_UNAVAILABLE
                 : state.value ? Tile.STATE_ACTIVE : Tile.STATE_INACTIVE;
-        state.icon = ResourceIcon.get(
-                state.value ? R.drawable.ic_qs_nfc_enabled : R.drawable.ic_qs_nfc_disabled);
+        state.icon = mIcon;
         state.label = mContext.getString(R.string.quick_settings_nfc_label);
         state.expandedAccessibilityClassName = Switch.class.getName();
         state.contentDescription = state.label;
