882682683ea1c7cdee6ff858f47c4ece0c122470
Keith Mok <kmok@cyngn.com>
2020-11-10 10:47:09 -0300

    SystemUI: Add caffeine QS tile
    * Caffeine Tile icon designed by Andrew Fluck
    
    [Adin Kwok] Add long press to infinity to caffeine qs tile
    [ghostrider-reborn] Updated for android 12
    
    Co-authored-by: Adin Kwok <adin@adin.dev>
    Change-Id: If54ea27ddf179c2de39b61850163e14173727698
    Signed-off-by: Adithya <gh0strider.2k18.reborn@gmail.com>
    Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>

--- packages/SystemUI/src/com/android/systemui/qs/dagger/cherish/QSModuleCherish.kt
+++ packages/SystemUI/src/com/android/systemui/qs/dagger/cherish/QSModuleCherish.kt
@@ -17,6 +17,7 @@
 package com.android.systemui.qs.dagger.cherish
 
 import com.android.systemui.qs.tileimpl.QSTileImpl
+import com.android.systemui.qs.tiles.CaffeineTile
 import com.android.systemui.qs.tiles.CellularTile
 import com.android.systemui.qs.tiles.PowerShareTile
 import com.android.systemui.qs.tiles.WifiTile
@@ -27,6 +28,12 @@ import dagger.multibindings.StringKey
 
 @Module
 interface QSModuleCherish {
+
+    /** Inject CaffeineTile into tileMap in QSModule */
+    @Binds
+    @IntoMap
+    @StringKey(CaffeineTile.TILE_SPEC)
+    fun bindCaffeineTile(caffeineTile: CaffeineTile): QSTileImpl<*>
 
     /** Inject CellularTile into tileMap in QSModule */
     @Binds
