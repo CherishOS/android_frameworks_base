9c6147bb6708909f596567f8f7031b4b36214cd3
Dil3mm4 <dil3mm4.dev@gmail.com>
2020-11-17 16:33:38 +0000

    SquaryClock: thumbnail and preview
    Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>

--- packages/SystemUI/src/com/android/keyguard/clock/SquaryClockController.java
+++ packages/SystemUI/src/com/android/keyguard/clock/SquaryClockController.java
@@ -134,27 +134,14 @@ public class SquaryClockController implements ClockPlugin {
 
     @Override
     public Bitmap getThumbnail() {
-        return BitmapFactory.decodeResource(mResources, R.drawable.samsung_bold_thumbnail);
+        return BitmapFactory.decodeResource(mResources, R.drawable.squary_clock_thumbnail);
     }
 
     @Override
     public Bitmap getPreview(int width, int height) {
-
-        View previewView = mLayoutInflater.inflate(R.layout.default_clock_preview, null);
-        TextClock previewTime = previewView.findViewById(R.id.time);
-        previewTime.setFormat12Hour(Html.fromHtml("<strong>hh</strong><br>mm"));
-        previewTime.setFormat24Hour(Html.fromHtml("<strong>kk</strong><br>mm"));
-        TextClock previewDate = previewView.findViewById(R.id.date);
-
-        // Initialize state of plugin before generating preview.
-        previewTime.setTextColor(Color.WHITE);
-        previewDate.setTextColor(Color.WHITE);
-        ColorExtractor.GradientColors colors = mColorExtractor.getColors(
-                WallpaperManager.FLAG_LOCK);
-        setColorPalette(colors.supportsDarkText(), colors.getColorPalette());
-        onTimeTick();
-
-        return mRenderer.createPreview(previewView, width, height);
+        createViews();
+        setTextColor(1);
+        return mRenderer.createPreview(getView(), width, height);
     }
 
     @Override
