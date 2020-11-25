ded140641908984f36ff3a480b3ef7b447988907
ezio84 <brabus84@gmail.com>
2020-11-25 19:59:56 +0100

    Advanced nav gestures: improve code and fix glitches
    fix  arrows animation glitch after triggering an action
    with the extended swipe
    
    hide the arrows before triggering a screenshot action
    
    code cleanup
    
    Change-Id: I5f01938b6b688acd14225f58ce5a2032f083f92f
    Signed-off-by: spezi77 <spezi7713@gmx.net>
    Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/EdgeBackGestureHandler.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/EdgeBackGestureHandler.java
@@ -201,7 +201,6 @@ public class EdgeBackGestureHandler extends CurrentUserTracker implements Displa
     private boolean mIsExtendedSwipe;
     private int mLeftVerticalSwipeAction;
     private int mRightVerticalSwipeAction;
-    private boolean mBlockNextEvent;
     private Handler mHandler;
     private final Vibrator mVibrator;
     private boolean mImeVisible;
@@ -774,7 +773,7 @@ public class EdgeBackGestureHandler extends CurrentUserTracker implements Displa
                 mEndPoint.set(-1, -1);
                 mThresholdCrossed = false;
             }
-        } else if ((mAllowGesture || mLogGesture) && !mBlockNextEvent) {
+        } else if (mAllowGesture || mLogGesture) {
             if (!mThresholdCrossed) {
                 // mThresholdCrossed is true only after the first move event
                 // then other events will go straight to "forward touch" line
@@ -849,9 +848,6 @@ public class EdgeBackGestureHandler extends CurrentUserTracker implements Displa
                 // forward touch
                 mEdgeBackPlugin.onMotionEvent(ev);
             }
-        } else if (mBlockNextEvent) {
-            mBlockNextEvent = false;
-            cancelGesture(ev);
         }
 
         Dependency.get(ProtoTracer.class).update();
@@ -872,8 +868,11 @@ public class EdgeBackGestureHandler extends CurrentUserTracker implements Displa
     }
 
     private void prepareForAction() {
-        mBlockNextEvent = true;
-        mEdgeBackPlugin.resetOnDown();
+        // cancel touch event then trigger the action
+        final long now = SystemClock.uptimeMillis();
+        final MotionEvent ev = MotionEvent.obtain(now, now,
+                MotionEvent.ACTION_CANCEL, 0.0f, 0.0f, 0);
+        cancelGesture(ev);
         mVibrator.vibrate(VibrationEffect.get(VibrationEffect.EFFECT_HEAVY_CLICK));
     }
 
