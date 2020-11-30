3214d1d6245c13c52ce349f98465d29f75053a6d
Joey Huab <joey@evolution-x.org>
2020-12-01 06:50:16 +0900

    [SQUASH] SystemUI: FOD pressed colors [1/2]
    Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5881,7 +5881,12 @@ public final class Settings {
          * @hide
          */
         public static final String FOD_ICON = "fod_icon";
-
+		
+		/**
+         * FOD pressed color
+         * @hide
+         */
+        public static final String FOD_COLOR = "fod_color";
 
         /**
          * Whether to use the custom status bar header or not
