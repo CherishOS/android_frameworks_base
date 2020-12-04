8a7f994a308609591f2adeccd9211b78d107efec
Dil3mm4 <dil3mm4.dev@gmail.com>
2020-12-04 18:42:54 +0000

    SystemUI: ensure keyguardstatusbar is hidden if not on keyguard
    There seem to be some un-identified weird cases where the various runnable aren't execute by keyguard dismissal, therefore let's be sure mKeyguardStatusBar status is correct on view reinflation.
    
    Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelViewController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelViewController.java
@@ -792,6 +792,7 @@ public class NotificationPanelViewController extends PanelViewController {
         mStatusBarStateListener.onDozeAmountChanged(mStatusBarStateController.getDozeAmount(),
                 mStatusBarStateController.getInterpolatedDozeAmount());
 
+        mKeyguardStatusBar.setVisibility(mKeyguardShowing ? View.VISIBLE : View.INVISIBLE);
         setKeyguardStatusViewVisibility(mBarState, false, false);
         setKeyguardBottomAreaVisibility(mBarState, false);
         if (mOnReinflationListener != null) {
