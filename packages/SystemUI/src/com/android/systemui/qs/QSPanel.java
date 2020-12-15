c70d591ce50045a033c346a28296e6fb586011bb
Hưng Phan <phandinhhungvp2001@gmail.com>
2020-12-15 13:04:38 +0700

    Revert "SystemUI: Brightness slider above footer jumps to top when media playing"
    This reverts commit a5387f76ce98d6e0aa36bc0a9d8dc50e9a772e47.

--- packages/SystemUI/src/com/android/systemui/qs/QSPanel.java
+++ packages/SystemUI/src/com/android/systemui/qs/QSPanel.java
@@ -305,7 +305,6 @@ public class QSPanel extends LinearLayout implements Callback, BrightnessMirrorL
     protected void onMediaVisibilityChanged(Boolean visible) {
         mMediaVisible = visible;
         switchTileLayout();
-        updateBrightnessSliderPosition();
         if (mMediaVisibilityChangedListener != null) {
             mMediaVisibilityChangedListener.accept(visible);
         }
@@ -478,7 +477,7 @@ public class QSPanel extends LinearLayout implements Callback, BrightnessMirrorL
         boolean above = Settings.System.getIntForUser(mContext.getContentResolver(),
                 Settings.System.QS_SHOW_BRIGHTNESS_ABOVE_FOOTER, 0,
                 UserHandle.USER_CURRENT) == 1;
-        View seekView = above && !mMediaVisible ? mFooter : mDivider;
+        View seekView = above ? mFooter : mDivider;
         for (int i = 0; i < getChildCount(); i++) {
             View v = getChildAt(i);
             if (v == seekView) {
