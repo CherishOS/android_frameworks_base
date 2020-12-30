aefb0cc6ef37c43a9d098f29cfd156c13fe26a30
rohan <purohit.rohan@gmail.com>
2020-12-30 19:30:29 +0900

    SystemUI: Fix active color for qs tile styles
    Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>

--- packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSTileImpl.java
+++ packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSTileImpl.java
@@ -540,7 +540,8 @@ public abstract class QSTileImpl<TState extends State> implements QSTile, Lifecy
 
         boolean qsIconPrimary = Settings.System.getIntForUser(context.getContentResolver(),
                     Settings.System.QS_TILE_ICON_PRIMARY, 0, UserHandle.USER_CURRENT) == 1;
-
+        int qsTileStyle = Settings.System.getIntForUser(context.getContentResolver(),
+                Settings.System.QS_TILE_STYLE, 0, UserHandle.USER_CURRENT);
         switch (state) {
             case Tile.STATE_UNAVAILABLE:
                 return Utils.getDisabled(context,
@@ -548,6 +549,10 @@ public abstract class QSTileImpl<TState extends State> implements QSTile, Lifecy
             case Tile.STATE_INACTIVE:
                 return Utils.getColorAttrDefaultColor(context, android.R.attr.textColorSecondary);
             case Tile.STATE_ACTIVE:
+			     if (qsTileStyle == 7 || qsTileStyle == 9 || qsTileStyle == 10 || qsTileStyle == 12 ||
+                        qsTileStyle == 13 || qsTileStyle == 14) {
+                    return Utils.getColorAttrDefaultColor(context, android.R.attr.colorAccent);
+                } else{
                  if (setQsUseNewTint == 1) {
                      return ColorUtils.genRandomAccentColor(isThemeDark(context));
                  } else if (setQsUseNewTint == 2) {
@@ -559,6 +564,7 @@ public abstract class QSTileImpl<TState extends State> implements QSTile, Lifecy
                          return Utils.getColorAttrDefaultColor(context, android.R.attr.colorPrimary);
                      }
                  }
+			}
             default:
                 Log.e("QSTile", "Invalid state " + state);
                 return 0;
