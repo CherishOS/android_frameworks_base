b6934dc4ad7ce955e6aef0a1bb9a7254ff0dcbdb
Jason Edson <jaysonedson@gmail.com>
2021-01-07 16:22:43 -0800

    SystemUI: QS: Add flipendo to Battery Saver tile
    Add Extreme Battery Saver(Flipendo) to the Battery Saver tile.
    
    Change-Id: Ib2a4051e63d636a4de582c05f9514f3b8ee7b2a5
    Signed-off-by: Jason Edson <jaysonedson@gmail.com>
    Signed-off-by: Joey Huab <joey@evolution-x.org>
    Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>

--- packages/SystemUI/src/com/android/systemui/dagger/SystemUIDefaultModule.java
+++ packages/SystemUI/src/com/android/systemui/dagger/SystemUIDefaultModule.java
@@ -44,6 +44,7 @@ import com.android.systemui.qs.dagger.QSModule;
 import com.android.systemui.qs.tileimpl.QSFactoryImpl;
 import com.android.systemui.recents.Recents;
 import com.android.systemui.recents.RecentsImplementation;
+import com.android.systemui.settings.UserContentResolverProvider;
 import com.android.systemui.statusbar.CommandQueue;
 import com.android.systemui.statusbar.NotificationLockscreenUserManager;
 import com.android.systemui.statusbar.NotificationLockscreenUserManagerImpl;
@@ -106,7 +107,8 @@ public abstract class SystemUIDefaultModule {
             BroadcastDispatcher broadcastDispatcher,
             DemoModeController demoModeController,
             @Main Handler mainHandler,
-            @Background Handler bgHandler) {
+            @Background Handler bgHandler,
+            UserContentResolverProvider userContentResolverProvider) {
         BatteryController bC = new BatteryControllerImpl(
                 context,
                 enhancedEstimates,
@@ -114,7 +116,8 @@ public abstract class SystemUIDefaultModule {
                 broadcastDispatcher,
                 demoModeController,
                 mainHandler,
-                bgHandler);
+                bgHandler,
+                userContentResolverProvider);
         bC.init();
         return bC;
     }
