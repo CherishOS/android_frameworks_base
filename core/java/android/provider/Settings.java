3e7e3a1ed74a52710409734f7262bed06e81ea42
spezi77 <spezi7713@gmx.net>
2021-01-26 18:44:33 +0300

    Pulse: Extend to Ambient Screen
    neobuddy89: Includes ambient mode clean up and fixes.
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6096,6 +6096,12 @@ public final class Settings {
         public static final String PULSE_SMOOTHING_ENABLED = "pulse_smoothing_enabled";
 		
 		/**
+         * Pulse ambient music visualizer
+         * @hide
+         */
+        public static final String AMBIENT_PULSE_ENABLED = "ambient_pulse_enabled";
+		
+		/**
          * Whether to enable PULSE Edge lights
          * @hide
          */
