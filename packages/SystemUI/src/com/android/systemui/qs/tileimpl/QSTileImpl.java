c2a196c92292662df96ff8234a9d27187f0ff59d
Hưng Phan <phandinhhungvp2001@gmail.com>
2021-01-28 15:03:32 +0700

    QSTileImpl:fix Gradient work with custom qs tiles
    Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>

--- packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSTileImpl.java
+++ packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSTileImpl.java
@@ -549,22 +549,20 @@ public abstract class QSTileImpl<TState extends State> implements QSTile, Lifecy
             case Tile.STATE_INACTIVE:
                 return Utils.getColorAttrDefaultColor(context, android.R.attr.textColorSecondary);
             case Tile.STATE_ACTIVE:
-			     if (qsTileStyle == 7 || qsTileStyle == 9 || qsTileStyle == 10 || qsTileStyle == 12 ||
-                        qsTileStyle == 13 || qsTileStyle == 14) {
-                    return Utils.getColorAttrDefaultColor(context, android.R.attr.colorAccent);
-                } else{
-                 if (setQsUseNewTint == 1) {
-                     return ColorUtils.genRandomAccentColor(isThemeDark(context));
-                 } else if (setQsUseNewTint == 2) {
-                     return Utils.getColorAttrDefaultColor(context, android.R.attr.colorAccent);
-                 } else {
-                     if (qsIconPrimary) {
-                         return Utils.getColorAttrDefaultColor(context, android.R.attr.textColorPrimary);
-                     } else {
-                         return Utils.getColorAttrDefaultColor(context, android.R.attr.colorPrimary);
-                     }
-                 }
-			}
+			     if (setQsUseNewTint == 1)
+                        return ColorUtils.genRandomAccentColor(isThemeDark(context));
+                    else if (setQsUseNewTint == 2 && (qsTileStyle == 0 || qsTileStyle == 7 ||
+                            qsTileStyle == 9 || qsTileStyle == 10 || qsTileStyle == 12 || qsTileStyle == 13 || qsTileStyle == 14))
+                            return Utils.getColorAttrDefaultColor(context, android.R.attr.colorAccent);
+                    else
+                        if (qsTileStyle == 7 || qsTileStyle == 9 || qsTileStyle == 10 ||
+                                                       qsTileStyle == 12 || qsTileStyle == 13 || qsTileStyle == 14)
+                            return Utils.getColorAttrDefaultColor(context, android.R.attr.colorAccent);
+                        else
+                            if (qsIconPrimary)
+                                return Utils.getColorAttrDefaultColor(context, android.R.attr.textColorPrimary);
+                            else
+                                return Utils.getColorAttrDefaultColor(context, android.R.attr.colorPrimary);
             default:
                 Log.e("QSTile", "Invalid state " + state);
                 return 0;
