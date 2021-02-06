d5c34ac4dc1e24e75cbd995db531f30057e59ff1
Arian <arian.kulmer@web.de>
2021-02-06 15:58:35 +0100

    SystemUI: tiles: Open display settings on long clicks for livedisplay tiles
    * Since commit "LineageParts: Allow removing livedisplay pref screen with
      config_enableLiveDisplay" I17103ad116d1d26855fb47fdee1fa6cdc914e053 the
      org.lineageos.lineageparts.LIVEDISPLAY_SETTINGS action does not exist
      anymore and hence the long click was broken.
    
    * Instead open the display settings where the LiveDisplay settings can be
      found easily.
    
    Change-Id: I5ddcf9f8877552f60adb5216d065ffdf93786a44
    Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>

--- packages/SystemUI/src/com/android/systemui/qs/tiles/ReadingModeTile.java
+++ packages/SystemUI/src/com/android/systemui/qs/tiles/ReadingModeTile.java
@@ -34,8 +34,7 @@ public class ReadingModeTile extends QSTileImpl<BooleanState> {
 
     private final Icon mIcon = ResourceIcon.get(R.drawable.ic_qs_reader);
 
-    private static final Intent LIVEDISPLAY_SETTINGS =
-            new Intent("com.android.settings.LIVEDISPLAY_SETTINGS");
+    private static final Intent DISPLAY_SETTINGS = new Intent("android.settings.DISPLAY_SETTINGS");
 
     private LineageHardwareManager mHardware;
 
@@ -59,7 +58,7 @@ public class ReadingModeTile extends QSTileImpl<BooleanState> {
 
     @Override
     public Intent getLongClickIntent() {
-        return LIVEDISPLAY_SETTINGS;
+        return DISPLAY_SETTINGS;
     }
 
     @Override
