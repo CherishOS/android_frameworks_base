6ae7444bdae53b6e3b14696a7e83b001c8f92616
TikkiTikki <radstylemusic@gmail.com>
2021-03-21 19:44:39 -0300

    Pulse: Custom gravity and Center mirrored [1/2]
    SDB: add all in-line to Secure Settings
    Change-Id: Iccd1ed13c51691ec238ed91b9452c43f4cb61a87
    Signed-off-by: mukesh22584 <mks22584@gmail.com>
    Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -13485,6 +13485,18 @@ public final class Settings {
          */
         public static final String PULSE_SMOOTHING_ENABLED = "pulse_smoothing_enabled";
 
+	/**
+         * Pulse gravity
+         * @hide
+         */
+        public static final String PULSE_CUSTOM_GRAVITY = "pulse_custom_gravity";
+
+        /**
+         * Enable and disable QS Panel visualizer
+         * @hide
+         */
+        public static final String VISUALIZER_CENTER_MIRRORED = "visualizer_center_mirrored";
+
         /**
          * Whether to enable DOZE only when charging
          * @hide
