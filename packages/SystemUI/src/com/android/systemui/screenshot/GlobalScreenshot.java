a860e8e08a3f7184c24391685a6849a384dfcc39
Pranav Vashi <neobuddy89@gmail.com>
2021-04-10 16:30:52 +0530

    Further improve redesigning screenshot menu buttons
    * Update colors as well:
      https://github.com/crdroidandroid/android_vendor_addons/commit/b69727844816084b1cba8bdb5db6d7aafffdb547
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>

--- packages/SystemUI/src/com/android/systemui/screenshot/GlobalScreenshot.java
+++ packages/SystemUI/src/com/android/systemui/screenshot/GlobalScreenshot.java
@@ -1066,7 +1066,6 @@ public class GlobalScreenshot implements ViewTreeObserver.OnComputeInternalInset
         for (Notification.Action smartAction : imageData.smartActions) {
             ScreenshotActionChip actionChip = (ScreenshotActionChip) inflater.inflate(
                     R.layout.global_screenshot_action_chip, mActionsView, false);
-            actionChip.setText(smartAction.title);
             actionChip.setIcon(smartAction.getIcon(), false);
             actionChip.setPendingIntent(smartAction.actionIntent,
                     () -> {
@@ -1154,7 +1153,7 @@ public class GlobalScreenshot implements ViewTreeObserver.OnComputeInternalInset
             mBackgroundProtection.setAlpha(t);
             float containerAlpha = t < alphaFraction ? t / alphaFraction : 1;
             mActionsContainer.setAlpha(containerAlpha);
-            mActionsContainerBackground.setAlpha(containerAlpha);
+            mActionsContainerBackground.setAlpha(containerAlpha/2);
             float containerScale = SCREENSHOT_ACTIONS_START_SCALE_X
                     + (t * (1 - SCREENSHOT_ACTIONS_START_SCALE_X));
             mActionsContainer.setScaleX(containerScale);
