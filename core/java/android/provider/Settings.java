ddc0476ea0fc493a28ab58162f92d91c59a08c20
TikkiTikki <radstylemusic@gmail.com>
2021-04-18 19:37:06 -0300

    Pulse: Vertical mirror [1/2]
    Change-Id: I4251534e88a8a88c0af418116c30e86bcda405ab
    Signed-off-by: mukesh22584 <mks22584@gmail.com>
    Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -13457,6 +13457,12 @@ public final class Settings {
          * @hide
          */
         public static final String VISUALIZER_CENTER_MIRRORED = "visualizer_center_mirrored";
+        
+        /**
+         * Pulse vertical mirror
+         * @hide
+         */
+        public static final String PULSE_VERTICAL_MIRROR = "pulse_vertical_mirror";    
 
         /**
          * Keys we no longer back up under the current schema, but want to continue to
