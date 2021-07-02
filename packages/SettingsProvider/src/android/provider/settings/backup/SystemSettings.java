TenX-OS/frameworks_base

SystemUI: Allow setting Custom statusbar Paddings [1/2]
- Start padding.
- End padding.

Signed-off-by: Aston-Martinn <advaithbhat9@gmail.com>

packages/SettingsProvider/src/android/provider/settings/backup/SystemSettings.java
@@ -107,5 +107,7 @@ public class SystemSettings {
         Settings.System.KEYGAURD_BATTERY_BAR_COLOR_MODE,
         Settings.System.KEYGAURD_BATTERY_BAR_COLOR_CUSTOM,
         Settings.System.LOCKSCREEN_BATTERY_INFO_FONT,
+        Settings.System.CUSTOM_STATUSBAR_PADDING_START,
+        Settings.System.CUSTOM_STATUSBAR_PADDING_END,
     };
 }

CherishOS/android_frameworks_base

Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>

packages/SettingsProvider/src/android/provider/settings/backup/SystemSettings.java
@@ -100,6 +100,8 @@ public class SystemSettings {
         Settings.System.KEYGAURD_BATTERY_BAR_COLOR_CUSTOM,
         Settings.System.LOCKSCREEN_BATTERY_INFO_FONT,
         Settings.System.COLOR_BUCKET_OVERLAY,
-        Settings.System.LOCKSCREEN_MEDIA_METADATA
+        Settings.System.LOCKSCREEN_MEDIA_METADATA,
+        Settings.System.CUSTOM_STATUSBAR_PADDING_START,
+        Settings.System.CUSTOM_STATUSBAR_PADDING_END
     };
 }