4bd48f24ab3387e863f1990cdec53de89f989870
Hưng Phan <phandinhhungvp2001@gmail.com>
2021-07-20 09:37:14 +0700

    base: Goodbye redundant nav gesture options [1/2]:
    Revert "Gestures: allow to set back swipe deadzone [1/2]"
    
    This reverts commit 9810b57da8146586038118c668cff9ca360d0a55.
    
    Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>
    
    Revert "base: Allow hide gesture navbar [1/3]"
    
    This reverts commit 1715f68633b5bff5481d3c3c77f77281e8edb72c.

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6274,16 +6274,6 @@ public final class Settings {
         public static final String PULSE_ON_NEW_TRACKS = "pulse_on_new_tracks";
 
         /**
-         * Set edge gesture Y dead zone
-         * 0 - disabled - stock behavior - default
-         * 1 - smaller top area of the screen
-         * 2 - small top area of the screen
-         * 3 - half top of the screen
-         * @hide
-         */
-        public static final String EDGE_GESTURE_Y_DEAD_ZONE = "edge_gesture_y_dead_zone";
-
-        /**
          * Gestures nav: left long back swipe action
          * @hide
          */
