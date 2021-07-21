1e04bcf8d9f1c3471429f1f31e310dc2ddd2c031
Hưng Phan <phandinhhungvp2001@gmail.com>
2021-07-21 14:48:02 +0700

    Revert "fw/b: [Squash] Add capability to allow tethering to use VPN upstreams"
    This reverts commit 0abecf44d5c7524a73e76de3bad201cd5338d56f.
    
    Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -11208,11 +11208,6 @@ public final class Settings {
          * @hide
          */
         public static final String QUICK_SETTINGS_TILES_VIBRATE = "quick_settings_vibrate";
-
-        /**
-         * Whether tethering is allowed to use VPN upstreams
-         */
-        public static final String TETHERING_ALLOW_VPN_UPSTREAMS = "tethering_allow_vpn_upstreams";
 		
 		/**
          * Whether the Adaptive connectivity option is enabled.
