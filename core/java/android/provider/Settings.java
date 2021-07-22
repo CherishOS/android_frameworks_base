32d27b41fb66d30cc3d0dcc6cd9061557e06eee1
703joko <703joko@gmail.com>
2021-07-22 12:39:13 +0700

    SystemUI : Introduce Battery Landscape | Left Battery Text [1/2]
    Inspiration from Ancient ROM, thanks to Kakek for helps
    
    Signed-off-by: 703joko <703joko@gmail.com>
    Signed-off-by: Genkzsz11 <genkzsz11@gmail.com>
    Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5790,6 +5790,14 @@ public final class Settings {
           * @hide
           */
         public static final String STATUS_BAR_SHOW_BATTERY_PERCENT = "status_bar_show_battery_percent";
+		
+		/**
+         * Setting to determine whether or not to show the battery percentage in the qs status bar header.
+         *    0 - Show remaining time
+         *    1 - Show percentage
+         * @hide
+         */
+        public static final String DO_LEFT_BATTERY_TEXT = "do_left_battery_text";
 
         /**
          * Change quick settings tiles animation style
