e59b57ab50741951c250cc41c63487592296d7bc
SuperDroidBond <superdroidbond@yahoo.com>
2021-08-20 15:06:19 +0530

    [SQUASHED] System Charging Animation [1/2]
    SDB: Since ancient stone age we have been using that dot based circle animation.
         Change it to something new, something cool. Will add a few later.
         5 New Animation - Files taken from LottieFiles.com n 1 borrowed from MemeUI !!!
    
    ChargingAnimation : change both Wired & Wiresless Charging animation [1/2]
    
    ChargingAnimations: add a few more animations [1/2]
    
    ChargingAnimation : bring back the default one [1/2]
    Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6881,6 +6881,12 @@ public final class Settings {
          */
         public static final String LOCKSCREEN_CLOCK_ANIMATION_SIZE = "lockscreen_clock_animation_size";
 
+        /**
+         * Select which System charging animation style to display
+         * @hide
+         */
+        public static final String CHARGING_ANIMATION_STYLE = "charging_animation_style";
+
 	/**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
