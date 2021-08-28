3345cff7366216438a69e77e84ddd399aec2cf1d
Hưng Phan <phandinhhungvp2001@gmail.com>
2021-08-28 08:13:24 +0700

    Revert "SystemUI: Refresh QS panel clock"
    This reverts commit 6e9d931f3d37abdb9b41e537dd0d7e18d5b0a930.
    
    Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>

--- packages/SystemUI/src/com/android/systemui/qs/QuickStatusBarHeader.java
+++ packages/SystemUI/src/com/android/systemui/qs/QuickStatusBarHeader.java
@@ -577,7 +577,7 @@ public class QuickStatusBarHeader extends RelativeLayout implements
         if (mExpanded == expanded) return;
         mExpanded = expanded;
         mHeaderQsPanel.setExpanded(expanded);
-        mDateView.setVisibility(mClockView.isClockDateEnabled() ? View.GONE : View.VISIBLE);
+	mDateView.setVisibility(mClockView.isClockDateEnabled() ? View.INVISIBLE : View.VISIBLE);
         updateEverything();
         updateClockLocation();
         updateHeaderImage();
