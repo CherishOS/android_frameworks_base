f1c193d12593eb518372fffa46bcf972f1c392b6
LibXZR <i@xzr.moe>
2021-09-01 17:38:50 +0800

    Introduce Optimized Charge service
    This is a simple but useful feature that protects battery from
    overcharge. Many tests have confirmed that charging lithium batteries
    to 80% or more hurts their capabilities. Also this feature has
    implemented by many oems. Its time to have my own implement for it.
    
    Change-Id: I54175e2501a45bb66f95fe9f295fe1b822ede701
    Signed-off-by: LibXZR <i@xzr.moe>
    Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>

--- services/java/com/android/server/SystemServer.java
+++ services/java/com/android/server/SystemServer.java
@@ -209,6 +209,8 @@ import dalvik.system.VMRuntime;
 
 import com.google.android.startop.iorap.IorapForwardingService;
 
+import ink.kaleidoscope.server.OptimizedChargeService;
+
 import java.io.File;
 import java.io.FileDescriptor;
 import java.io.IOException;
@@ -382,7 +384,9 @@ public final class SystemServer implements Dumpable {
     private static final String MEDIA_COMMUNICATION_SERVICE_CLASS =
             "com.android.server.media.MediaCommunicationService";
     private static final String APP_COMPAT_OVERRIDES_SERVICE_CLASS =
-            "com.android.server.compat.overrides.AppCompatOverridesService$Lifecycle";
+            "com.android.server.compat.overrides.AppCompatOverridesService$Lifecycle";		
+    private static final String OPTIMIZED_CHARGE_SERVICE_CLASS =
+            "ink.kaleidoscope.server.OptimizedChargeService";
 
     private static final String ROLE_SERVICE_CLASS = "com.android.role.RoleService";
     private static final String GAME_MANAGER_SERVICE_CLASS =
@@ -2677,6 +2681,10 @@ public final class SystemServer implements Dumpable {
 
         t.traceBegin("AppCompatOverridesService");
         mSystemServiceManager.startService(APP_COMPAT_OVERRIDES_SERVICE_CLASS);
+        t.traceEnd();
+
+        t.traceBegin("StartOptimizedChargeService");
+        mSystemServiceManager.startService(OPTIMIZED_CHARGE_SERVICE_CLASS);
         t.traceEnd();
 
         // These are needed to propagate to the runnable below.
