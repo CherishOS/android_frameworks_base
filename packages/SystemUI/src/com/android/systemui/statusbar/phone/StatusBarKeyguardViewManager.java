745d71bd2e24bc8df92de8d26b3cd1f91e9106bf
Winson Chung <winsonc@google.com>
2021-09-28 11:45:46 -0700

    Don't factor animating-to-sleep for nav bar visibility
    - Instead, just animate the current nav bar view (which doesn't cause
      insets changes).  This should be safe to do because:
      1) Nothing else animates the alpha of this particular part of the
         view hierarchy
      2) Confirmed with SysUI folks that we always get onStartedWakingUp
         after onStartedGoingToSleep
      3) This otherwise reverts to the same nav bar visibility logic in R
    
    Bug: 196789451
    Bug: 190368572
    Test: atest StatusBarKeyguardViewManagerTest
    Test: In three button nav w/ AOD, turn off the screen
          - also try above with interrupting the screen off animation
          - also try above with power button instantly locks disabled
    Change-Id: I1be44a9a8dbb8cea17bbf354a2e03dd4bad5a4a2
    Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBarKeyguardViewManager.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBarKeyguardViewManager.java
@@ -196,7 +196,6 @@ public class StatusBarKeyguardViewManager implements RemoteInputController.Callb
     private boolean mLastGesturalNav;
     private boolean mLastIsDocked;
     private boolean mLastPulsing;
-    private boolean mLastAnimatedToSleep;
     private int mLastBiometricMode;
     private boolean mQsExpanded;
 
@@ -995,7 +994,6 @@ public class StatusBarKeyguardViewManager implements RemoteInputController.Callb
         mLastBiometricMode = mBiometricUnlockController.getMode();
         mLastGesturalNav = mGesturalNav;
         mLastIsDocked = mIsDocked;
-        mLastAnimatedToSleep = mAnimatedToSleep;
         mStatusBar.onKeyguardViewManagerStatesUpdated();
     }
 
@@ -1058,7 +1056,7 @@ public class StatusBarKeyguardViewManager implements RemoteInputController.Callb
         boolean hideWhileDozing = mLastDozing && mLastBiometricMode != MODE_WAKE_AND_UNLOCK_PULSING;
         boolean keyguardWithGestureNav = (keyguardShowing && !mLastDozing
                 || mLastPulsing && !mLastIsDocked) && mLastGesturalNav;
-        return (!mLastAnimatedToSleep && !keyguardShowing && !hideWhileDozing || mLastBouncerShowing
+        return (!keyguardShowing && !hideWhileDozing || mLastBouncerShowing
                 || mLastRemoteInputActive || keyguardWithGestureNav
                 || mLastGlobalActionsVisible);
     }
