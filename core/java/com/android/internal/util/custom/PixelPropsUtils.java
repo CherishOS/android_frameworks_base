80084e11e6a0c6c0f5de9acccaf6f6660fbe3c52
Maitreya29 <Maitreya29@users.noreply.github.com>
2021-10-20 06:58:54 +0000

    base: PixelPropsUtils: Add Material You redesigned apps to spoof list.
    * Allows us to do a lot of cool things including monet theme them with our inhouse color theming system.
    
    Signed-off-by: ugly-kid-af <dwarmachine24@gmail.com>
    Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>

--- core/java/com/android/internal/util/custom/PixelPropsUtils.java
+++ core/java/com/android/internal/util/custom/PixelPropsUtils.java
@@ -50,7 +50,17 @@ public class PixelPropsUtils {
             "com.netflix.mediaclient",
             "in.startv.hotstar",
      	    "com.google.android.apps.wallpaper",
-            "com.google.android.gm"
+            "com.google.android.contacts",
+	    "com.android.chrome",
+	    "com.google.android.apps.googleassistant",
+	    "com.google.android.deskclock",
+            "com.google.android.apps.youtube.music",
+            "com.google.android.apps.podcasts",
+            "com.google.android.apps.tachyon",
+            "com.google.android.apps.nbu.files",
+            "com.google.android.android.contacts",
+            "com.google.android.android.apps.messaging",
+	    "com.google.android.gm"
     };
 
     private static ArrayList<String> allProps = new ArrayList<>(Arrays.asList("BRAND", "MANUFACTURER", "DEVICE", "PRODUCT", "MODEL", "FINGERPRINT"));
