f2ecd6b218642f21097b0ebf4f65967faf1be057
Danny Lin <danny@kdrag0n.dev>
2021-10-25 19:32:33 -0700

    SystemUI: Allow Wi-Fi/cell tiles to co-exist with provider model
    The dedicated Wi-Fi/cellular data QS tiles are partially working now
    that we've exposed them in the list of tile options, but they're very
    buggy and somewhat broken when the provider model (unified internet) is
    enabled.
    
    Allow the tiles to co-exist with InternetTile and the provider model
    (including both settings_provider_model and combined signal icons) by
    always including QS icons in Wi-Fi/data indicator callbacks, and add
    a new flag to indicate whether they should be shown in the internet
    tile.
    
    Change-Id: I10af134b35dfabdb9275f1aca8ca8512e1db6d27
    Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/policy/NetworkController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/policy/NetworkController.java
@@ -58,10 +58,11 @@ public interface NetworkController extends CallbackController<SignalCallback>, D
         public String description;
         public boolean isTransient;
         public String statusLabel;
+        public boolean isDefault;
 
         public WifiIndicators(boolean enabled, IconState statusIcon, IconState qsIcon,
                 boolean activityIn, boolean activityOut, String description,
-                boolean isTransient, String statusLabel) {
+                boolean isTransient, String statusLabel, boolean isDefault) {
             this.enabled = enabled;
             this.statusIcon = statusIcon;
             this.qsIcon = qsIcon;
@@ -70,6 +71,7 @@ public interface NetworkController extends CallbackController<SignalCallback>, D
             this.description = description;
             this.isTransient = isTransient;
             this.statusLabel = statusLabel;
+            this.isDefault = isDefault;
         }
 
         @Override
@@ -83,6 +85,7 @@ public interface NetworkController extends CallbackController<SignalCallback>, D
                 .append(",description=").append(description)
                 .append(",isTransient=").append(isTransient)
                 .append(",statusLabel=").append(statusLabel)
+                .append(",isDefault=").append(isDefault)
                 .append(']').toString();
         }
     }
@@ -105,12 +108,13 @@ public interface NetworkController extends CallbackController<SignalCallback>, D
         public boolean roaming;
         public boolean showTriangle;
         public int volteId;
+        public boolean isDefault;
 
         public MobileDataIndicators(IconState statusIcon, IconState qsIcon, int statusType,
                 int qsType, boolean activityIn, boolean activityOut,
                 CharSequence typeContentDescription, CharSequence typeContentDescriptionHtml,
                 CharSequence description, boolean isWide, int subId, boolean roaming,
-                boolean showTriangle, int volteId) {
+                boolean showTriangle, int volteId, boolean isDefault) {
             this.statusIcon = statusIcon;
             this.qsIcon = qsIcon;
             this.statusType = statusType;
@@ -125,6 +129,7 @@ public interface NetworkController extends CallbackController<SignalCallback>, D
             this.roaming = roaming;
             this.showTriangle = showTriangle;
             this.volteId = volteId;
+            this.isDefault = isDefault;
         }
 
         @Override
@@ -144,6 +149,7 @@ public interface NetworkController extends CallbackController<SignalCallback>, D
                 .append(",roaming=").append(roaming)
                 .append(",showTriangle=").append(showTriangle)
                 .append(",volteId=").append(volteId)
+                .append(",isDefault=").append(isDefault)
                 .append(']').toString();
         }
     }
