65aac1a1538105f01ae00454fea7acf0b9701d3f
SagarMakhar <sagarmakhar@gmail.com>
2021-10-31 18:31:53 +0000

    Fixup: "Update keyguard indication margin when fod is in use"
    - Don't listen for biometric running state, an dummy udfps view with lock icon is still added when there are no fingerprints registered.
    
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>
    Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/KeyguardBottomAreaView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/KeyguardBottomAreaView.java
@@ -564,7 +564,7 @@ public class KeyguardBottomAreaView extends FrameLayout implements View.OnClickL
     }
 
     private boolean hasInDisplayFingerprint() {
-        return FodUtils.hasFodSupport(mContext) && mIsFingerprintRunning;
+        return FodUtils.hasFodSupport(mContext);
     }
 
     public boolean isLeftVoiceAssist() {
