5b8e0f70bf297a01c6095963dd38c258bfd7b35e
SKULSHADY <anushekprasal@gmail.com>
2021-11-06 11:14:33 +0000

    SystemUI: Add toggle to disable battery estimates in QS [1/2]
    Signed-off-by: chiru2000 <chiranthangadi123@gmail.com>
    Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5914,6 +5914,12 @@ public final class Settings {
          * @hide
          */
         public static final String LIVE_DISPLAY_HINTED = "live_display_hinted";
+		
+		/**
+         * Whether to show battery estimates in QS
+         * @hide
+         */
+        public static final String QS_SHOW_BATTERY_ESTIMATE = "qs_show_battery_estimate";
 
         /**
          * Keys we no longer back up under the current schema, but want to continue to
