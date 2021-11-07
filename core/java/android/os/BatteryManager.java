de6aa158b3b8fe1181956fd5f088f7c0fce0605c
Thecrazyskull <anaskarbila@gmail.com>
2021-11-07 09:56:19 +0700

    BatteryService: add dash charging support
    DennySPb: port to R
    Change-Id: I0515a3b6c643936a14b764530ffa58396beaadb8
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>
    Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>

--- core/java/android/os/BatteryManager.java
+++ core/java/android/os/BatteryManager.java
@@ -162,6 +162,13 @@ public class BatteryManager {
      */
     @SystemApi
     public static final String EXTRA_EVENT_TIMESTAMP = "android.os.extra.EVENT_TIMESTAMP";
+	
+	/**
+     * Extra for {@link android.content.Intent#ACTION_BATTERY_CHANGED}:
+     * boolean value to detect fast charging
+     * {@hide}
+     */
+    public static final String EXTRA_DASH_CHARGER = "dash_charger";
 
     /**
      * Extra for {@link android.content.Intent#ACTION_BATTERY_CHANGED}:
