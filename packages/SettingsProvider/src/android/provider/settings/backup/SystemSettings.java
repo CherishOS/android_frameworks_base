41aab3cf81546f1567c021c7ee28fa1bcd6fb0d6
Rishawn <70256146+Stealth1226@users.noreply.github.com>
2022-01-10 09:47:15 +0530

    base: allow disabling ripple effect on unlock [1/2]
        From: Nauval Rizky <enuma.alrizky@gmail.com>
        Date: Wed, 19 Jan 2022 08:31:56 +0700
        Subject: SystemUI: Fix method for disabling unlock ripple
         animation
    
        NotificationShadeWindowController's are not gracefully closed, which
        leads to top parts of the screen is being obscured by invisible shade window.
        And also, "ripple" is actually still exists due to lightRevealScrim are
        being started by onKeyguardFadingAwayChanged, which is not what user expect
        when they want to disable the effect.
    
        So in order to handle those cases, lets intercept it from the
        AuthRippleController instead.
    
        Signed-off-by: alanndz <alanndz7@gmail.com>
        Signed-off-by: Joey Huab <joey@evolution-x.org>
        Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    
    @/neobuddy89:
    * Reverse logic. Enabled animation by default.
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>

--- packages/SettingsProvider/src/android/provider/settings/backup/SystemSettings.java
+++ packages/SettingsProvider/src/android/provider/settings/backup/SystemSettings.java
@@ -90,5 +90,6 @@ public class SystemSettings {
         Settings.System.NAVIGATION_BAR_INVERSE,
         Settings.System.NETWORK_TRAFFIC_STATE,
         Settings.System.NETWORK_TRAFFIC_AUTOHIDE_THRESHOLD,
+        Settings.System.ENABLE_RIPPLE_EFFECT,
     };
 }
