d8c3b66fa2b5b9a6392aa0b896e59a5a602a29df
spkal01 <kalligeross@gmail.com>
2022-02-01 21:52:59 +0800

    SystemUI: Allow Configuring Navbar Radius [1/2]
    Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6084,6 +6084,12 @@ public final class Settings {
         public static final String SHOW_APP_VOLUME = "show_app_volume";
         
         /**
+         * Size of gesture bar radius.
+         * @hide
+         */
+        public static final String GESTURE_NAVBAR_RADIUS = "gesture_navbar_radius";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
