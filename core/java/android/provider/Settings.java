84c4129ca07f1d09f695484e14c533d2ffc4a9b4
travarilo <travarilo@gmail.com>
2022-02-25 20:32:59 +0100

    SystemUI: Toggle data usage view on QS footer [1/2]
    Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6207,6 +6207,12 @@ public final class Settings {
          * @hide
          */
         public static final String SHOW_QS_DATE = "show_qs_date";
+		
+		/**
+         * Whether to show data usage in qs footer
+         * @hide
+         */
+        public static final String QS_FOOTER_DATA_USAGE = "qs_footer_data_usage";
 
         /**
          * Keys we no longer back up under the current schema, but want to continue to
