5e84e254194ca4c0d7b140647f79da2ef9100160
Pranav Vashi <neobuddy89@gmail.com>
2022-04-16 10:38:36 +0530

    Fixes and improvements for Black Theme [1/2]
    * Add to SettingsObserver for observing toggle behavior.
    * Apply only with dark theme - looks awful on light mode.
    * Add configuration listener to reapply theme when dark mode changes.
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Mayur <ultramayur123@gmail.com>
    Signed-off-by: spezi77 <spezi7713@gmx.net>
    Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -11352,6 +11352,12 @@ public final class Settings {
         public static final String ENABLE_PROJECTION_PRIVACY_INDICATOR = "enable_projection_privacy_indicator";
 
         /**
+         * Whether to turn on black theme
+         * @hide
+         */
+        public static final String SYSTEM_BLACK_THEME = "system_black_theme";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
