86adec39475218c0a0154d9dbb7aa80d80cebc69
Michael W <baddaemon87@gmail.com>
2022-04-18 18:21:03 +0200

    SystemUI: Fix alarm icon
    * The icon was initially shown but not again after a reboot
    * Reason is, that the user setup isn't completed again
      and therefore the variable mCurrentUserSetup will never
      hold true again
    * Fix this by calling onUserSetupChanged once manually to
      get a proper initial state
    * Similar code exists in other places in SystemUI as well:
      NavigationBar.java, NetworkControllerImpl.java, ...
      -> Looks like this one was forgotten
    
    Change-Id: I02295a70a82696e7c0c6ade93c96e661140886f0
    Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBarPolicy.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBarPolicy.java
@@ -377,6 +377,9 @@ public class PhoneStatusBarPolicy
                 this::onConnectedDisplayAvailabilityChanged);
 
         mCommandQueue.addCallback(this);
+
+        // Get initial user setup state
+        onUserSetupChanged();
     }
 
     private String getManagedProfileAccessibilityString() {
