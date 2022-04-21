a1dd5758930cdaa26e4c860d99fe2025b619c3d2
spkal01 <kalligeross@gmail.com>
2022-04-21 15:09:02 +0300

    Settings Base Layout toggle
    Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5768,6 +5768,13 @@ public final class Settings {
         public static final String SETTINGS_DASHBOARD_STYLE = "settings_dashboard_style";
 
         /**
+         * Settings Layout
+         * @hide
+         */
+        public static final String ALT_SETTINGS_LAYOUT = "alt_settings_layout";
+
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
