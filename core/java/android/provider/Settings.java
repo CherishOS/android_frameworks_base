91b3fa82ca5e7a0b86e094ca525da64d069a2136
spkal01 <kalligeross@gmail.com>
2022-04-30 15:28:46 +0300

    SystemUI: Transparent QS customization
    Inspired by dotOS
    
    neobuddy89: Clean up unused code and convert to 1-100 scale.
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: spezi77 <spezi7713@gmx.net>
    Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6241,6 +6241,11 @@ public final class Settings {
         public static final String QS_HEADER_CLOCK_SIZE = "qs_header_clock_size";
 
         /**
+         * @hide
+         */
+        public static final String QS_TRANSPARENCY = "qs_transparency";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
