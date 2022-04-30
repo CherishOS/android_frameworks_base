dfd8cd6ebbd8c941b5aa1901484d86c8b1c82719
ariffjenong <arifbuditantodablekk@gmail.com>
2022-04-30 08:54:58 +0700

    SystemUI: fixup! Hide OOS clear all if panel visible because Headsup
    Signed-off-by: ariffjenong <arifbuditantodablekk@gmail.com>
    Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelViewController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelViewController.java
@@ -3018,7 +3018,7 @@ public class NotificationPanelViewController extends PanelViewController {
             alpha *= mClockPositionResult.clockAlpha;
         }
         mNotificationStackScrollLayoutController.setAlpha(alpha);
-        if (mBarState != StatusBarState.KEYGUARD && !isFullyCollapsed()) {
+        if (mBarState != StatusBarState.KEYGUARD && !isFullyCollapsed() || !isPanelVisibleBecauseOfHeadsUp()) {
             mStatusBar.updateDismissAllVisibility(true);
         }
     }
