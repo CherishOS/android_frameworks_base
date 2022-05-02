9d16c4527e3e0ee5b9390001dab2839de151f4ed
Hưng Phan <phandinhhungvp2001@gmail.com>
2022-05-02 19:04:05 +0700

    Revert "Initial import of lineagehw"
    This reverts commit e6cec3fa97541efbc2aca567fe749291a2089eb1.
    
    Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>

--- services/java/com/android/server/SystemServer.java
+++ services/java/com/android/server/SystemServer.java
@@ -225,9 +225,6 @@ import java.util.TreeSet;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.Future;
 
-// LineageHardware
-import com.android.server.custom.LineageHardwareService;
-
 /**
  * Entry point to {@code system_server}.
  */
@@ -2438,13 +2435,6 @@ public final class SystemServer implements Dumpable {
             t.traceBegin("StartMediaMetricsManager");
             mSystemServiceManager.startService(MediaMetricsManagerService.class);
             t.traceEnd();
-
-            // LineageHardware
-            if (!mOnlyCore){
-                t.traceBegin("StartLineageHardwareService");
-                mSystemServiceManager.startService(LineageHardwareService.class);
-                t.traceEnd();
-            }
         }
 
         if (!isWatch) {
