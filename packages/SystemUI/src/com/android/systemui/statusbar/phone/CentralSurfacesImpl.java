73aced28040734d1df1588eddaf98f386206e651
jhenrique09 <jhsv09@gmail.com>
2022-05-14 22:51:51 +0000

    Call setBrightness when user stops swiping on statusbar
    * This way brightness gets saved and changes are shown on quick settings brightness bar
    
    Change-Id: I5daf99d7c2c1bebf430902bdbfad9971142be93b
    Signed-off-by: Chenyang Zhong <zhongcy95@gmail.com>
    Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/CentralSurfacesImpl.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/CentralSurfacesImpl.java
@@ -577,6 +577,7 @@ public class CentralSurfacesImpl extends CoreStartable implements
     private boolean mBrightnessControl;
     private boolean mBrightnessChanged;
     private boolean mJustPeeked;
+    private float mCurrentBrightness;
 
     // Flags for disabling the status bar
     // Two variables because the first one evidently ran out of room for new flags.
@@ -2194,6 +2195,7 @@ public class CentralSurfacesImpl extends CoreStartable implements
         final float val = convertGammaToLinearFloat(
                 Math.round(value * GAMMA_SPACE_MAX),
                 mMinimumBacklight, mMaximumBacklight);
+        mCurrentBrightness = val;
         mDisplayManager.setTemporaryBrightness(mDisplayId, val);
         AsyncTask.execute(() -> {
             Settings.System.putFloatForUser(mContext.getContentResolver(),
@@ -2300,6 +2302,7 @@ public class CentralSurfacesImpl extends CoreStartable implements
             if (mJustPeeked && mExpandedVisible) {
                 mNotificationPanelViewController.fling(10, false);
             }
+            mDisplayManager.setBrightness(mDisplayId, mCurrentBrightness);
         }
     }
 
