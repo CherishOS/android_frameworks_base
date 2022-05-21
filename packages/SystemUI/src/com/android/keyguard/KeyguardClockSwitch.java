d461a3a05bba03bdf803e5ef4fac49e2995d5c5f
Adithya R <gh0strider.2k18.reborn@gmail.com>
2022-05-21 17:52:09 +0530

    SystemUI: Set lockscreen clock font dynamically
    Change-Id: I834f3436dbd89c4019d84eb9aff76a115fb32676
    Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>

--- packages/SystemUI/src/com/android/keyguard/KeyguardClockSwitch.java
+++ packages/SystemUI/src/com/android/keyguard/KeyguardClockSwitch.java
@@ -8,6 +8,7 @@ import android.content.Context;
 import android.content.res.Configuration;
 import android.graphics.Paint;
 import android.graphics.Paint.Style;
+import android.graphics.Typeface;
 import android.util.AttributeSet;
 import android.database.ContentObserver;
 import android.graphics.Paint;
@@ -176,6 +177,15 @@ public class KeyguardClockSwitch extends RelativeLayout {
                 R.dimen.keyguard_smartspace_top_offset);
     }
 
+    public void onThemeChanged() {
+        String font = mContext.getString(com.android.internal.R.string.config_headlineFontFamily);
+        Typeface tf = font.equals("google-sans")
+                ? mContext.getResources().getFont(R.font.clock)
+                : Typeface.create(font, Typeface.NORMAL);
+        mClockView.setTypeface(tf);
+        mLargeClockView.setTypeface(tf);
+    }
+
     /**
      * Returns if this view is presenting a custom clock, or the default implementation.
      */
@@ -194,6 +204,7 @@ public class KeyguardClockSwitch extends RelativeLayout {
         mStatusArea = findViewById(R.id.keyguard_status_area);
 
         onDensityOrFontScaleChanged();
+        onThemeChanged();
     }
 
     void setClockPlugin(ClockPlugin plugin, int statusBarState) {
