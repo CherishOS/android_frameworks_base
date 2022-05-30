1df54896898f7138aa8a34813d42a85815b6e58b
minarypenguin <alexfinhart@gmail.com>
2022-05-30 11:26:29 +0800

    VolumePanel: Fix expand animation for left volume panel
    Signed-off-by: minarypenguin <alexfinhart@gmail.com>
    Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>

--- packages/SystemUI/src/com/android/systemui/volume/VolumeDialogImpl.java
+++ packages/SystemUI/src/com/android/systemui/volume/VolumeDialogImpl.java
@@ -1290,10 +1290,14 @@ public class VolumeDialogImpl implements VolumeDialog,
             mExpandRows.setOnClickListener(v -> {
                 mExpanded = !mExpanded;
                 updateRowsH(mDefaultRow, true);
-                if (!mExpanded) {
+                if (!mExpanded && !mVolumePanelOnLeft) {
                     rotateIcon();
-                } else {
+                } else if (!mExpanded && mVolumePanelOnLeft) {
                     rotateIconReverse();
+                } else if (mExpanded && mVolumePanelOnLeft) {
+                   rotateIcon();
+                } else {
+                   rotateIconReverse();
                 }
             });
         }
