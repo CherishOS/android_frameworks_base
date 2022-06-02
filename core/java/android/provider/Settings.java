31abee2eab2e76c2235fa9e5152d791f2ef71c6d
cjh1249131356 <cjh1249131356@gmail.com>
2022-06-03 01:13:40 +0800

    base: Rework lock gesture feature
    Match OEM behavior:
    - Lock swipe gestures from bottom, left & right side
    - Require double swipe to unlock gestures
    - Auto relock gestures after intervals
    - Avoid showing NavigationBar / TaskBar when swiping from status bar / left & right side
    
    Change-Id: I58b5362e19aef6f134b14ef8427bbf35d3dbbd57
    Signed-off-by: cjh1249131356 <cjh1249131356@gmail.com>
    [YumeMichi: Port to 14]
    Signed-off-by: Yuan Si <do4suki@gmail.com>
    Signed-off-by: Dmitrii <bankersenator@gmail.com>
    Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6799,6 +6799,12 @@ public final class Settings {
          */
         public static final String QS_HEADER_IMAGE = "qs_header_image";
 
+	/**
+         * Current status of whether gestures are locked
+         * @hide
+         */
+        public static final String LOCK_GESTURE_STATUS = "lock_gesture_status";
+
         /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
