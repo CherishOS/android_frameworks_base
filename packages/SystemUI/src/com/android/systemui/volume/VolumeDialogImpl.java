53a84ce054d775ac5253f0170e2edd6aa484f55b
Arian <arian.kulmer@web.de>
2022-06-17 13:46:42 +0200

    Revert "VolumeDialogImpl: Fix cuttered layout when on setup or lock task mode"
    This reverts commit e6fbafa82c32385cc721875d2c48fcf1f5d2cb7d.
    
    Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>

--- packages/SystemUI/src/com/android/systemui/volume/VolumeDialogImpl.java
+++ packages/SystemUI/src/com/android/systemui/volume/VolumeDialogImpl.java
@@ -304,8 +304,6 @@ public class VolumeDialogImpl implements VolumeDialog,
     // Variable to track the default row with which the panel is initially shown
     private VolumeRow mDefaultRow = null;
 
-    private FrameLayout mRoundedBorderBottom;
-
     // Volume panel expand state
     private boolean mExpanded;
 
@@ -640,8 +638,6 @@ public class VolumeDialogImpl implements VolumeDialog,
         mSettingsView = mDialog.findViewById(R.id.settings_container);
         mSettingsViewSpacer = mDialog.findViewById(R.id.settings_container_spacer);
         mSettingsIcon = mDialog.findViewById(R.id.settings);
-
-        mRoundedBorderBottom = mDialog.findViewById(R.id.rounded_border_bottom);
 
         mExpandRowsView = mDialog.findViewById(R.id.expandable_indicator_container);
         mExpandRows = mDialog.findViewById(R.id.expandable_indicator);
@@ -1241,20 +1237,16 @@ public class VolumeDialogImpl implements VolumeDialog,
     }
 
     private void initSettingsH(int lockTaskModeState) {
-        if (mRoundedBorderBottom != null){
-            mRoundedBorderBottom.setVisibility(!mDeviceProvisionedController.isCurrentUserSetup() ||
-                    mActivityManager.getLockTaskModeState() != LOCK_TASK_MODE_NONE
-                    ? VISIBLE : GONE);
-            mSettingsViewSpacer.setVisibility(mDeviceProvisionedController.isCurrentUserSetup()
+        if (mSettingsView != null) {
+            mSettingsView.setVisibility(mDeviceProvisionedController.isCurrentUserSetup()
                     && lockTaskModeState == LOCK_TASK_MODE_NONE
                     && isBluetoothA2dpConnected()
                     ? VISIBLE : GONE);
-        }
-        if (mSettingsView != null) {
-            mSettingsView.setVisibility(
-                    mDeviceProvisionedController.isCurrentUserSetup() &&
-                            lockTaskModeState == LOCK_TASK_MODE_NONE && isBluetoothA2dpConnected() ? VISIBLE : GONE);
-        }
+			mSettingsViewSpacer.setVisibility(mDeviceProvisionedController.isCurrentUserSetup()
+                    && lockTaskModeState == LOCK_TASK_MODE_NONE
+                    && isBluetoothA2dpConnected()
+                    ? VISIBLE : GONE);
+					}
         if (mSettingsIcon != null) {
             mSettingsIcon.setOnClickListener(v -> {
                 Events.writeEvent(Events.EVENT_SETTINGS_CLICK);
