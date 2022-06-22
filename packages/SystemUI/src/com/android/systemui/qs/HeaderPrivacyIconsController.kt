85bb98de1ee7b4f36048b1c9969321084d00d93f
Pranav Vashi <neobuddy89@gmail.com>
2022-06-23 00:59:00 +0530

    SystemUI: Fix few issues with toggling privacy indicators
    * Fixes camera/mic toggle not working.
    * Fixes location indicator stuck in rare instances.
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>

--- packages/SystemUI/src/com/android/systemui/qs/HeaderPrivacyIconsController.kt
+++ packages/SystemUI/src/com/android/systemui/qs/HeaderPrivacyIconsController.kt
@@ -79,15 +79,16 @@ class HeaderPrivacyIconsController @Inject constructor(
             uiEventLogger.log(PrivacyChipEvent.ONGOING_INDICATORS_CHIP_CLICK)
             privacyDialogController.showDialog(privacyChip.context)
         }
-        setChipVisibility(privacyChip.visibility == View.VISIBLE)
         micCameraIndicatorsEnabled = privacyItemController.micCameraAvailable
         locationIndicatorsEnabled = privacyItemController.locationAvailable
+        setChipVisibility(privacyChip.visibility == View.VISIBLE)
 
         // Ignore privacy icons because they show in the space above QQS
         updatePrivacyIconSlots()
     }
 
     fun onParentInvisible() {
+        setChipVisibility(false)
         chipVisibilityListener = null
         privacyChip.setOnClickListener(null)
     }
@@ -104,6 +105,7 @@ class HeaderPrivacyIconsController @Inject constructor(
         listening = false
         privacyItemController.removeCallback(picCallback)
         privacyChipLogged = false
+        setChipVisibility(false)
     }
 
     private fun setChipVisibility(visible: Boolean) {
