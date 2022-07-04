9546952d076f47b9e2c1b36c3fee7a1c144fa33a
timjosten <timjosten@gmail.com>
2022-07-04 12:18:25 +0900

    Battery Styles: Do not apply SB battery meter style changes for QS battery meter
    Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>

--- packages/SystemUI/src/com/android/systemui/qs/QuickStatusBarHeader.java
+++ packages/SystemUI/src/com/android/systemui/qs/QuickStatusBarHeader.java
@@ -176,6 +176,7 @@ public class QuickStatusBarHeader extends FrameLayout implements TunerService.Tu
         mDatePrivacySeparator = findViewById(R.id.space);
         // Tint for the battery icons are handled in setupHost()
         mBatteryRemainingIcon = findViewById(R.id.batteryRemainingIcon);
+        mBatteryRemainingIcon.mQS = true;
 
         mBatteryIcon = findViewById(R.id.batteryIcon);
 
