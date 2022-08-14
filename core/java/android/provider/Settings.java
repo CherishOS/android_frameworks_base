ad0a12aa710b8516fa1a8820a161a0e5efb88128
strwbrry143 <alexfinhart@gmail.com>
2022-08-14 10:28:16 +0800

    Lockscreen Album Filter: Make LS media filter blur radius configurable [1/2]
    * this is only the customization left that is using renderscript, since we aren't using gaussian type of blur now, i believe renderscript can now handle up to 25f blur radius, taking consideration that media notification is now using rendereffect
    
    Signed-off-by: strwbrry143 <alexfinhart@gmail.com>
    Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5730,6 +5730,12 @@ public final class Settings {
         public static final String LOCKSCREEN_ALBUMART_FILTER = "lockscreen_albumart_filter";
 
         /**
+         * Lockscreen album art blur level
+         * @hide
+         */
+        public static final String LS_MEDIA_FILTER_BLUR_RADIUS = "ls_media_filter_blur_radius";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
