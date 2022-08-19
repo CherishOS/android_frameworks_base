87981e59badbcd32c7a471ce2878af3a9ea5fdaa
Joey Huab <joey@evolution-x.org>
2022-08-19 18:18:21 +0000

    SystemUI: Adapt some changes for older QS tiles
    Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>

--- packages/SystemUI/src/com/android/systemui/qs/tiles/PowerShareTile.java
+++ packages/SystemUI/src/com/android/systemui/qs/tiles/PowerShareTile.java
@@ -187,10 +187,6 @@ public class PowerShareTile extends QSTileImpl<QSTile.BooleanState>
             return;
         }
 
-        if (state.slash == null) {
-            state.slash = new SlashState();
-        }
-
         state.icon = ResourceIcon.get(R.drawable.ic_qs_powershare);
         try {
             state.value = mPowerShare.isEnabled();
@@ -198,7 +194,6 @@ public class PowerShareTile extends QSTileImpl<QSTile.BooleanState>
             state.value = false;
             ex.printStackTrace();
         }
-        state.slash.isSlashed = state.value;
         state.label = mContext.getString(R.string.quick_settings_powershare_label);
 
         if (mBatteryController.isPowerSave() || getBatteryLevel() < getMinBatteryLevel()) {
