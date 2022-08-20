788dec7fc117b022665ae597b4957373ecea4d15
Dmitry Muhomor <muhomor.dmitry@gmail.com>
2022-08-20 15:18:23 +0300

    SystemUI: update PixelUdfpsHbmProvider for Android 13

--- packages/SystemUI/src/com/android/systemui/biometrics/PixelUdfpsHbmProvider.kt
+++ packages/SystemUI/src/com/android/systemui/biometrics/PixelUdfpsHbmProvider.kt
@@ -22,8 +22,9 @@ import android.hardware.display.DisplayManager
 import android.os.Handler
 import android.os.IBinder
 import android.os.ServiceManager
-import android.view.Surface
 import com.android.systemui.biometrics.AuthController
+import com.android.systemui.biometrics.UdfpsHbmProvider
+import com.android.systemui.dagger.SysUISingleton
 import com.android.systemui.dagger.qualifiers.DisplayId
 import com.android.systemui.dagger.qualifiers.Main
 import com.android.systemui.dagger.qualifiers.UiBackground
@@ -62,11 +63,7 @@ class PixelUdfpsHbmProvider constructor(
         displayManager.registerDisplayListener(this, handler)
     }
 
-    override fun enableHbm(hbmType: Int, surface: Surface?, onHbmEnabled: Runnable?) {
-        if (hbmType != HBM_TYPE) {
-            return
-        }
-
+    override fun enableHbm(halControlsIllumination: Boolean, onHbmEnabled: Runnable?) {
         // Run the callback and skip enabling if already enabled
         // (otherwise it may fail, similar to disabling)
         if (displayHal.getLhbmState()) {
@@ -78,7 +75,7 @@ class PixelUdfpsHbmProvider constructor(
         bgExecutor.execute {
             // Request HbmSVManager to lock the refresh rate. On the Pixel 6 Pro (raven), LHBM only
             // works at peak refresh rate.
-            authController.udfpsHbmListener?.onHbmEnabled(hbmType, displayId)
+            authController.udfpsHbmListener?.onHbmEnabled(displayId)
 
             if (currentRefreshRate == peakRefreshRate) {
                 // Enable immediately if refresh rate is correct
@@ -123,7 +120,7 @@ class PixelUdfpsHbmProvider constructor(
         bgExecutor.execute {
             displayHal?.setLhbmState(false)
             // Unlock refresh rate
-            handler.post { authController.udfpsHbmListener?.onHbmDisabled(HBM_TYPE, displayId) }
+            handler.post { authController.udfpsHbmListener?.onHbmDisabled(displayId) }
 
             onHbmDisabled?.let { handler.post(it) }
         }
@@ -145,8 +142,5 @@ class PixelUdfpsHbmProvider constructor(
     companion object {
         // Descriptor for Pixel display HAL's AIDL service
         private const val PIXEL_DISPLAY_HAL = "com.google.hardware.pixel.display.IDisplay/default"
-
-        // For simplicity, we only support local HBM as that's the only mode used in production
-        private const val HBM_TYPE = UdfpsHbmTypes.LOCAL_HBM
     }
 }
