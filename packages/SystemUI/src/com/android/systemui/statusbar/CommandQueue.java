50f3d9dde242550620bb498bca69f9523ae27f82
jhonboy121 <alfredmathew05@gmail.com>
2022-08-27 12:30:58 +0000

    fixup! base: Add some static custom utilities
    [jhonboy121]: adapt to A12.1 and A13, simplify some code
    
    Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/CommandQueue.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/CommandQueue.java
@@ -1332,7 +1332,9 @@ public class CommandQueue extends IStatusBar.Stub implements
     @Override
     public void toggleCameraFlash() {
         synchronized (mLock) {
-            mHandler.removeMessages(MSG_TOGGLE_CAMERA_FLASH);
+            if (mHandler.hasMessages(MSG_TOGGLE_CAMERA_FLASH)) {
+                mHandler.removeMessages(MSG_TOGGLE_CAMERA_FLASH);
+            }
             mHandler.sendEmptyMessage(MSG_TOGGLE_CAMERA_FLASH);
         }
     }
@@ -1788,9 +1790,7 @@ public class CommandQueue extends IStatusBar.Stub implements
                     }
                     break;
                 case MSG_TOGGLE_CAMERA_FLASH:
-                    for (int i = 0; i < mCallbacks.size(); i++) {
-                        mCallbacks.get(i).toggleCameraFlash();
-                    }
+                    mCallbacks.forEach(cb -> cb.toggleCameraFlash());
                     break;
             }
         }
