ac3b1ecff01bfee6e9b01a011d19ee39d311df13
str143 <alexfinhart@gmail.com>
2022-08-30 09:52:59 +0800

    Volume Media output dialog when stream is active toggle [1/2]
    Signed-off-by: str143 <alexfinhart@gmail.com>
    Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6415,6 +6415,11 @@ public final class Settings {
          * @hide
          */
         public static final String POWER_MENU_ANIMATIONS = "power_menu_animations";
+        
+	/**
+         * @hide
+         */
+        public static final String VOLUME_MEDIA_OUTPUT_TOGGLE = "volume_media_output_toggle";
 
         /**
          * Keys we no longer back up under the current schema, but want to continue to
