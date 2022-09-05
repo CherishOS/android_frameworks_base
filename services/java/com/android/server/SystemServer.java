3a3ed0f875fc31a3a2e3b61c20d6d62b6a700962
stallix <joey@evolution-x.org>
2022-09-05 20:26:15 +0700

    [SQUASH] Clean up previous lineagehw import Revert "base: Add Touch HIDL support"
    This reverts commit d869d73997f4593e1d7c1203aad65bd6acbb5872.
    
    Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>
    
    Revert "Initial import of lineagehw"
    
    This reverts commit f8172d2ba3a0d4e85d6ba1bac7ddee71f0bf0ea2.
    
    Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>

--- services/java/com/android/server/SystemServer.java
+++ services/java/com/android/server/SystemServer.java
@@ -233,9 +233,6 @@ import java.util.TreeSet;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.Future;
 
-// LineageHardware
-import com.android.server.cherish.LineageHardwareService;
-
 /**
  * Entry point to {@code system_server}.
  */
@@ -2528,19 +2525,12 @@ public final class SystemServer implements Dumpable {
             mSystemServiceManager.startService(MediaMetricsManagerService.class);
             t.traceEnd();
 
-            // LineageHardware
-            if (!mOnlyCore){
-                t.traceBegin("StartLineageHardwareService");
-                mSystemServiceManager.startService(LineageHardwareService.class);
-                t.traceEnd();
-            }
             if (!context.getResources().getString(
                     com.android.internal.R.string.config_pocketBridgeSysfsInpocket).isEmpty()) {
                 t.traceBegin("StartPocketBridgeService");
                 mSystemServiceManager.startService(PocketBridgeService.class);
                 t.traceEnd();
             }
-
         }
 
         t.traceBegin("StartMediaProjectionManager");
