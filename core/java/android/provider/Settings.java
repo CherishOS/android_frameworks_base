a579ad74727b38b7dc0c8234adcb10f900475619
dankoman30 <dankoman30@gmail.com>
2022-09-07 10:27:13 +0700

    Add kill button to notification guts [1/2]
    neobuddy89:
    * Use direct strings wherever possible
    * Add FORCE_CLOSE priv-app permission
    * Remove unused strings
    
    SDB:Pie Bringup
    
    [SpiritCroc]
    - Make it a setting
    - Don't allow killing apps when device is locked
    - Bring it to Oreo
    
    SagarMakhar: Convert TextView to ImageButton
    
    Change-Id: Id2c2f3259ae2ff45ab28288375bc14148a0e9f1e
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6076,6 +6076,13 @@ public final class Settings {
          * @hide
          */
         public static final String NAVIGATION_BAR_IME_SPACE = "navigation_bar_ime_space";
+		
+		/**
+         * Whether to show the kill app button in notification guts
+         * @hide
+         */
+        public static final String NOTIFICATION_GUTS_KILL_APP_BUTTON =
+                "notification_guts_kill_app_button";
 
         /**
          * Keys we no longer back up under the current schema, but want to continue to
