0c45541179a382d75cc319e8b9162c10c43af82b
Konsta <konsta09@gmail.com>
2022-09-11 04:50:30 +0000

    fixup! InputMethodService: Volume key cursor control (1/2)
    This feature is moved to framework so it also works
    with third party keyboards.
    
    Change-Id: I8e20240e7bee5351ab20bb3d701eb95a5fd3e112
    Signed-off-by: Jason Edson <jaysonedson@gmail.com>
    Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>

--- packages/SettingsProvider/src/android/provider/settings/backup/SystemSettings.java
+++ packages/SettingsProvider/src/android/provider/settings/backup/SystemSettings.java
@@ -97,5 +97,6 @@ public class SystemSettings {
         Settings.System.CUSTOM_RINGTONE_VIBRATION_PATTERN,
         Settings.System.NOTIFICATION_VIBRATION_PATTERN,
         Settings.System.CUSTOM_NOTIFICATION_VIBRATION_PATTERN,
+        Settings.System.VOLUME_KEY_CURSOR_CONTROL,
     };
 }
