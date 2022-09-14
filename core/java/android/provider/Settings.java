d27b7a47b110966988de0ad7f2e0c286a346b28d
Hưng Phan <phandinhhungvp2001@gmail.com>
2022-09-14 07:47:09 +0700

    Revert "base: Forward Port Volume Long Press Skip Tracks [1/2]"
    This reverts commit 157df46b89d2cc7bc6f6181baa85711c9ada199b.
    
    Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5631,12 +5631,6 @@ public final class Settings {
         public static final String ALERT_SLIDER_NOTIFICATIONS = "alert_slider_notifications";
 		
 		/**
-         * Whether or not volume button music controls should be enabled to seek media tracks
-         * @hide
-         */
-        public static final String VOLBTN_MUSIC_CONTROLS = "volbtn_music_controls";
-		
-		/**
          * Whether to allow one finger quick settings expansion on the side of the statusbar.
          * 0 = 0ff, 1 = right, 2 = left, 3 = both
          * @hide
