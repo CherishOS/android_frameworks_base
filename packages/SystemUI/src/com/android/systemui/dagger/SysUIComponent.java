de262c6a757b0d5e669fe7e8b4a8a8c9de06f304
Hưng Phan <phandinhhungvp2001@gmail.com>
2022-09-16 23:22:11 +0700

    Revert "SystemUI: Forward-port smartspace to Android 13"
    This reverts commit 5a7603bd0071e21830c8f13582d4a83d6cdc1caa.
    
    Revert "SystemUI: Implement Reverse-engineered Smartspace"
    
    This reverts commit b703355189be5df6d05d45b8d76c6a00e32ab902.
    
    Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>

--- packages/SystemUI/src/com/android/systemui/dagger/SysUIComponent.java
+++ packages/SystemUI/src/com/android/systemui/dagger/SysUIComponent.java
@@ -54,8 +54,6 @@ import com.android.wm.shell.startingsurface.StartingSurface;
 import com.android.wm.shell.tasksurfacehelper.TaskSurfaceHelper;
 import com.android.wm.shell.transition.ShellTransitions;
 
-import com.google.android.systemui.smartspace.KeyguardSmartspaceController;
-
 import java.util.Map;
 import java.util.Optional;
 
@@ -261,10 +259,4 @@ public interface SysUIComponent {
      * Member injection into the supplied argument.
      */
     void inject(PeopleProvider peopleProvider);
-
-    /**
-     * Creates a KeyguardSmartspaceController.
-     */
-    @SysUISingleton
-    KeyguardSmartspaceController createKeyguardSmartspaceController();
 }
