7220f519da1f5e7d175479b0bbf2f1c2b2415ef0
minaripenguin37 <alexfinhart@gmail.com>
2022-10-10 00:11:06 +0800

    SystemUI: Improve volume panel layout
    Signed-off-by: minaripenguin37 <alexfinhart@gmail.com>
    Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>

--- packages/SystemUI/src/com/android/systemui/volume/VolumeDialogImpl.java
+++ packages/SystemUI/src/com/android/systemui/volume/VolumeDialogImpl.java
@@ -266,6 +266,7 @@ public class VolumeDialogImpl implements VolumeDialog,
     private View mExpandRowsView;
     private View mAppVolumeView;
     private ImageButton mAppVolumeIcon;
+    private View mAppVolumeSpacer;
     private FrameLayout mZenIcon;
     private final List<VolumeRow> mRows = new ArrayList<>();
     private ConfigurableTexts mConfigurableTexts;
@@ -684,6 +685,7 @@ public class VolumeDialogImpl implements VolumeDialog,
 
         mAppVolumeView = mDialog.findViewById(R.id.app_volume_container);
         mAppVolumeIcon = mDialog.findViewById(R.id.app_volume);
+        mAppVolumeSpacer = mDialog.findViewById(R.id.app_volume_container_spacer);
 
         if (mRows.isEmpty()) {
             if (!AudioSystem.isSingleVolume(mContext)) {
@@ -1344,6 +1346,7 @@ public class VolumeDialogImpl implements VolumeDialog,
     public void initAppVolumeH() {
         if (mAppVolumeView != null) {
             mAppVolumeView.setVisibility(shouldShowAppVolume() ? VISIBLE : GONE);
+            mAppVolumeSpacer.setVisibility(shouldShowAppVolume() ? VISIBLE : GONE);
         }
         if (mAppVolumeIcon != null) {
             mAppVolumeIcon.setOnClickListener(v -> {
