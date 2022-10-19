b0bc6ee45a2e42c28866c6f5e4cc79793141c01f
huwenbin1 <huwenbin1@xiaomi.corp-partner.google.com>
2022-10-19 23:00:31 +0800

    [DPC] Reduce Sreen Refresh Rate when Dim for DisplayPowerController
    when the mobile phone is unlocked and it is not used by the user for a long
    time, the system will automatically enter 'dim' state.
    Normally, the users do not used phones in this circumstance.
    However, the screen still keeps a high refresh rate.
    It seems unnecessary to offer such high refresh rate there.
    
    XM suggests that the refresh rate could be set at ane lower level when dim to
    reduce the phone's power comsumption.
    
    We expect that when the screen is dim detected by using PowerManagerService.
    The system could automatically reduce the screen refresh rate to 60Hz.
    
    Signed-off-by: Wenbin Hu<huwenbin1@xiaomi.corp-partner.google.com>
    Change-Id: Id3ed27a603e300cddf585824b75e81488b30d957
    Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>

--- services/core/java/com/android/server/display/DisplayPowerController.java
+++ services/core/java/com/android/server/display/DisplayPowerController.java
@@ -42,8 +42,10 @@ import android.os.Handler;
 import android.os.IBinder;
 import android.os.Looper;
 import android.os.Message;
+import android.os.Parcel;
 import android.os.PowerManager;
 import android.os.RemoteException;
+import android.os.ServiceManager;
 import android.os.SystemClock;
 import android.os.SystemProperties;
 import android.os.Trace;
@@ -501,6 +503,12 @@ final class DisplayPowerController implements AutomaticBrightnessController.Call
     private final String mSuspendBlockerIdProxNegative;
     private final String mSuspendBlockerIdProxDebounce;
 
+    // MIUI ADD: START
+    // For SurfaceFlinger to calculate alpha.
+    private IBinder mISurfaceFlinger;
+    private static final int TRANSACTION_NOTIFY_DIM = 31107;
+    // END
+
     /**
      * Creates the display power controller.
      */
@@ -545,6 +553,9 @@ final class DisplayPowerController implements AutomaticBrightnessController.Call
         mAutoBrightnessOneShot = getAutoBrightnessOneShotSetting();
 
         PowerManager pm = context.getSystemService(PowerManager.class);
+
+        // MIUI ADD: START
+        mISurfaceFlinger = ServiceManager.getService("SurfaceFlinger");
 
         final Resources resources = context.getResources();
 
@@ -775,6 +786,13 @@ final class DisplayPowerController implements AutomaticBrightnessController.Call
                 if (!mPendingRequestChangedLocked) {
                     mPendingRequestChangedLocked = true;
                     sendUpdatePowerStateLocked();
+                    // MIUI ADD: START
+                    if(DisplayPowerRequest.POLICY_DIM == request.policy){
+                        sendDimStateToSurfaceFlinger(true);
+                    }else{
+                        sendDimStateToSurfaceFlinger(false);
+                    }
+                    // END
                 }
             }
 
@@ -3219,4 +3237,28 @@ final class DisplayPowerController implements AutomaticBrightnessController.Call
             return false;
         }
     }
+
+    // MIUI ADD: START
+    public void sendDimStateToSurfaceFlinger(boolean isDim) {
+        if (DEBUG) {
+            Slog.d(TAG, "sendDimStateToSurfaceFlinger is dim "+isDim);
+        }
+        if (mISurfaceFlinger != null) {
+            final Parcel data = Parcel.obtain();
+            data.writeInterfaceToken("android.ui.ISurfaceComposer");
+            int val = isDim ? 1:0;
+            data.writeInt(val);
+            data.writeInt(262);
+            data.writeString("system");
+            try {
+                mISurfaceFlinger.transact(TRANSACTION_NOTIFY_DIM, data,
+                        null, IBinder.FLAG_ONEWAY);
+            } catch (RemoteException | SecurityException ex) {
+                Slog.e(TAG, "Failed to send brightness to SurfaceFlinger", ex);
+            } finally {
+                data.recycle();
+            }
+        }
+    }
+    // END
 }
