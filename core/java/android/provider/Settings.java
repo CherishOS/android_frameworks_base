148646f49887fba16434c6cc6c81e232db77cf43
minaripenguin37 <alexfinhart@gmail.com>
2022-11-01 19:40:11 +0800

    base: Make IME space optional
    Signed-off-by: minaripenguin37 <alexfinhart@gmail.com>
    Co-authored-by: hydrangea07 <104114625+hydrangea07@users.noreply.github.com>
    Signed-off-by: hydrangea07 <104114625+hydrangea07@users.noreply.github.com>
    Signed-off-by: DennySPb <dennyspb@gmail.com>
    Change-Id: I6f1fef4c3795e83934cd51936fb7129bfafd3a11
    Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6633,6 +6633,13 @@ public final class Settings {
          * @hide
          */
         public static final String RECENTS_LOCKED_TASKS = "recents_locked_tasks";
+		
+		
+		/**
+         * Whether to show IME space
+         * @hide
+         */
+        public static final String HIDE_IME_SPACE_ENABLE = "hide_ime_space_enable";
 
         /**
          * Keys we no longer back up under the current schema, but want to continue to
