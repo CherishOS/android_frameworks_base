27d3ee9c0b39c05085dcd3b52f162ae98857df56
minaripenguin37 <alexfinhart@gmail.com>
2022-11-12 20:56:37 +0800

    SystemUI: Perform soft reboot when enabling combined signal icons
    * most systemui restart api's were randomly causing security exception even AOSP's [1] and [2] as per testing.
    * use AOSP's way for soft reboot [3] for a faster way of reloading combined signal icons
    
    [1] 59d86ed2a80364aa27541d8117ccf80551b45e20
    [2] https://github.com/RiceDroid/android_frameworks_base/blob/8b30be833157cb2ba75b8b48b6d986d47521283a/packages/SystemUI/src/com/android/systemui/flags/FeatureFlagsDebug.java#L269
    [3] https://github.com/RiceDroid/android_frameworks_base/blob/8b30be833157cb2ba75b8b48b6d986d47521283a/packages/SystemUI/src/com/android/systemui/flags/FeatureFlagsDebug.java#L279
    
    Signed-off-by: minaripenguin37 <alexfinhart@gmail.com>
    Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>

--- packages/SystemUI/src/com/android/systemui/theme/ThemeOverlayController.java
+++ packages/SystemUI/src/com/android/systemui/theme/ThemeOverlayController.java
@@ -43,6 +43,7 @@ import android.database.ContentObserver;
 import android.graphics.Color;
 import android.net.Uri;
 import android.os.Handler;
+import android.os.RemoteException;
 import android.os.UserHandle;
 import android.os.UserManager;
 import android.provider.Settings;
@@ -58,6 +59,7 @@ import androidx.annotation.NonNull;
 import androidx.annotation.VisibleForTesting;
 
 import com.android.internal.graphics.ColorUtils;
+import com.android.internal.statusbar.IStatusBarService;
 import com.android.systemui.CoreStartable;
 import com.android.systemui.Dumpable;
 import com.android.systemui.broadcast.BroadcastDispatcher;
@@ -120,6 +122,7 @@ public class ThemeOverlayController extends CoreStartable implements Dumpable {
     private final UserTracker mUserTracker;
     private final DeviceProvisionedController mDeviceProvisionedController;
     private final Resources mResources;
+    private final IStatusBarService mBarService;
     // Current wallpaper colors associated to a user.
     private final SparseArray<WallpaperColors> mCurrentColors = new SparseArray<>();
     private final WallpaperManager mWallpaperManager;
@@ -358,7 +361,7 @@ public class ThemeOverlayController extends CoreStartable implements Dumpable {
             SecureSettings secureSettings, WallpaperManager wallpaperManager,
             UserManager userManager, DeviceProvisionedController deviceProvisionedController,
             UserTracker userTracker, DumpManager dumpManager, FeatureFlags featureFlags,
-            @Main Resources resources, WakefulnessLifecycle wakefulnessLifecycle, SystemSettings systemSettings) {
+            @Main Resources resources, WakefulnessLifecycle wakefulnessLifecycle, SystemSettings systemSettings, IStatusBarService barService) {
         super(context);
 
         mIsMonetEnabled = featureFlags.isEnabled(Flags.MONET);
@@ -376,6 +379,7 @@ public class ThemeOverlayController extends CoreStartable implements Dumpable {
         mResources = resources;
         mWakefulnessLifecycle = wakefulnessLifecycle;
         dumpManager.registerDumpable(TAG, this);
+        mBarService = barService;
     }
 
     @Override
@@ -431,7 +435,7 @@ public class ThemeOverlayController extends CoreStartable implements Dumpable {
                     @Override
                     public void onChange(boolean selfChange, Collection<Uri> collection, int flags,
                             int userId) {
-                        restartSystemUI();
+                        restartAndroid();
                     }
                 },
                 UserHandle.USER_ALL);
@@ -487,8 +491,11 @@ public class ThemeOverlayController extends CoreStartable implements Dumpable {
         });
     }
 
-    private void restartSystemUI() {
-        android.os.Process.killProcess(android.os.Process.myPid());
+    private void restartAndroid() {
+        try {
+            mBarService.restart();
+        } catch (RemoteException e) {
+        }
     }
 
     private void reevaluateSystemTheme(boolean forceReload) {
