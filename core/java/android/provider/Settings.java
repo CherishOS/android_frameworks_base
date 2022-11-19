ce697eee764c921faf77cbab0d7da077039a9557
Hưng Phan <phandinhhungvp2001@gmail.com>
2022-11-19 08:09:00 +0700

    Revert "Comeback Data Usuage in qs"
    This reverts commit 40b8a19245a624f77adf96690726d92848cc0dc0.

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6259,12 +6259,6 @@ public final class Settings {
         public static final String SHOW_QS_DATE = "show_qs_date";
 		
 		/**
-         * Whether to show data usage in qs footer
-         * @hide
-         */
-        public static final String QS_FOOTER_DATA_USAGE = "qs_footer_data_usage";
-		
-		/**
          * Whether edge light is enabled.
          * Default 0
          * @hide
