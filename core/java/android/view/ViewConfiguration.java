3e645733b9e0344bb3e2f47acf88fc1326439883
minaripenguin <minaripenguin@users.noreply.github.com>
2022-11-20 18:50:06 +0800

    ViewConfiguration: Fix multi-thread safety issue
    test: boot to homescreen, perform scroll/flings across apps, no thread exceptions
    
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>
    Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>

--- core/java/android/view/ViewConfiguration.java
+++ core/java/android/view/ViewConfiguration.java
@@ -17,6 +17,7 @@
 package android.view;
 
 import android.annotation.FloatRange;
+import android.annotation.MainThread;
 import android.annotation.NonNull;
 import android.annotation.TestApi;
 import android.annotation.UiContext;
@@ -40,6 +41,7 @@ import android.util.TypedValue;
 /**
  * Contains methods to standard constants used in the UI for timeouts, sizes, and distances.
  */
+@MainThread
 public class ViewConfiguration {
     private static final String TAG = "ViewConfiguration";
 
