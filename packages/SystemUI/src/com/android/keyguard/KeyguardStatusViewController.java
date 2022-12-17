9568d8f5d4e3563aed3f26a63dfdcfb9c61c1f97
Pranav Vashi <neobuddy89@gmail.com>
2022-12-17 18:33:47 +0530

    SystemUI: refresh clock font when system font changes

--- packages/SystemUI/src/com/android/keyguard/KeyguardStatusViewController.java
+++ packages/SystemUI/src/com/android/keyguard/KeyguardStatusViewController.java
@@ -208,6 +208,11 @@ public class KeyguardStatusViewController extends ViewController<KeyguardStatusV
         public void onDensityOrFontScaleChanged() {
             mKeyguardClockSwitchController.onDensityOrFontScaleChanged();
         }
+
+        @Override
+        public void onThemeChanged() {
+            mKeyguardClockSwitchController.onDensityOrFontScaleChanged();
+        }
     };
 
     private KeyguardUpdateMonitorCallback mInfoCallback = new KeyguardUpdateMonitorCallback() {
