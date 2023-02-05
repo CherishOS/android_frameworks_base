79b7302efab80290eb6285cad6e4bc9b2433e938
rdx420 <padraramesh420@gmail.com>
2023-02-05 11:50:27 +0530

    QS Styles: Sync with new QS Styles
    Signed-off-by: rdx420 <padraramesh420@gmail.com>
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>
    Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>

--- packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSIconViewImpl.java
+++ packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSIconViewImpl.java
@@ -256,6 +256,7 @@ public class QSIconViewImpl extends QSIconView {
         (mRandomColor.nextInt(256) / 2f + 0.5),
          mRandomColor.nextInt(256),
          mRandomColor.nextInt(256));
+        int mWhiteTint = Color.WHITE; 
 
         if (state.disabledByPolicy || state.state == Tile.STATE_UNAVAILABLE) {
             return Utils.getColorAttrDefaultColor(
@@ -268,6 +269,8 @@ public class QSIconViewImpl extends QSIconView {
                         android.R.attr.colorAccent);
              } else if (qsPanelStyle == 3) {
                return mRandomTint;
+             } else if (qsPanelStyle == 4 || qsPanelStyle == 6 || qsPanelStyle == 9) {
+               return mWhiteTint;
              } else {
               return Utils.getColorAttrDefaultColor(context,
                     com.android.internal.R.attr.textColorPrimaryInverse);
