7270b36df3a0b1383b17b5d1d9e1088594c88344
Ido Ben-Hur <idoybh2@gmail.com>
2023-02-10 15:59:38 +0200

    SystemUI: screenshot: Also post a notification with a preview
    Make each notification unique so user can take many and still quick access em all
    This does not need a setting side as the notification channel is blockable
    No notification actions for now -
    the file can be accessed by pressing the notification. Gallery can manage it just fine
    
    Change-Id: I24af5868f6cf4feb287257c8ef93f47e09e555a9
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>

--- packages/SystemUI/src/com/android/systemui/screenshot/ScreenshotController.java
+++ packages/SystemUI/src/com/android/systemui/screenshot/ScreenshotController.java
@@ -1171,6 +1171,8 @@ public class ScreenshotController {
                 } else {
                     doPostAnimation(imageData);
                 }
+                mNotificationsController.showPostActionNotification(
+                    imageData, mScreenBitmap);
             });
         }
     }
