fcc54a5ce91dc696f3683238ab95c3ef47c34e0b
Hưng Phan <phandinhhungvp2001@gmail.com>
2023-03-22 23:43:16 +0700

    [Cleanup]Smartspace
    Revert "SystemUI: smartspace: Adapt to 13-QPR2"
    
    This reverts commit 9ae8fd8fe8d56f9704aaf6d92d0600e9ed87ef78.
    
    Revert "SystemUI: Grant additional permissions for smartspace"
    
    This reverts commit 4e76da08d26ba050d30060f0d7eb62c876d9a511.
    
    Revert "Smartspace: Fix music indication not showing on doze/aod"
    
    This reverts commit 4c2f32c286b11e0b3a4687f1a9f5084c60ad0f29.
    
    Revert "SystemUI: Update smartspace to 13-QPR1"
    
    This reverts commit c5f0f59370915cef936e289a9eeb7951acbfb1dd.
    
    Revert "SystemUI: Forward-port smartspace to Android 13"
    
    This reverts commit d01a3c11ec0249d6390498d245b8174e972b5f6f.
    
    Revert "SystemUI: Implement Reverse-engineered Smartspace"
    
    This reverts commit 4e4321d60c04c52468a1ddd1a55ef5da885abba3.

--- packages/SystemUI/src/com/android/systemui/dagger/SysUIComponent.java
+++ packages/SystemUI/src/com/android/systemui/dagger/SysUIComponent.java
@@ -49,8 +49,6 @@ import com.android.wm.shell.startingsurface.StartingSurface;
 import com.android.wm.shell.sysui.ShellInterface;
 import com.android.wm.shell.transition.ShellTransitions;
 
-import com.google.android.systemui.smartspace.KeyguardSmartspaceController;
-
 import java.util.Map;
 import java.util.Optional;
 
@@ -238,10 +236,4 @@ public interface SysUIComponent {
      * Member injection into the supplied argument.
      */
     void inject(PeopleProvider peopleProvider);
-
-    /**
-     * Creates a KeyguardSmartspaceController.
-     */
-    @SysUISingleton
-    KeyguardSmartspaceController createKeyguardSmartspaceController();
 }
