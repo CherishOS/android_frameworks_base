1da34f6890d7a4ef8268c2fe232e4f79ad0c5b63
jhenrique09 <jhsv09@gmail.com>
2023-03-22 21:25:05 -0300

    NetworkTraffic: Refactor and remove indication on quick settings
    Change-Id: I0944df51e5d61bbde6bd9b0c9b0c5126ef20b86c
    Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6778,13 +6778,12 @@ public final class Settings {
         public static final String FINGERPRINT_SUCCESS_VIB = "fingerprint_success_vib";
 
         /**
-         * Network traffic indicator location
+         * Network traffic indication
          * 0 = Disabled
-         * 1 = Statusbar
-         * 2 = Quick statusbar
+         * 1 = Enabled
          * @hide
          */
-        public static final String NETWORK_TRAFFIC_LOCATION = "network_traffic_location";
+        public static final String NETWORK_TRAFFIC_ENABLED = "network_traffic_enabled";
 
         /**
          * Whether or not to hide the network traffic indicator when there is no activity
