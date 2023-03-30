52f0ec58d1993dc4ad74a3edc32104884119d28b
Hưng Phan <phandinhhungvp2001@gmail.com>
2023-03-31 03:18:45 +0700

    Revert "SystemUI: Combined QS header layout fixes"
    This reverts commit 2166c4bf4f63fbd285f6326fd493ecec91beb978.

--- packages/SystemUI/src/com/android/systemui/shade/NotificationPanelViewController.java
+++ packages/SystemUI/src/com/android/systemui/shade/NotificationPanelViewController.java
@@ -674,7 +674,6 @@ public final class NotificationPanelViewController implements Dumpable {
     private boolean mQSAnimatingHiddenFromCollapsed;
     private boolean mUseLargeScreenShadeHeader;
     private boolean mEnableQsClipping;
-    private boolean mUseCombinedQSHeaders;
 
     private int mQsClipTop;
     private int mQsClipBottom;
@@ -1325,7 +1324,6 @@ public final class NotificationPanelViewController implements Dumpable {
 
         mUseLargeScreenShadeHeader =
                 LargeScreenUtils.shouldUseLargeScreenShadeHeader(mView.getResources());
-        mUseCombinedQSHeaders = mFeatureFlags.isEnabled(Flags.COMBINED_QS_HEADERS);
 
         mLargeScreenShadeHeaderHeight =
                 mResources.getDimensionPixelSize(R.dimen.large_screen_shade_header_height);
@@ -1339,9 +1337,7 @@ public final class NotificationPanelViewController implements Dumpable {
             mQuickQsHeaderHeight = SystemBarUtils.getQuickQsOffsetHeight(mView.getContext());
         }
         int topMargin = mUseLargeScreenShadeHeader ? mLargeScreenShadeHeaderHeight :
-                mResources.getDimensionPixelSize(mUseCombinedQSHeaders
-                        ? R.dimen.notification_panel_margin_top_combined_headers
-                        : R.dimen.notification_panel_margin_top);
+                mResources.getDimensionPixelSize(R.dimen.notification_panel_margin_top);
         mLargeScreenShadeHeaderController.setLargeScreenActive(mUseLargeScreenShadeHeader);
         mAmbientState.setStackTopMargin(topMargin);
         mNotificationsQSContainerController.updateResources();
--- packages/SystemUI/src/com/android/systemui/shade/NotificationsQSContainerController.kt
+++ packages/SystemUI/src/com/android/systemui/shade/NotificationsQSContainerController.kt
@@ -58,13 +58,6 @@ class NotificationsQSContainerController @Inject constructor(
 ) : ViewController<NotificationsQuickSettingsContainer>(view), QSContainerController {
 
     private var qsExpanded = false
-        set(value) {
-            if (field != value) {
-                field = value
-                mView.invalidate()
-            }
-        }
-
     private var splitShadeEnabled = false
     private var isQSDetailShowing = false
     private var isQSCustomizing = false
@@ -79,7 +72,6 @@ class NotificationsQSContainerController @Inject constructor(
     private var bottomCutoutInsets = 0
     private var panelMarginHorizontal = 0
     private var topMargin = 0
-    private var qsTopMargin = 0
 
     private val useCombinedQSHeaders = featureFlags.isEnabled(Flags.COMBINED_QS_HEADERS)
 
@@ -160,13 +152,7 @@ class NotificationsQSContainerController @Inject constructor(
         topMargin = if (largeScreenShadeHeaderActive) {
             largeScreenShadeHeaderHeight
         } else {
-            resources.getDimensionPixelSize(
-                if (useCombinedQSHeaders) {
-                    R.dimen.notification_panel_margin_top_combined_headers
-                } else {
-                    R.dimen.notification_panel_margin_top
-                }
-            )
+            resources.getDimensionPixelSize(R.dimen.notification_panel_margin_top)
         }
         updateConstraints()
 
