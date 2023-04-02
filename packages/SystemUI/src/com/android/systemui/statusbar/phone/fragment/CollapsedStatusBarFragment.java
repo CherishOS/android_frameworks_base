d050ebffee9f663f6999fe9feea691b41586db3b
Michael W <baddaemon87@gmail.com>
2023-04-02 14:53:47 +0200

    SystemUI: Make clock & battery clickable again^2
    * Since qpr2, the QS has changed again:
      * The clock is now large when expanded fully
      * Battery icon is in a separate row when fully expanded
      * Battery icon is hidden when partially expanded and privacy
        chip is visible, therefore we don't need to set it to non-clickable
        anymore
    * Works in collapsed, partially expanded and expanded states
    
    Fixes: https://gitlab.com/LineageOS/issues/android/-/issues/5684
    Change-Id: I66751353944b4cf8a5b5827c3ce53129ec84f1e7
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/fragment/CollapsedStatusBarFragment.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/fragment/CollapsedStatusBarFragment.java
@@ -29,12 +29,14 @@ import android.annotation.Nullable;
 import android.annotation.SuppressLint;
 import android.app.Fragment;
 import android.content.ContentResolver;
+import android.content.Intent;
 import android.database.ContentObserver;
 import android.os.Handler;
 import android.os.UserHandle;
 import android.os.Bundle;
 import android.os.Parcelable;
 import android.os.UserHandle;
+import android.provider.AlarmClock;
 import android.provider.Settings;
 import android.telephony.SubscriptionManager;
 import android.util.ArrayMap;
@@ -56,6 +58,7 @@ import com.android.systemui.battery.BatteryMeterView;
 import com.android.systemui.dagger.qualifiers.Main;
 import com.android.systemui.dump.DumpManager;
 import com.android.systemui.flags.FeatureFlags;
+import com.android.systemui.plugins.ActivityStarter;
 import com.android.systemui.plugins.statusbar.StatusBarStateController;
 import com.android.systemui.shade.NotificationPanelViewController;
 import com.android.systemui.shade.ShadeExpansionStateManager;
@@ -149,7 +152,7 @@ public class CollapsedStatusBarFragment extends Fragment implements CommandQueue
     private final SecureSettings mSecureSettings;
     private final Executor mMainExecutor;
     private final DumpManager mDumpManager;
-
+    private final ActivityStarter mActivityStarter;
     private ClockController mClockController;
     private boolean mIsClockBlacklisted;
 
@@ -251,7 +254,8 @@ public class CollapsedStatusBarFragment extends Fragment implements CommandQueue
             OperatorNameViewController.Factory operatorNameViewControllerFactory,
             SecureSettings secureSettings,
             @Main Executor mainExecutor,
-            DumpManager dumpManager
+            DumpManager dumpManager,
+            ActivityStarter activityStarter
     ) {
         mStatusBarFragmentComponentFactory = statusBarFragmentComponentFactory;
         mOngoingCallController = ongoingCallController;
@@ -274,6 +278,7 @@ public class CollapsedStatusBarFragment extends Fragment implements CommandQueue
         mSecureSettings = secureSettings;
         mMainExecutor = mainExecutor;
         mDumpManager = dumpManager;
+        mActivityStarter = activityStarter;
     }
 
     @Override
@@ -608,6 +613,9 @@ public class CollapsedStatusBarFragment extends Fragment implements CommandQueue
 
     private void showClock(boolean animate) {
         animateShow(mClockController.getClock(), animate);
+        mClockController.getClock().setOnClickListener(v ->
+                mActivityStarter.postStartActivityDismissingKeyguard(
+                        new Intent(AlarmClock.ACTION_SHOW_ALARMS), 0));
     }
 
     /** Hides the ongoing call chip. */
