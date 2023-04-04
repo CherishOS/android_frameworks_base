26a1b39ae805d27759c53b727a86c897fd8dd046
Dil3mm4 <dil3mm4.dev@gmail.com>
2023-04-04 19:28:36 +0800

    SystemUI: use app icon instead of notification icon small
    * fallback to non-colored notification icon if work profile image or appicon is null
    
    mnri: fixed NPE with work profile, refactored for risingOS
    
    Co-authored-by: minaripenguin <minaripenguin@users.noreply.github.com>
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/notification/row/wrapper/NotificationHeaderViewWrapper.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/notification/row/wrapper/NotificationHeaderViewWrapper.java
@@ -20,6 +20,8 @@ import static com.android.systemui.statusbar.notification.TransformState.TRANSFO
 
 import android.app.Notification;
 import android.content.Context;
+import android.content.pm.PackageManager;
+import android.graphics.drawable.Drawable;
 import android.util.ArraySet;
 import android.view.NotificationHeaderView;
 import android.view.NotificationTopLineView;
@@ -73,9 +75,11 @@ public class NotificationHeaderViewWrapper extends NotificationViewWrapper imple
     private boolean mTransformLowPriorityTitle;
     private boolean mUseRoundnessSourceTypes;
     private RoundnessChangedListener mRoundnessChangedListener;
+    private Context mContext;
 
     protected NotificationHeaderViewWrapper(Context ctx, View view, ExpandableNotificationRow row) {
         super(ctx, view, row);
+        mContext = ctx;
         mRoundableState = new RoundableState(
                 mView,
                 this,
@@ -187,6 +191,16 @@ public class NotificationHeaderViewWrapper extends NotificationViewWrapper imple
         addRemainingTransformTypes();
         updateCropToPaddingForImageViews();
         Notification notification = row.getEntry().getSbn().getNotification();
+        String pkgname = row.getEntry().getSbn().getPackageName();
+        Drawable appIcon = pkgname != null ?
+                    getApplicationIcon(pkgname) : null;
+        if (appIcon != null && mWorkProfileImage != null) {
+            mIcon.setImageDrawable(appIcon);
+            mWorkProfileImage.setImageIcon(notification.getSmallIcon());
+            // The work profile image is always the same lets just set the icon tag for it not to
+            // animate
+            mWorkProfileImage.setTag(ImageTransformState.ICON_TAG, notification.getSmallIcon());
+        }
         mIcon.setTag(ImageTransformState.ICON_TAG, notification.getSmallIcon());
 
         // We need to reset all views that are no longer transforming in case a view was previously
@@ -198,6 +212,17 @@ public class NotificationHeaderViewWrapper extends NotificationViewWrapper imple
                 mTransformationHelper.resetTransformedView(view);
             }
         }
+    }
+
+    private Drawable getApplicationIcon(String packageName) {
+        PackageManager pm = mContext.getPackageManager();
+        Drawable icon = null;
+        try {
+            icon = pm.getApplicationIcon(packageName);
+        } catch (Exception e) {
+            // nothing to do
+        }
+        return icon;
     }
 
     /**
