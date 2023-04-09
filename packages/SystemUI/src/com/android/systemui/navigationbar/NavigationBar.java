e76784b6fca8ac9418ab6a07eda116807e2eb6c3
Hưng Phan <phandinhhungvp2001@gmail.com>
2023-04-09 11:05:09 +0700

    Cleanup old hide IME Space
    Revert "base: Disable hide IME space feature by default"
    
    This reverts commit 6ab4faba89684e0f320e411076b91e7f996c6aae.
    
    Revert "base: Make NavigationBar to use Hidden IME Space only on gesture bar"
    
    This reverts commit 1e7794f2a0a1040e5b3690c78f1cc40b794f784b.
    
    Revert "base: Make IME space optional"
    
    This reverts commit e816c6a4620ea4f9bfc8effd7e8b55a7d9b957d4.
    
    Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>

--- packages/SystemUI/src/com/android/systemui/navigationbar/NavigationBar.java
+++ packages/SystemUI/src/com/android/systemui/navigationbar/NavigationBar.java
@@ -55,8 +55,6 @@ import android.annotation.NonNull;
 import android.app.ActivityTaskManager;
 import android.app.IActivityTaskManager;
 import android.app.StatusBarManager;
-import android.content.BroadcastReceiver;
-import android.content.ContentResolver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.res.Configuration;
@@ -72,7 +70,6 @@ import android.os.Handler;
 import android.os.IBinder;
 import android.os.RemoteException;
 import android.os.UserHandle;
-import android.provider.Settings;
 import android.provider.DeviceConfig;
 import android.telecom.TelecomManager;
 import android.text.TextUtils;
@@ -1437,8 +1434,6 @@ public class NavigationBar extends ViewController<NavigationBarView> implements 
         int insetsHeight = -1;
         int gravity = Gravity.BOTTOM;
         boolean navBarCanMove = true;
-        boolean IsHideIMESpaceEnabled = Settings.System.getIntForUser(mContext.getContentResolver(),
-                Settings.System.HIDE_IME_SPACE_ENABLE , 0, UserHandle.USER_CURRENT) != 0;
         final Context userContext = mUserContextProvider.createCurrentUserContext(mContext);
         if (mWindowManager != null && mWindowManager.getCurrentWindowMetrics() != null) {
             Rect displaySize = mWindowManager.getCurrentWindowMetrics().getBounds();
@@ -1447,13 +1442,8 @@ public class NavigationBar extends ViewController<NavigationBarView> implements 
                     com.android.internal.R.bool.config_navBarCanMove);
         }
         if (!navBarCanMove) {
-	    if (IsHideIMESpaceEnabled && isGesturalMode(mNavBarMode)) {
-              height = userContext.getResources().getDimensionPixelSize(
-                      com.android.internal.R.dimen.navigation_bar_frame_height_hide_ime);
-            } else {
-              height = userContext.getResources().getDimensionPixelSize(
-                      com.android.internal.R.dimen.navigation_bar_frame_height);
-	   }
+            height = userContext.getResources().getDimensionPixelSize(
+                    com.android.internal.R.dimen.navigation_bar_frame_height);
             insetsHeight = userContext.getResources().getDimensionPixelSize(
                     com.android.internal.R.dimen.navigation_bar_height);
         } else {
@@ -1461,13 +1451,8 @@ public class NavigationBar extends ViewController<NavigationBarView> implements 
                 case ROTATION_UNDEFINED:
                 case Surface.ROTATION_0:
                 case Surface.ROTATION_180:
-                    if (IsHideIMESpaceEnabled && isGesturalMode(mNavBarMode)) {
-                      height = userContext.getResources().getDimensionPixelSize(
-                              com.android.internal.R.dimen.navigation_bar_frame_height_hide_ime);
-                    } else {
-                      height = userContext.getResources().getDimensionPixelSize(
-                              com.android.internal.R.dimen.navigation_bar_frame_height);
-                    }
+                    height = userContext.getResources().getDimensionPixelSize(
+                            com.android.internal.R.dimen.navigation_bar_frame_height);
                     insetsHeight = userContext.getResources().getDimensionPixelSize(
                             com.android.internal.R.dimen.navigation_bar_height);
                     break;
