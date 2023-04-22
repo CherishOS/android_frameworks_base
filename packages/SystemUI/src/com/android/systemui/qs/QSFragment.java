661c5337b0bfae6f9eb0f49a56d00bd6c4a307c9
IacobIonut01 <paulionut2003@gmail.com>
2023-04-22 10:07:28 +0800

    SystemUI: A11 QS UI Style [1/3]
    includes:
     SystemUI: Redesign Quick settings
    
    Author: IacobIonut01 <paulionut2003@gmail.com>
    Date:   Fri Apr 1 17:38:27 2022 +0000
    
        SystemUI: Redesign Quick settings
    
        - Brings up the A11 style of tiles
          + Added morph status of tiles (Circle - Active | Rounded Rect - Inactive/Disabled)
          + Kept compatibility with the secondaryLabel from A12
        - Initial support for columns/rows control in QQS, QS and Customizer
        - Removed color differencce between QSPanel and Notification Drawer
        - Aligned Notification Footer buttons with the notifications (History | Clear All)
        - Added transparency for blur support
        [Temporary removed] - Keep showing notifications when the QSPanel is fully expanded
    
        Bugs / W.I.P.....
        [Temporary removed] - After exiting the QS Customizer, the Notification Drawer's scrim color goes to TRANSPARENT until the user scroll slightly
    
        [Linux4: Remove changes made to QuickStatusBarHeader.java]
        [Linux4: Adapt for T]
    
        Signed-off-by: IacobIonut01 <paulionut2003@gmail.com>
    
    Author: Tim Zimmermann <tim@linux4.de>
    Date:   Thu Jun 30 17:43:05 2022 +0200
    
        SystemUI: Use colorSurfaceHeader for QS background again
    
        * We want it to be pure black in dark theme, like on R
    
        Change-Id: I43cf9a1999601d1ce967fa79862f7153b5a24ff8
    
    Author: SamarV-121 <samarvispute121@pm.me>
    Date:   Thu Sep 29 03:34:10 2022 +0000
    
        base: Bring carrier info back on qs status bar header
    
        Change-Id: I80c18bb655bd71e01859cc72063c1aafd8fb2908
    
    Change-Id: I9630d414cd6db3b896dcc6f0591b1cd728012f36
    
    Author: minaripenguin <minaripenguin@users.noreply.github.com>
    Date:   Wed May 3 09:18:10 2023 +0800
    
        SystemUI: Introduce QS UI Style settings
          * initial QS UI styles implementation using dotOS' a11 qs style
    
        SystemUI: Fix A11 QS UI styles compatibility with qs layout settings
          * adjust tiles configuration to fit current qs tiles settings
          * Resolve crash caused by layerdrawable -> gradientdrawable classcastexception separate qs tile background for a11 and material you style
    
        SystemUI: Refactor landscape media tile setup
          * optimize space for media player
    
    Co-authored-by: SamarV-121 <samarvispute121@pm.me>
    Co-authored-by: Tim Zimmermann <tim@linux4.de>
    Co-authored-by: minaripenguin <minaripenguin@users.noreply.github.com>
    Change-Id: I02ad5e6873527b12bf436a9b1097d5d6ce9a23bd
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>
    Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>

--- packages/SystemUI/src/com/android/systemui/qs/QSFragment.java
+++ packages/SystemUI/src/com/android/systemui/qs/QSFragment.java
@@ -93,6 +93,9 @@ public class QSFragment extends LifecycleFragment implements QS, CommandQueue.Ca
     private static final String QS_TRANSPARENCY =
             "system:" + Settings.System.QS_TRANSPARENCY;
 
+    private static final String QS_UI_STYLE =
+            "system:" + Settings.System.QS_UI_STYLE;
+
     private final Rect mQsBounds = new Rect();
     private final SysuiStatusBarStateController mStatusBarStateController;
     private final KeyguardBypassController mBypassController;
@@ -114,6 +117,8 @@ public class QSFragment extends LifecycleFragment implements QS, CommandQueue.Ca
     private float mSquishinessFraction = 1;
     private boolean mQsDisabled;
     private int[] mLocationTemp = new int[2];
+    private int mQSPanelScrollY = 0;
+    private boolean isA11Style;
 
     private final RemoteInputQuickSettingsDisabler mRemoteInputQuickSettingsDisabler;
     private final MediaHost mQsMediaHost;
@@ -245,6 +250,9 @@ public class QSFragment extends LifecycleFragment implements QS, CommandQueue.Ca
         mQSPanelScrollView.setOnScrollChangeListener(
                 (v, scrollX, scrollY, oldScrollX, oldScrollY) -> {
                     // Lazily update animators whenever the scrolling changes
+                    if (isA11Style) {
+                        mQSPanelScrollY = scrollY;
+                    }
                     mQSAnimator.requestAnimatorUpdate();
                     if (mScrollListener != null) {
                         mScrollListener.onQsPanelScrollChanged(scrollY);
@@ -294,6 +302,7 @@ public class QSFragment extends LifecycleFragment implements QS, CommandQueue.Ca
                 });
 
         mTunerService.addTunable(this, QS_TRANSPARENCY);
+        mTunerService.addTunable(this, QS_UI_STYLE);
     }
 
     private void bindFooterActionsView(View root) {
@@ -434,6 +443,9 @@ public class QSFragment extends LifecycleFragment implements QS, CommandQueue.Ca
             case QS_TRANSPARENCY:
                 mCustomAlpha =
                         TunerService.parseInteger(newValue, 100) / 100f;
+                break;
+            case QS_UI_STYLE:
+                isA11Style = TunerService.parseInteger(newValue, 0) == 1;
                 break;
             default:
                 break;
