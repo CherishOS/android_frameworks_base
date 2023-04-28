db5a1eebb5d6c985736196a650a2af8d8571108b
Pranav Vashi <neobuddy89@gmail.com>
2023-04-28 11:01:16 +0530

    SystemUI: Add pixel style lockscreen weather based OmniJaws
    * Similar to SmartSpace but more consistent with keyguard status area.
    * Complete rewrite of omni implementation on Android 11.
    
    Ref:
    https://github.com/crdroidandroid/android_frameworks_base/commit/97734230d0c1f92f5bc87ff8a515b198d92cdd15
    https://github.com/crdroidandroid/android_frameworks_base/commit/05fae49a27afdc68ff9747cbe03de7da0d26bb52
    https://github.com/crdroidandroid/android_frameworks_base/commit/8a923c24fb633b7d9f28e4d78145abc44dbb1350
    
    Change-Id: I9b7272ba73c8836a3c3576dbfb27d96b693bc6f6
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>

--- packages/SystemUI/src/com/android/keyguard/KeyguardStatusViewController.java
+++ packages/SystemUI/src/com/android/keyguard/KeyguardStatusViewController.java
@@ -221,6 +221,7 @@ public class KeyguardStatusViewController extends ViewController<KeyguardStatusV
             if (visible) {
                 if (DEBUG) Slog.v(TAG, "refresh statusview visible:true");
                 refreshTime();
+                mKeyguardClockSwitchController.updateWeatherView();
             }
         }
     };
