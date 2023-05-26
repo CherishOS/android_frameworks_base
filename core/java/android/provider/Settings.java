6ccd2b25536a94b25af352fb0ab38e017c0d94e7
Ghosuto <clash.raja10@gmail.com>
2023-05-26 12:39:30 +0200

    SystemUI: Allow toggle dual tone QS background [1/2]
    neobuddy89: Keep dual tone QS as default.
    
    Change-Id: I885fc0a052374af31a55d6dc6e0fcec9e94d48ff
    Signed-off-by: Ghosuto <clash.raja10@gmail.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6825,6 +6825,12 @@ public final class Settings {
         public static final String DOUBLE_TAP_SLEEP_LOCKSCREEN = "double_tap_sleep_lockscreen";
 
         /**
+         * Enable and Disable Dual Tone Colors QsPanel
+         * @hide
+         */
+        public static final String QS_DUAL_TONE = "qs_dual_tone";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
