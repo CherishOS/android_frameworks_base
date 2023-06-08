ae8cf9995f4be9dfdd3f8857023226f2aab3e659
aswin7469 <aswinas@pixysos.com>
2023-06-08 13:30:13 +0530

    PixelPropUtils: Switch to raven for asi
    Change-Id: Ie57206e750e8554cf097c9466523526526b575b4
    Signed-off-by: aswin7469 <aswinas@pixysos.com>
    Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>

--- core/java/com/android/internal/util/cherish/PixelPropsUtils.java
+++ core/java/com/android/internal/util/cherish/PixelPropsUtils.java
@@ -68,6 +68,7 @@ public class PixelPropsUtils {
             "com.google.android.gms",
             "com.google.android.googlequicksearchbox",
             "com.google.android.inputmethod.latin",
+            "com.google.android.as",
             "com.google.android.wallpaper.effects",
             "com.google.android.apps.emojiwallpaper",
     };
