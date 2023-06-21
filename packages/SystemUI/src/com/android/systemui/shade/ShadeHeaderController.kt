11d6e72953ecaf15a407833b4d9b5e55859439e5
Michael W <baddaemon87@gmail.com>
2023-06-22 02:39:40 +0900

    SystemUI: Make clock & battery clickable again
    [Shaughzam] - Adapt for A13 QPR3
    
    Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>

--- packages/SystemUI/src/com/android/systemui/shade/ShadeHeaderController.kt
+++ packages/SystemUI/src/com/android/systemui/shade/ShadeHeaderController.kt
@@ -22,11 +22,13 @@ import android.annotation.IdRes
 import android.app.StatusBarManager
 import android.content.Context
 import android.content.res.ColorStateList
+import android.content.Intent
 import android.content.res.Configuration
 import android.graphics.Color
 import android.os.Bundle
 import android.os.Trace
 import android.os.Trace.TRACE_TAG_APP
+import android.provider.AlarmClock
 import android.util.Pair
 import android.view.DisplayCutout
 import android.view.View
@@ -44,6 +46,7 @@ import com.android.systemui.battery.BatteryMeterViewController
 import com.android.systemui.demomode.DemoMode
 import com.android.systemui.demomode.DemoModeController
 import com.android.systemui.dump.DumpManager
+import com.android.systemui.plugins.ActivityStarter
 import com.android.systemui.qs.ChipVisibilityListener
 import com.android.systemui.qs.HeaderPrivacyIconsController
 import com.android.systemui.qs.carrier.QSCarrierGroup
@@ -99,6 +102,7 @@ constructor(
     private val demoModeController: DemoModeController,
     private val qsBatteryModeController: QsBatteryModeController,
     private val featureFlags: FeatureFlags,
+    private val activityStarter: ActivityStarter
 ) : ViewController<View>(header), Dumpable {
 
     companion object {
@@ -287,6 +291,18 @@ constructor(
         privacyIconsController.onParentVisible()
 
         clock.setQsHeader()
+
+        clock.setOnClickListener {
+            activityStarter.postStartActivityDismissingKeyguard(
+                Intent(AlarmClock.ACTION_SHOW_ALARMS), 0
+            )
+        }
+
+        batteryIcon.setOnClickListener {
+            activityStarter.postStartActivityDismissingKeyguard(
+                Intent(Intent.ACTION_POWER_USAGE_SUMMARY), 0
+            )
+        }
     }
 
     override fun onViewAttached() {
