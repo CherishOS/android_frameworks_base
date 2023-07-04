fed26081c5cf730a674edbc9d1d667bcfc5a9cea
minaripenguin <minaripenguin@users.noreply.github.com>
2023-07-04 13:56:02 +0800

    Add Settings URI for Weather Icon packs
    Change-Id: I4c73608f58026be1027e9142b20754c815bd3ab8
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6762,6 +6762,11 @@ public final class Settings {
          * @hide
          */
         public static final String LOCKSCREEN_WEATHER_LOCATION = "lockscreen_weather_location";
+        
+        /**
+         * @hide
+         */
+        public static final String OMNIJAWS_WEATHER_ICON_PACK = "omnijaws_weather_icon_pack";
 
         /**
          * These are all public system settings
@@ -6906,6 +6911,7 @@ public final class Settings {
             PRIVATE_SETTINGS.add(TRANSISTENT_TASK_MODE);
             PRIVATE_SETTINGS.add(PREVENT_POINTER_ACCELERATION);
             PRIVATE_SETTINGS.add(FORCE_MOUSE_AS_TOUCH);
+            PRIVATE_SETTINGS.add(OMNIJAWS_WEATHER_ICON_PACK);
         }
 
         /**
