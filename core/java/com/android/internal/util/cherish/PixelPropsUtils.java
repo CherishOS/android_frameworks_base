bc0b47d4422584117c283a3932774220cf67488e
someone5678 <nemui3353@gmail.com>
2023-07-06 06:51:56 +0900

    PixelPropsUtils: Update pixelCodenames
    * Add tangorpro and felix
    
    Change-Id: I0a7634343b053a8c46d52b36a9fcf13c76bb3700
    Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>

--- core/java/com/android/internal/util/cherish/PixelPropsUtils.java
+++ core/java/com/android/internal/util/cherish/PixelPropsUtils.java
@@ -164,6 +164,8 @@ public class PixelPropsUtils {
 
     // Codenames for currently supported Pixels by Google
     private static final String[] pixelCodenames = {
+            "felix",
+            "tangorpro",
             "lynx",
             "cheetah",
             "panther",
