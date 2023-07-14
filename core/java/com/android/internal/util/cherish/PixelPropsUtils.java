ff94715dbaa93c53e862f4375e79ffe40feb4d31
someone5678 <nemui3353@gmail.com>
2023-07-14 18:29:03 +0900

    Revert "PropImitationHooks: Enable new Velvet Weather UI"
    * Not needed anymore
    
    This reverts commit 188cc15e3ba7de631294829372e6aa411277158c.
    
    Change-Id: I8759d1bab3d8d4c256ea09d03fc31d5e835c5516
    Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>

--- core/java/com/android/internal/util/cherish/PixelPropsUtils.java
+++ core/java/com/android/internal/util/cherish/PixelPropsUtils.java
@@ -48,7 +48,6 @@ public class PixelPropsUtils {
 
     private static final Map<String, Object> propsToChangeGeneric;
     private static final Map<String, Object> propsToChangePixel2;
-    private static final Map<String, Object> propsToChangePixelTablet;
     private static final Map<String, Object> propsToChangePixel7Pro;
     private static final Map<String, Object> propsToChangePixel5a;
     private static final Map<String, Object> propsToChangePixelXL;
@@ -60,11 +59,6 @@ public class PixelPropsUtils {
     // Packages to Spoof as Pixel 2
     private static final String[] packagesToChangePixel2 = {
             "com.snapchat.android"
-    };
-
-    private static final String[] packagesToChangePixelTablet = {
-            "com.google.android.googlequicksearchbox",
-            "com.google.android.apps.googleassistant",
     };
 
     private static final String[] packagesToChangePixel7Pro = {
@@ -199,13 +193,6 @@ public class PixelPropsUtils {
         propsToChangeGeneric = new HashMap<>();
         propsToChangeGeneric.put("TYPE", "user");
         propsToChangeGeneric.put("TAGS", "release-keys");
-        propsToChangePixelTablet = new HashMap<>();
-        propsToChangePixelTablet.put("BRAND", "google");
-        propsToChangePixelTablet.put("MANUFACTURER", "Google");
-        propsToChangePixelTablet.put("DEVICE", "tangorpro");
-        propsToChangePixelTablet.put("PRODUCT", "tangorpro");
-        propsToChangePixelTablet.put("MODEL", "Pixel Tablet");
-        propsToChangePixelTablet.put("FINGERPRINT", "google/tangorpro/tangorpro:13/TQ3A.230705.001.B4/10255998:user/release-keys");
         propsToChangePixel7Pro = new HashMap<>();
         propsToChangePixel7Pro.put("BRAND", "google");
         propsToChangePixel7Pro.put("MANUFACTURER", "Google");
@@ -335,9 +322,6 @@ public class PixelPropsUtils {
             } else if (Arrays.asList(packagesToChangePixel2).contains(packageName)) {
                 if (isPixelDevice) return;
                 propsToChange.putAll(propsToChangePixel2);
-            } else if (Arrays.asList(packagesToChangePixelTablet).contains(packageName)) {
-                if (isPixelDevice) return;
-                propsToChange.putAll(propsToChangePixelTablet);
             } else if (Arrays.asList(packagesToChangePixel7Pro).contains(packageName)) {
                 if (isPixelDevice) return;
                 propsToChange.putAll(propsToChangePixel7Pro);
