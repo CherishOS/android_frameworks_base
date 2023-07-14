57665b1814b0c2db5bb29a700e58896bee52611e
minaripenguin <minaripenguin@users.noreply.github.com>
2023-07-14 18:10:29 +0800

    SystemUI: Disable smartspace weather view when omniweather is enabled
    Change-Id: Id4b02f2fd719c220da10e6b957fdafbe398a4034
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>

--- packages/SystemUI/src/com/android/systemui/theme/ThemeOverlayController.java
+++ packages/SystemUI/src/com/android/systemui/theme/ThemeOverlayController.java
@@ -627,6 +627,18 @@ public class ThemeOverlayController implements CoreStartable, Dumpable {
                     }
                 },
                 UserHandle.USER_ALL);
+                
+            mSystemSettings.registerContentObserverForUser(
+                    Settings.System.getUriFor(Settings.System.LOCKSCREEN_WEATHER_ENABLED),
+                    false,
+                    new ContentObserver(mBgHandler) {
+                        @Override
+                        public void onChange(boolean selfChange, Collection<Uri> collection, int flags,
+                                int userId) {
+                            restartSystemUI();
+                        }
+                    },
+                    UserHandle.USER_ALL);
 
         mUserTracker.addCallback(mUserTrackerCallback, mMainExecutor);
 
