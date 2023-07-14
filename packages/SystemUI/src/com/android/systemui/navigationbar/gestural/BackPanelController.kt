2c5e895a861fd092ffea1d224b015b3dd0fd2a59
someone5678 <nemui3353@gmail.com>
2023-07-14 17:26:19 +0900

    Implement edge long swipe gesture to new back gesture affordance
    * Adapt and fix from original long swipe gesture impl
    
    Ref:
    https://github.com/LineageOS/android_frameworks_base/commit/c24f69dd449b8f7fc24a03d080579ff1627f0939
    
    Change-Id: I22c6442299df0cf15d046718fa40df6fb6aee07c
    Signed-off-by: someone5678 <nemui3353@gmail.com>
    Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>

--- packages/SystemUI/src/com/android/systemui/navigationbar/gestural/BackPanelController.kt
+++ packages/SystemUI/src/com/android/systemui/navigationbar/gestural/BackPanelController.kt
@@ -190,6 +190,10 @@ class BackPanelController internal constructor(
 
     private var edgeHapticEnabled = false
 
+    private var longSwipeThreshold = 0f
+    private var triggerLongSwipe = false
+    private var isLongSwipeEnabled = false
+
     internal enum class GestureState {
         /* Arrow is off the screen and invisible */
         GONE,
@@ -315,13 +319,16 @@ class BackPanelController internal constructor(
                 startIsLeft = mView.isLeftPanel
                 hasPassedDragSlop = false
                 mView.resetStretch()
+                mView.setTriggerLongSwipe(false)
             }
             MotionEvent.ACTION_MOVE -> {
                 if (dragSlopExceeded(event.x, startX)) {
+                    mView.setTriggerLongSwipe(triggerLongSwipe)
                     handleMoveEvent(event)
                 }
             }
             MotionEvent.ACTION_UP -> {
+                mView.setTriggerLongSwipe(triggerLongSwipe)
                 when (currentState) {
                     GestureState.ENTRY -> {
                         if (isFlungAwayFromEdge(endX = event.x) ||
@@ -376,6 +383,7 @@ class BackPanelController internal constructor(
                 velocityTracker = null
             }
             MotionEvent.ACTION_CANCEL -> {
+                mView.setTriggerLongSwipe(triggerLongSwipe)
                 // Receiving a CANCEL implies that something else intercepted
                 // the gesture, i.e., the user did not cancel their gesture.
                 // Therefore, disappear immediately, with minimum fanfare.
@@ -471,6 +479,8 @@ class BackPanelController internal constructor(
         // occurs between the screen edge and the touch start.
         val xTranslation = max(0f, if (mView.isLeftPanel) x - startX else startX - x)
 
+        val isLongSwipe = MathUtils.abs(xTranslation) > longSwipeThreshold
+
         // Compared to last time, how far we moved in the x direction. If <0, we are moving closer
         // to the edge. If >0, we are moving further from the edge
         val xDelta = xTranslation - previousXTranslation
@@ -515,6 +525,8 @@ class BackPanelController internal constructor(
                 else -> {}
             }
         }
+
+        if (isLongSwipeEnabled) setTriggerLongSwipe(isLongSwipe)
 
         setArrowStrokeAlpha(gestureProgress)
         setVerticalTranslation(yOffset)
@@ -675,6 +687,28 @@ class BackPanelController internal constructor(
 
     override fun setEdgeHapticEnabled(enabled: Boolean) {
         edgeHapticEnabled = enabled
+    }
+    
+    override fun setLongSwipeEnabled(enabled: Boolean) {
+        longSwipeThreshold = if (enabled) MathUtils.min(
+                displaySize.x * 0.5f, layoutParams.width * 2.5f) else 0.0f
+        isLongSwipeEnabled = longSwipeThreshold > 0
+        setTriggerLongSwipe(isLongSwipeEnabled && triggerLongSwipe)
+    }
+
+    private fun setTriggerLongSwipe(enabled: Boolean) {
+        if (triggerLongSwipe != enabled) {
+            triggerLongSwipe = enabled
+            vibratorHelper.vibrate(VIBRATE_ACTIVATED_EFFECT)
+            updateRestingArrowDimens()
+            // Whenever the trigger back state changes
+            // the existing translation animation should be cancelled
+            cancelFailsafe()
+            mView.cancelAnimations()
+            mView.setTriggerLongSwipe(triggerLongSwipe)
+            updateConfiguration()
+            backCallback.setTriggerLongSwipe(triggerLongSwipe)
+        }
     }
 
     private fun isFlungAwayFromEdge(endX: Float, startX: Float = touchDeltaStartX): Boolean {
@@ -901,10 +935,16 @@ class BackPanelController internal constructor(
             }
             GestureState.ENTRY,
             GestureState.INACTIVE -> {
+                setTriggerLongSwipe(false)
                 backCallback.setTriggerBack(false)
             }
             GestureState.ACTIVE -> {
-                backCallback.setTriggerBack(true)
+                if (triggerLongSwipe) {
+                    backCallback.triggerBack(false)
+                    backCallback.setTriggerBack(true)
+                } else {
+                    backCallback.setTriggerBack(true)
+                }
             }
             GestureState.GONE -> { }
         }
