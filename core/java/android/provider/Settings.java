eb9e06f521da236627ca805b5f6a928f96c3ef65
minaripenguin <minaripenguin@users.noreply.github.com>
2023-07-24 11:27:14 +0800

    SystemUI: QS Header Image [1/2]
    * reverse engineered from ancient os with [1] as reference for broken codes from jadx
    * Optimized qs banner resources for systemui file size
    * all credits goes to ancientOS
    
    [1]: Hikari-no-Tenshi/crdroid_android_frameworks_base@354d981
    
    mnri: rewrite and refactored
    
    Squashed:
        From: Pranav Vashi <neobuddy89@gmail.com>
        Date: Wed, 26 Apr 2023 03:17:27 +0530
        Subject: [PATCH] SystemUI: Make QS Header Image less intrusive with codebase
    
        Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    
        From: Idc <59904122+IDontCare-05@users.noreply.github.com>
        Date: Wed, 10 May 2023 05:41:46 +0800
        Subject: [PATCH] SystemUI: Import more QS Header resources
    
        * decompiled resources from ancientOS, all credits goes to respective owners
    
    Co-authored-by: Idc <59904122+IDontCare-05@users.noreply.github.com>
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6708,6 +6708,12 @@ public final class Settings {
         public static final String QS_TRANSPARENCY = "qs_transparency";
 
         /**
+         * QS Header
+         * @hide
+         */
+        public static final String QS_HEADER_IMAGE = "qs_header_image";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
