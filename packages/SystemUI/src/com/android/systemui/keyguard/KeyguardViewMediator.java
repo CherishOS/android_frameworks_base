c4c9fbdccbed093a47e3ec3f84c2d88de757cbbb
Joey Huab <joey@evolution-x.org>
2023-08-08 07:56:45 +0900

    [SQUASH] Reverts for A13 August 2023 security patches
    Revert "Use profile's power button/timeout lock settings"
    
    This reverts commit 773ebdd4e54d88f1c1f15d0a2eca866261c784b1.
    
    Revert "SystemUI: Fix double unlock sound issue"
    
    This reverts commit 43b9d8063f088fa97a96529025f9b93f8f53689b.
    
    Revert "always auto-grant permissions that have "normal" protectionLevel"
    
    This reverts commit 129d16e0f8c77e762e4eae29ebecb5cbbb53f948.
    
    Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>

--- packages/SystemUI/src/com/android/systemui/keyguard/KeyguardViewMediator.java
+++ packages/SystemUI/src/com/android/systemui/keyguard/KeyguardViewMediator.java
@@ -408,7 +408,6 @@ public class KeyguardViewMediator implements CoreStartable, Dumpable,
     private boolean mHideAnimationRun = false;
     private boolean mHideAnimationRunning = false;
 
-    private long mLastTimeSoundWasPlayed = 0;
     private SoundPool mLockSounds;
     private int mLockSoundId;
     private int mUnlockSoundId;
@@ -2397,8 +2396,6 @@ public class KeyguardViewMediator implements CoreStartable, Dumpable,
         if (soundId == 0) return;
         final ContentResolver cr = mContext.getContentResolver();
         if (Settings.System.getInt(cr, Settings.System.LOCKSCREEN_SOUNDS_ENABLED, 1) == 1) {
-            if (SystemClock.elapsedRealtime() - mLastTimeSoundWasPlayed < 300) return;
-            mLastTimeSoundWasPlayed = SystemClock.elapsedRealtime();
 
             mLockSounds.stop(mLockSoundStreamId);
             // Init mAudioManager
