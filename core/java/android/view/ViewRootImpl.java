OrionOS-Project/frameworks_base

DO NOT SUBMIT (Testing Code)
pure logging

Bug: 284687313
Test: Manual
(cherry picked from https://googleplex-android-review.googlesource.com/q/commit:bad80cbbe130740a115acb2aec51b5ed8c1a955b)
Merged-In: I1a19f339b94917b46c0f9fd5fb6db9404a8dfc0e
Change-Id: I1a19f339b94917b46c0f9fd5fb6db9404a8dfc0e

core/java/android/view/ViewRootImpl.java
@@ -1438,6 +1438,11 @@ public final class ViewRootImpl implements ViewParent,
                 // We should update mAttachInfo.mDisplayState after registerDisplayListener
                 // because displayState might be changed before registerDisplayListener.
                 mAttachInfo.mDisplayState = mDisplay.getState();
+                if (mBasePackageName != null
+                        && mBasePackageName.equals("com.google.android.dialer")) {
+                    Slog.w(TAG, "(" + mBasePackageName + ") Initial DisplayState: "
+                            + mAttachInfo.mDisplayState, new Throwable());
+                }
                 if ((res & WindowManagerGlobal.ADD_FLAG_USE_BLAST) != 0) {
                     mUseBLASTAdapter = true;
                 }
@@ -1522,6 +1527,7 @@ public final class ViewRootImpl implements ViewParent,
      * Register any kind of listeners if setView was success.
      */
     private void registerListeners() {
+        Slog.i(TAG, "Register listeners: " + mBasePackageName);
         mAccessibilityManager.addAccessibilityStateChangeListener(
                 mAccessibilityInteractionConnectionManager, mHandler);
         mAccessibilityManager.addHighTextContrastStateChangeListener(
@@ -1547,6 +1553,7 @@ public final class ViewRootImpl implements ViewParent,
         DisplayManagerGlobal
                 .getInstance()
                 .unregisterDisplayListener(mDisplayListener);
+        Slog.w(TAG, "Unregister listeners: " + mBasePackageName, new Throwable());
     }
 
     private void setTag() {
@@ -1950,9 +1957,13 @@ public final class ViewRootImpl implements ViewParent,
     private final DisplayListener mDisplayListener = new DisplayListener() {
         @Override
         public void onDisplayChanged(int displayId) {
+            if (mBasePackageName != null && mBasePackageName.equals("com.google.android.dialer")) {
+                Slog.i(TAG, "Received onDisplayChanged - " + mView);
+            }
             if (mView != null && mDisplay.getDisplayId() == displayId) {
                 final int oldDisplayState = mAttachInfo.mDisplayState;
                 final int newDisplayState = mDisplay.getState();
+                Slog.i(TAG, "DisplayState - old: " + oldDisplayState + ", new: " + newDisplayState);
                 if (oldDisplayState != newDisplayState) {
                     mAttachInfo.mDisplayState = newDisplayState;
                     pokeDrawLockIfNeeded();

CherishOS/android_frameworks_base

(cherry picked from https://googleplex-android-review.googlesource.com/q/commit:c7a9266a1216860a4d009bdab00c07f3df4b56c7)

core/java/android/view/ViewRootImpl.java
@@ -1438,6 +1438,11 @@ public final class ViewRootImpl implements ViewParent,
                 // We should update mAttachInfo.mDisplayState after registerDisplayListener
                 // because displayState might be changed before registerDisplayListener.
                 mAttachInfo.mDisplayState = mDisplay.getState();
+                if (mBasePackageName != null
+                        && mBasePackageName.equals("com.google.android.dialer")) {
+                    Slog.w(mTag, "(" + mBasePackageName + ") Initial DisplayState: "
+                            + mAttachInfo.mDisplayState, new Throwable());
+                }
                 if ((res & WindowManagerGlobal.ADD_FLAG_USE_BLAST) != 0) {
                     mUseBLASTAdapter = true;
                 }
@@ -1522,6 +1527,7 @@ public final class ViewRootImpl implements ViewParent,
      * Register any kind of listeners if setView was success.
      */
     private void registerListeners() {
+        Slog.i(mTag, "Register listeners: " + mBasePackageName);
         mAccessibilityManager.addAccessibilityStateChangeListener(
                 mAccessibilityInteractionConnectionManager, mHandler);
         mAccessibilityManager.addHighTextContrastStateChangeListener(
@@ -1547,6 +1553,7 @@ public final class ViewRootImpl implements ViewParent,
         DisplayManagerGlobal
                 .getInstance()
                 .unregisterDisplayListener(mDisplayListener);
+        Slog.w(mTag, "Unregister listeners: " + mBasePackageName, new Throwable());
     }
 
     private void setTag() {
@@ -1950,9 +1957,13 @@ public final class ViewRootImpl implements ViewParent,
     private final DisplayListener mDisplayListener = new DisplayListener() {
         @Override
         public void onDisplayChanged(int displayId) {
+            if (mBasePackageName != null && mBasePackageName.equals("com.google.android.dialer")) {
+                Slog.i(mTag, "Received onDisplayChanged - " + mView);
+            }
             if (mView != null && mDisplay.getDisplayId() == displayId) {
                 final int oldDisplayState = mAttachInfo.mDisplayState;
                 final int newDisplayState = mDisplay.getState();
+                Slog.i(mTag, "DisplayState - old: " + oldDisplayState + ", new: " + newDisplayState);
                 if (oldDisplayState != newDisplayState) {
                     mAttachInfo.mDisplayState = newDisplayState;
                     pokeDrawLockIfNeeded();