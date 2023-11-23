387d02ccf20658d5771041c71e993778ba346f9c
minaripenguin <minaripenguin@users.noreply.github.com>
2023-11-23 20:28:22 +0800

    SystemUI: Implement Volume Styles [1/3]
    Co-authored-by: DrDisagree <crazymahmud08@gmail.com>
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>
    Signed-off-by: mukesh22584 <mks22584@gmail.com>
    Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -12327,6 +12327,12 @@ public final class Settings {
         public static final String SHOW_FPS_OVERLAY = "show_fps_overlay";
 
         /**
+         * Volume styles
+         * @hide
+         */
+        public static final String CUSTOM_VOLUME_STYLES = "custom_volume_styles";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
