CherishOS/android_frameworks_base

commit 1ca97c55e97a78b604c4b0b20ee54f020557ce8c

Allow back predict when no more activity below adjacnet task fragment.
For adjacent case, if both TaskFragment has only one activity left, then
even if they do not match shouldMoveTaskToBack, it can still predict for
previous task.

Bug: 311336844
Test: launch sample app from widget, verify both side can be predict for
back-to-home if no more activities below the task.

Change-Id: If22e4d71e19db8b5605214b717e334123970569f

services/core/java/com/android/server/wm/BackNavigationController.java
@@ -429,9 +429,12 @@ class BackNavigationController {
             final TaskFragment prevTFAdjacent = prevTF.getAdjacentTaskFragment();
             if (prevTFAdjacent != null) {
                 if (prevTFAdjacent == currTF) {
-                    // Cannot predict what will happen when app receive back key, skip animation.
                     outPrevActivities.clear();
-                    return false;
+                    // No more activity in previous task fragment, so it can predict if previous
+                    // task exists. Otherwise, unable to predict what will happen when app receive
+                    // back key, skip animation.
+                    return prevTF.getActivity((below) -> !below.finishing, prevActivity,
+                            false /*includeBoundary*/, true /*traverseTopToBottom*/) == null;
                 } else {
                     final ActivityRecord prevActivityAdjacent =
                             prevTFAdjacent.getTopNonFinishingActivity();

commit 184e99d01f1e7c7e003d94d71ae0a05c8be24516

Fix a regression in BNC search previous activity inside task.
Regression from 1ca97c5, where there should search previous activity
from task, instead of only search from a task fragment.

Bug: 311336844
Test: launch a single activity A, then launch two adjacet activities
B & C in two task fragment. Verify there is no default back predict
animation when back gesture start on neither B or C.

Change-Id: Id7823194a1f06b02b54184bf632bb51c374e179f

services/core/java/com/android/server/wm/BackNavigationController.java
@@ -430,10 +430,10 @@ class BackNavigationController {
             if (prevTFAdjacent != null) {
                 if (prevTFAdjacent == currTF) {
                     outPrevActivities.clear();
-                    // No more activity in previous task fragment, so it can predict if previous
-                    // task exists. Otherwise, unable to predict what will happen when app receive
+                    // No more activity in task, so it can predict if previous task exists.
+                    // Otherwise, unable to predict what will happen when app receive
                     // back key, skip animation.
-                    return prevTF.getActivity((below) -> !below.finishing, prevActivity,
+                    return currentTask.getActivity((below) -> !below.finishing, prevActivity,
                             false /*includeBoundary*/, true /*traverseTopToBottom*/) == null;
                 } else {
                     final ActivityRecord prevActivityAdjacent =