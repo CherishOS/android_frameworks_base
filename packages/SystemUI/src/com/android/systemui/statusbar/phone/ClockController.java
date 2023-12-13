f2066b4d9880208b0db3f54b22340036cb8a1e46
Hưng Phan <phandinhhungvp2001@gmail.com>
2023-12-13 13:14:35 +0700

    Revert "SystemUI: Properly apply dark theming to clock"
    This reverts commit 92efcbcae67ab7c19145e97436ccf9724cfcbb1f.
    
    Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/ClockController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/ClockController.java
@@ -24,6 +24,7 @@ import android.view.View;
 
 import com.android.systemui.Dependency;
 import com.android.systemui.R;
+import com.android.systemui.plugins.DarkIconDispatcher;
 import com.android.systemui.statusbar.phone.StatusBarIconController;
 import com.android.systemui.statusbar.policy.Clock;
 import com.android.systemui.tuner.TunerService;
@@ -104,5 +105,13 @@ public class ClockController implements TunerService.Tunable {
             default:
                 break;
         }
+    }
+
+    public void addDarkReceiver() {
+        Dependency.get(DarkIconDispatcher.class).addDarkReceiver(mActiveClock);
+    }
+
+    public void removeDarkReceiver() {
+        Dependency.get(DarkIconDispatcher.class).removeDarkReceiver(mActiveClock);
     }
 }
