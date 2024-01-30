cd432a67858274f74f09a04afbdb4553f10fca30
Dhina17 <dhinalogu@gmail.com>
2024-01-30 05:30:14 +0000

    SystemUI: Make sure the udfps animation is hidden on the bouncer
    When pressing the udfps icon repetively for some time, it will enter primary bouncer.
    While launching bouncer, Somehow touch event ACTION_DOWN passes the checks in newOnTouch()
    and invokes onFingerDown(). So it shows udfps animation.
    on next event, checks in newOnTouch() stop it and not able to reach onFingerUp().
    So unable to hide udfps animation until you exit bouncer and tap the screen once.
    
    To avoid this case, hide the udfps animation in all checks of newOnTouch().
    
    Change-Id: I629a1b4e0a1c45814cc8470315c29a44cb46a0c9
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>

--- packages/SystemUI/src/com/android/systemui/biometrics/UdfpsController.java
+++ packages/SystemUI/src/com/android/systemui/biometrics/UdfpsController.java
@@ -551,15 +551,18 @@ public class UdfpsController implements DozeReceiver, Dumpable {
     private boolean onTouch(long requestId, @NonNull MotionEvent event, boolean fromUdfpsView) {
         if (!fromUdfpsView) {
             Log.e(TAG, "ignoring the touch injected from outside of UdfpsView");
+            hideUdfpsAnimation();
             return false;
         }
         if (mOverlay == null) {
             Log.w(TAG, "ignoring onTouch with null overlay");
+            hideUdfpsAnimation();
             return false;
         }
         if (!mOverlay.matchesRequestId(requestId)) {
             Log.w(TAG, "ignoring stale touch event: " + requestId + " current: "
                     + mOverlay.getRequestId());
+            hideUdfpsAnimation();
             return false;
         }
         if (!DeviceEntryUdfpsRefactor.isEnabled()) {
@@ -567,6 +570,7 @@ public class UdfpsController implements DozeReceiver, Dumpable {
                     && !mAlternateBouncerInteractor.isVisibleState())
                     || mPrimaryBouncerInteractor.isInTransit()) {
                 Log.w(TAG, "ignoring touch due to qsDragProcess or primaryBouncerInteractor");
+                hideUdfpsAnimation();
                 return false;
             }
         }
@@ -580,6 +584,7 @@ public class UdfpsController implements DozeReceiver, Dumpable {
                 mOverlayParams);
         if (result instanceof TouchProcessorResult.Failure) {
             Log.w(TAG, ((TouchProcessorResult.Failure) result).getReason());
+            hideUdfpsAnimation();
             return false;
         }
 
@@ -1259,9 +1264,9 @@ public class UdfpsController implements DozeReceiver, Dumpable {
                 cb.onFingerUp();
             }
         }
-        if (mUdfpsAnimation != null) {
-            mUdfpsAnimation.hide();
-        }
+
+        hideUdfpsAnimation();
+
         mOnFingerDown = false;
         unconfigureDisplay(view);
         cancelAodSendFingerUpAction();
@@ -1269,6 +1274,12 @@ public class UdfpsController implements DozeReceiver, Dumpable {
 
     public boolean isAnimationEnabled() {
         return mUdfpsAnimation != null && mUdfpsAnimation.isAnimationEnabled();
+    }
+
+    private void hideUdfpsAnimation() {
+        if (mUdfpsAnimation != null) {
+            mUdfpsAnimation.hide();
+        }
     }
 
     /**
