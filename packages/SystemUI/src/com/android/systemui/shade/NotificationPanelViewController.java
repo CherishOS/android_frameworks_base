1c1c4d3510de7ae680f569b424d0827c34d21ede
Dhina17 <dhinalogu@gmail.com>
2024-02-22 08:35:37 +0000

    PulseLightView: Cancel the animation on pulse end
    Seems like just the view is hidden but animation kept running.
    (logs says)
    
    Change-Id: I9b3ab71fd3e60edea60652869afbfaf38de88540
    Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>

--- packages/SystemUI/src/com/android/systemui/shade/NotificationPanelViewController.java
+++ packages/SystemUI/src/com/android/systemui/shade/NotificationPanelViewController.java
@@ -3325,11 +3325,10 @@ public final class NotificationPanelViewController implements ShadeSurface, Dump
             // Animate edge light only for notification pulse.
             // Package not empty means pulse caused by a notification.
             if (!notifPackageName.isEmpty()) {
-                mPulseLightView.setVisibility(View.VISIBLE);
-                mPulseLightView.animateNotification(notifPackageName);
+                mPulseLightView.startAnimation(notifPackageName);
             }
         } else {
-            mPulseLightView.setVisibility(View.GONE);
+            mPulseLightView.stopAnimation();
         }
     }
 
