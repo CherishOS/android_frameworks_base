Evolution-X-AOSP/frameworks_base

[SQUASH] Bring back UDFPS animation
neobuddy89: Adapted for Android 12L
shaazice - Adapted for A13 QPR1

[Dhina17]
- Drop Udfps icon customization
- Use ContentObserver instead of TunerService

Co-authored-by: Pranav Vashi <neobuddy89@gmail.com>
Co-authored-by: jhonboy121 <alfredmathew05@gmail.com>
Co-authored-by: daniml3 <danimoral1001@gmail.com>
Co-authored-by: DennySPb <dennyspb@gmail.com>
Co-authored-by: kondors1995 <normandija1945@gmail.com>
Co-authored-by: jhenrique09 <jhenrique09.mcz@hotmail.com>
Co-authored-by: Hikari-no-Tenshi <Hikari-no-Tenshi@users.noreply.github.com>
Co-authored-by: TheScarastic <warabhishek@gmail.com>
Co-authored-by: nullxception <nullxception@users.noreply.github.com>
Co-authored-by: Joey Huab <joey@evolution-x.org>
Co-authored-by: 00day0 <00day0@users.noreply.github.com>
Co-authored-by: Dhina17 <dhinalogu@gmail.com>
Change-Id: I0f0fcde998ad3d80f22f76f7136f141331ca6c7c
Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

packages/SystemUI/src/com/android/systemui/biometrics/UdfpsAnimation.java
@@ -1,0 +1,192 @@
+/**
+ * Copyright (C) 2019 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.systemui.biometrics;
+
+import static com.android.systemui.doze.util.BurnInHelperKt.getBurnInOffset;
+import static com.android.systemui.doze.util.BurnInHelperKt.getBurnInProgressOffset;
+
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.content.res.Resources;
+import android.database.ContentObserver;
+import android.graphics.PixelFormat;
+import android.graphics.drawable.AnimationDrawable;
+import android.hardware.fingerprint.FingerprintSensorPropertiesInternal;
+import android.net.Uri;
+import android.os.UserHandle;
+import android.provider.Settings;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.util.MathUtils;
+import android.view.Gravity;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.ImageView;
+import android.graphics.Rect;
+
+import com.android.systemui.Dependency;
+import com.android.systemui.R;
+import com.android.systemui.plugins.statusbar.StatusBarStateController;
+
+public class UdfpsAnimation extends ImageView {
+
+    private static final boolean DEBUG = true;
+    private static final String LOG_TAG = "UdfpsAnimations";
+
+    private boolean mShowing = false;
+    private Context mContext;
+    private int mAnimationSize;
+    private int mAnimationOffset;
+    private AnimationDrawable recognizingAnim;
+
+    private final WindowManager.LayoutParams mAnimParams = new WindowManager.LayoutParams();
+    private WindowManager mWindowManager;
+
+    private boolean mIsKeyguard;
+    private boolean mEnabled;
+
+    private final int mMaxBurnInOffsetX;
+    private final int mMaxBurnInOffsetY;
+
+    private int mSelectedAnim;
+    private String[] mStyleNames;
+
+    private static final String UDFPS_ANIMATIONS_PACKAGE = "com.evolution.udfps.resources";
+
+    private Resources mApkResources;
+
+    public UdfpsAnimation(Context context, WindowManager windowManager,
+           FingerprintSensorPropertiesInternal props) {
+        super(context);
+        mContext = context;
+
+        mWindowManager = windowManager;
+
+        mMaxBurnInOffsetX = context.getResources()
+            .getDimensionPixelSize(R.dimen.udfps_burn_in_offset_x);
+        mMaxBurnInOffsetY = context.getResources()
+            .getDimensionPixelSize(R.dimen.udfps_burn_in_offset_y);
+
+        mAnimationSize = mContext.getResources().getDimensionPixelSize(R.dimen.udfps_animation_size);
+        mAnimationOffset = mContext.getResources().getDimensionPixelSize(R.dimen.udfps_animation_offset);
+
+        mAnimParams.height = mAnimationSize;
+        mAnimParams.width = mAnimationSize;
+
+        mAnimParams.format = PixelFormat.TRANSLUCENT;
+        mAnimParams.type = WindowManager.LayoutParams.TYPE_VOLUME_OVERLAY; // it must be behind Udfps icon
+        mAnimParams.flags =  WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE
+                | WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE
+                | WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS;
+        mAnimParams.gravity = Gravity.TOP | Gravity.CENTER;
+        mAnimParams.y = props.getLocation().sensorLocationY - props.getLocation().sensorRadius
+                - (mAnimationSize / 2) + mAnimationOffset;
+
+        try {
+            PackageManager pm = mContext.getPackageManager();
+            mApkResources = pm.getResourcesForApplication(UDFPS_ANIMATIONS_PACKAGE);
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
+        int res = mApkResources.getIdentifier("udfps_animation_styles",
+                "array", UDFPS_ANIMATIONS_PACKAGE);
+        mStyleNames = mApkResources.getStringArray(res);
+
+        setScaleType(ImageView.ScaleType.CENTER_INSIDE);
+
+        Uri udfpsAnim = Settings.System.getUriFor(Settings.System.UDFPS_ANIM);
+        Uri udfpsAnimStyle = Settings.System.getUriFor(Settings.System.UDFPS_ANIM_STYLE);
+        ContentObserver contentObserver = new ContentObserver(null) {
+            @Override
+            public void onChange(boolean selfChange, Uri uri) {
+                if (udfpsAnim.equals(uri)) {
+                    mEnabled = Settings.System.getIntForUser(mContext.getContentResolver(),
+                            Settings.System.UDFPS_ANIM, 0, UserHandle.USER_CURRENT) == 1;
+                } else if (udfpsAnimStyle.equals(uri)) {
+                    mSelectedAnim = Settings.System.getIntForUser(mContext.getContentResolver(),
+                            Settings.System.UDFPS_ANIM_STYLE, 0, UserHandle.USER_CURRENT);
+                    mContext.getMainExecutor().execute(() -> {
+                        updateAnimationStyle(mStyleNames[mSelectedAnim]);
+                    });
+                }
+            }
+        };
+        mContext.getContentResolver().registerContentObserver(
+                udfpsAnim, false, contentObserver, UserHandle.USER_CURRENT);
+        mContext.getContentResolver().registerContentObserver(
+                udfpsAnimStyle, false, contentObserver, UserHandle.USER_CURRENT);
+        contentObserver.onChange(true, udfpsAnim);
+        contentObserver.onChange(true, udfpsAnimStyle);
+    }
+
+    private void updateAnimationStyle(String drawableName) {
+        if (DEBUG) Log.i(LOG_TAG, "Updating animation style to:" + drawableName);
+        int resId = mApkResources.getIdentifier(drawableName, "drawable", UDFPS_ANIMATIONS_PACKAGE);
+        if (DEBUG) Log.i(LOG_TAG, "Got resource id: "+ resId +" from package" );
+        setBackgroundDrawable(mApkResources.getDrawable(resId));
+        recognizingAnim = (AnimationDrawable) getBackground();
+    }
+
+    public void show() {
+        if (!mShowing && mIsKeyguard && mEnabled) {
+            mShowing = true;
+            try {
+                if (getWindowToken() == null) {
+                    mWindowManager.addView(this, mAnimParams);
+                } else {
+                    mWindowManager.updateViewLayout(this, mAnimParams);
+                }
+            } catch (RuntimeException e) {
+                // Ignore
+            }
+            if (recognizingAnim != null) {
+                recognizingAnim.start();
+            }
+        }
+    }
+
+    public void hide() {
+        if (mShowing) {
+            mShowing = false;
+            if (recognizingAnim != null) {
+                clearAnimation();
+                recognizingAnim.stop();
+                recognizingAnim.selectDrawable(0);
+            }
+            if (getWindowToken() != null) {
+                mWindowManager.removeView(this);
+            }
+        }
+    }
+
+    public void setIsKeyguard(boolean isKeyguard) {
+        mIsKeyguard = isKeyguard;
+    }
+
+    public void dozeTimeTick() {
+        float amt = Dependency.get(StatusBarStateController.class).getDozeAmount();
+
+        float mBurnInOffsetX = MathUtils.lerp(0f,
+                getBurnInOffset(mMaxBurnInOffsetX * 2, true /* xAxis */)
+                - mMaxBurnInOffsetX, amt);
+        float mBurnInOffsetY = MathUtils.lerp(0f,
+                getBurnInOffset(mMaxBurnInOffsetY * 2, false /* xAxis */)
+                - mMaxBurnInOffsetY, amt);
+        setTranslationX(mBurnInOffsetX);
+        setTranslationY(mBurnInOffsetY);
+    }
+}

packages/SystemUI/src/com/android/systemui/biometrics/UdfpsController.java
@@ -38,6 +38,7 @@ import android.database.ContentObserver;
 import android.graphics.Point;
 import android.graphics.Rect;
 import android.hardware.biometrics.BiometricFingerprintConstants;
+import android.hardware.biometrics.BiometricOverlayConstants;
 import android.hardware.biometrics.SensorProperties;
 import android.hardware.display.AmbientDisplayConfiguration;
 import android.hardware.display.ColorDisplayManager;
@@ -73,6 +74,7 @@ import androidx.annotation.Nullable;
 import com.android.internal.annotations.VisibleForTesting;
 import com.android.internal.logging.InstanceId;
 import com.android.internal.util.LatencyTracker;
+import com.android.internal.util.evolution.EvolutionUtils;
 import com.android.internal.util.evolution.udfps.CustomUdfpsUtils;
 import com.android.keyguard.FaceAuthApiRequestReason;
 import com.android.keyguard.KeyguardUpdateMonitor;
@@ -246,6 +248,8 @@ public class UdfpsController implements DozeReceiver, Dumpable {
     private BoostFramework mPerf = null;
     private boolean mIsPerfLockAcquired = false;
     private static final int BOOST_DURATION_TIMEOUT = 2000;
+
+    private UdfpsAnimation mUdfpsAnimation;
 
     @VisibleForTesting
     public static final VibrationAttributes UDFPS_VIBRATION_ATTRIBUTES =
@@ -936,13 +940,7 @@ public class UdfpsController implements DozeReceiver, Dumpable {
         mLatencyTracker = latencyTracker;
         mActivityLaunchAnimator = activityLaunchAnimator;
         mAlternateTouchProvider = alternateTouchProvider.map(Provider::get).orElse(null);
-        mSensorProps = new FingerprintSensorPropertiesInternal(
-                -1 /* sensorId */,
-                SensorProperties.STRENGTH_CONVENIENCE,
-                0 /* maxEnrollmentsPerUser */,
-                new ArrayList<>() /* componentInfo */,
-                FingerprintSensorProperties.TYPE_UNKNOWN,
-                false /* resetLockoutRequiresHardwareAuthToken */);
+        mSensorProps = findFirstUdfps();
 
         mBiometricExecutor = biometricsExecutor;
         mPrimaryBouncerInteractor = primaryBouncerInteractor;
@@ -1004,6 +1002,11 @@ public class UdfpsController implements DozeReceiver, Dumpable {
                 }
             );
         }
+
+        if (EvolutionUtils.isPackageInstalled(mContext,
+                "com.evolution.udfps.resources")) {
+            mUdfpsAnimation = new UdfpsAnimation(mContext, mWindowManager, mSensorProps);
+        }
     }
 
     private void disableNightMode() {
@@ -1050,6 +1053,17 @@ public class UdfpsController implements DozeReceiver, Dumpable {
         }
     }
 
+    @Nullable
+    private FingerprintSensorPropertiesInternal findFirstUdfps() {
+        for (FingerprintSensorPropertiesInternal props :
+                mFingerprintManager.getSensorPropertiesInternal()) {
+            if (props.isAnyUdfpsType()) {
+                return props;
+            }
+        }
+        return null;
+    }
+
     @Override
     public void dozeTimeTick() {
         if (mOverlay != null) {
@@ -1075,6 +1089,12 @@ public class UdfpsController implements DozeReceiver, Dumpable {
 
         mOverlay = overlay;
         final int requestReason = overlay.getRequestReason();
+
+        if (mUdfpsAnimation != null) {
+            mUdfpsAnimation.setIsKeyguard(requestReason ==
+                    BiometricOverlayConstants.REASON_AUTH_KEYGUARD);
+        }
+
         if (requestReason == REASON_AUTH_KEYGUARD
                 && !mKeyguardUpdateMonitor.isFingerprintDetectionRunning()) {
             Log.d(TAG, "Attempting to showUdfpsOverlay when fingerprint detection"
@@ -1403,6 +1423,9 @@ public class UdfpsController implements DozeReceiver, Dumpable {
         for (Callback cb : mCallbacks) {
             cb.onFingerDown();
         }
+        if (mUdfpsAnimation != null) {
+            mUdfpsAnimation.show();
+        }
     }
 
     private void onFingerUp(long requestId, @NonNull UdfpsView view) {
@@ -1456,6 +1479,9 @@ public class UdfpsController implements DozeReceiver, Dumpable {
             for (Callback cb : mCallbacks) {
                 cb.onFingerUp();
             }
+        }
+        if (mUdfpsAnimation != null) {
+            mUdfpsAnimation.hide();
         }
         mOnFingerDown = false;
         if (isOptical()) {

CherishOS/android_frameworks_base

Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>

packages/SystemUI/src/com/android/systemui/biometrics/UdfpsAnimation.java
@@ -1,0 +1,192 @@
+/**
+ * Copyright (C) 2019 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.systemui.biometrics;
+
+import static com.android.systemui.doze.util.BurnInHelperKt.getBurnInOffset;
+import static com.android.systemui.doze.util.BurnInHelperKt.getBurnInProgressOffset;
+
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.content.res.Resources;
+import android.database.ContentObserver;
+import android.graphics.PixelFormat;
+import android.graphics.drawable.AnimationDrawable;
+import android.hardware.fingerprint.FingerprintSensorPropertiesInternal;
+import android.net.Uri;
+import android.os.UserHandle;
+import android.provider.Settings;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.util.MathUtils;
+import android.view.Gravity;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.ImageView;
+import android.graphics.Rect;
+
+import com.android.systemui.Dependency;
+import com.android.systemui.R;
+import com.android.systemui.plugins.statusbar.StatusBarStateController;
+
+public class UdfpsAnimation extends ImageView {
+
+    private static final boolean DEBUG = true;
+    private static final String LOG_TAG = "UdfpsAnimations";
+
+    private boolean mShowing = false;
+    private Context mContext;
+    private int mAnimationSize;
+    private int mAnimationOffset;
+    private AnimationDrawable recognizingAnim;
+
+    private final WindowManager.LayoutParams mAnimParams = new WindowManager.LayoutParams();
+    private WindowManager mWindowManager;
+
+    private boolean mIsKeyguard;
+    private boolean mEnabled;
+
+    private final int mMaxBurnInOffsetX;
+    private final int mMaxBurnInOffsetY;
+
+    private int mSelectedAnim;
+    private String[] mStyleNames;
+
+    private static final String UDFPS_ANIMATIONS_PACKAGE = "com.cherish.udfps.animations";
+
+    private Resources mApkResources;
+
+    public UdfpsAnimation(Context context, WindowManager windowManager,
+           FingerprintSensorPropertiesInternal props) {
+        super(context);
+        mContext = context;
+
+        mWindowManager = windowManager;
+
+        mMaxBurnInOffsetX = context.getResources()
+            .getDimensionPixelSize(R.dimen.udfps_burn_in_offset_x);
+        mMaxBurnInOffsetY = context.getResources()
+            .getDimensionPixelSize(R.dimen.udfps_burn_in_offset_y);
+
+        mAnimationSize = mContext.getResources().getDimensionPixelSize(R.dimen.udfps_animation_size);
+        mAnimationOffset = mContext.getResources().getDimensionPixelSize(R.dimen.udfps_animation_offset);
+
+        mAnimParams.height = mAnimationSize;
+        mAnimParams.width = mAnimationSize;
+
+        mAnimParams.format = PixelFormat.TRANSLUCENT;
+        mAnimParams.type = WindowManager.LayoutParams.TYPE_VOLUME_OVERLAY; // it must be behind Udfps icon
+        mAnimParams.flags =  WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE
+                | WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE
+                | WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS;
+        mAnimParams.gravity = Gravity.TOP | Gravity.CENTER;
+        mAnimParams.y = props.getLocation().sensorLocationY - props.getLocation().sensorRadius
+                - (mAnimationSize / 2) + mAnimationOffset;
+
+        try {
+            PackageManager pm = mContext.getPackageManager();
+            mApkResources = pm.getResourcesForApplication(UDFPS_ANIMATIONS_PACKAGE);
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
+        int res = mApkResources.getIdentifier("udfps_animation_styles",
+                "array", UDFPS_ANIMATIONS_PACKAGE);
+        mStyleNames = mApkResources.getStringArray(res);
+
+        setScaleType(ImageView.ScaleType.CENTER_INSIDE);
+
+        Uri udfpsAnim = Settings.System.getUriFor(Settings.System.UDFPS_ANIM);
+        Uri udfpsAnimStyle = Settings.System.getUriFor(Settings.System.UDFPS_ANIM_STYLE);
+        ContentObserver contentObserver = new ContentObserver(null) {
+            @Override
+            public void onChange(boolean selfChange, Uri uri) {
+                if (udfpsAnim.equals(uri)) {
+                    mEnabled = Settings.System.getIntForUser(mContext.getContentResolver(),
+                            Settings.System.UDFPS_ANIM, 0, UserHandle.USER_CURRENT) == 1;
+                } else if (udfpsAnimStyle.equals(uri)) {
+                    mSelectedAnim = Settings.System.getIntForUser(mContext.getContentResolver(),
+                            Settings.System.UDFPS_ANIM_STYLE, 0, UserHandle.USER_CURRENT);
+                    mContext.getMainExecutor().execute(() -> {
+                        updateAnimationStyle(mStyleNames[mSelectedAnim]);
+                    });
+                }
+            }
+        };
+        mContext.getContentResolver().registerContentObserver(
+                udfpsAnim, false, contentObserver, UserHandle.USER_CURRENT);
+        mContext.getContentResolver().registerContentObserver(
+                udfpsAnimStyle, false, contentObserver, UserHandle.USER_CURRENT);
+        contentObserver.onChange(true, udfpsAnim);
+        contentObserver.onChange(true, udfpsAnimStyle);
+    }
+
+    private void updateAnimationStyle(String drawableName) {
+        if (DEBUG) Log.i(LOG_TAG, "Updating animation style to:" + drawableName);
+        int resId = mApkResources.getIdentifier(drawableName, "drawable", UDFPS_ANIMATIONS_PACKAGE);
+        if (DEBUG) Log.i(LOG_TAG, "Got resource id: "+ resId +" from package" );
+        setBackgroundDrawable(mApkResources.getDrawable(resId));
+        recognizingAnim = (AnimationDrawable) getBackground();
+    }
+
+    public void show() {
+        if (!mShowing && mIsKeyguard && mEnabled) {
+            mShowing = true;
+            try {
+                if (getWindowToken() == null) {
+                    mWindowManager.addView(this, mAnimParams);
+                } else {
+                    mWindowManager.updateViewLayout(this, mAnimParams);
+                }
+            } catch (RuntimeException e) {
+                // Ignore
+            }
+            if (recognizingAnim != null) {
+                recognizingAnim.start();
+            }
+        }
+    }
+
+    public void hide() {
+        if (mShowing) {
+            mShowing = false;
+            if (recognizingAnim != null) {
+                clearAnimation();
+                recognizingAnim.stop();
+                recognizingAnim.selectDrawable(0);
+            }
+            if (getWindowToken() != null) {
+                mWindowManager.removeView(this);
+            }
+        }
+    }
+
+    public void setIsKeyguard(boolean isKeyguard) {
+        mIsKeyguard = isKeyguard;
+    }
+
+    public void dozeTimeTick() {
+        float amt = Dependency.get(StatusBarStateController.class).getDozeAmount();
+
+        float mBurnInOffsetX = MathUtils.lerp(0f,
+                getBurnInOffset(mMaxBurnInOffsetX * 2, true /* xAxis */)
+                - mMaxBurnInOffsetX, amt);
+        float mBurnInOffsetY = MathUtils.lerp(0f,
+                getBurnInOffset(mMaxBurnInOffsetY * 2, false /* xAxis */)
+                - mMaxBurnInOffsetY, amt);
+        setTranslationX(mBurnInOffsetX);
+        setTranslationY(mBurnInOffsetY);
+    }
+}

packages/SystemUI/src/com/android/systemui/biometrics/UdfpsController.java
@@ -36,6 +36,7 @@ import android.content.IntentFilter;
 import android.database.ContentObserver;
 import android.graphics.Rect;
 import android.hardware.biometrics.BiometricFingerprintConstants;
+import android.hardware.biometrics.BiometricRequestConstants;
 import android.hardware.biometrics.SensorProperties;
 import android.hardware.display.AmbientDisplayConfiguration;
 import android.hardware.display.ColorDisplayManager;
@@ -70,6 +71,7 @@ import androidx.annotation.OptIn;
 import com.android.internal.annotations.VisibleForTesting;
 import com.android.internal.logging.InstanceId;
 import com.android.internal.util.LatencyTracker;
+import com.android.internal.util.cherish.CherishUtils;
 import com.android.internal.util.cherish.udfps.UdfpsUtils;
 import com.android.keyguard.KeyguardUpdateMonitor;
 import com.android.systemui.Dumpable;
@@ -237,6 +239,8 @@ public class UdfpsController implements DozeReceiver, Dumpable {
 
     private boolean mFrameworkDimming;
     private int[][] mBrightnessAlphaArray;
+
+    private UdfpsAnimation mUdfpsAnimation;
 
     @VisibleForTesting
     public static final VibrationAttributes UDFPS_VIBRATION_ATTRIBUTES =
@@ -741,13 +745,7 @@ public class UdfpsController implements DozeReceiver, Dumpable {
         mUnlockedScreenOffAnimationController = unlockedScreenOffAnimationController;
         mLatencyTracker = latencyTracker;
         mActivityLaunchAnimator = activityLaunchAnimator;
-        mSensorProps = new FingerprintSensorPropertiesInternal(
-                -1 /* sensorId */,
-                SensorProperties.STRENGTH_CONVENIENCE,
-                0 /* maxEnrollmentsPerUser */,
-                new ArrayList<>() /* componentInfo */,
-                FingerprintSensorProperties.TYPE_UNKNOWN,
-                false /* resetLockoutRequiresHardwareAuthToken */);
+        mSensorProps = findFirstUdfps();
 
         mBiometricExecutor = biometricsExecutor;
         mPrimaryBouncerInteractor = primaryBouncerInteractor;
@@ -812,6 +810,11 @@ public class UdfpsController implements DozeReceiver, Dumpable {
                 }
             );
         }
+
+        if (CherishUtils.isPackageInstalled(mContext,
+                "com.cherish.udfps.animations")) {
+            mUdfpsAnimation = new UdfpsAnimation(mContext, mWindowManager, mSensorProps);
+        }
     }
 
     private void disableNightMode() {
@@ -849,6 +852,17 @@ public class UdfpsController implements DozeReceiver, Dumpable {
         }
     }
 
+    @Nullable
+    private FingerprintSensorPropertiesInternal findFirstUdfps() {
+        for (FingerprintSensorPropertiesInternal props :
+                mFingerprintManager.getSensorPropertiesInternal()) {
+            if (props.isAnyUdfpsType()) {
+                return props;
+            }
+        }
+        return null;
+    }
+
     @Override
     public void dozeTimeTick() {
         if (mOverlay != null && mOverlay.getTouchOverlay() instanceof UdfpsView) {
@@ -875,6 +889,12 @@ public class UdfpsController implements DozeReceiver, Dumpable {
 
         mOverlay = overlay;
         final int requestReason = overlay.getRequestReason();
+
+        if (mUdfpsAnimation != null) {
+            mUdfpsAnimation.setIsKeyguard(requestReason ==
+                    BiometricRequestConstants.REASON_AUTH_KEYGUARD);
+        }
+
         if (requestReason == REASON_AUTH_KEYGUARD
                 && !mKeyguardUpdateMonitor.isFingerprintDetectionRunning()) {
             Log.d(TAG, "Attempting to showUdfpsOverlay when fingerprint detection"
@@ -1192,6 +1212,9 @@ public class UdfpsController implements DozeReceiver, Dumpable {
         for (Callback cb : mCallbacks) {
             cb.onFingerDown();
         }
+        if (mUdfpsAnimation != null) {
+            mUdfpsAnimation.show();
+        }
     }
 
     private void onFingerUp(long requestId, @NonNull View view) {
@@ -1230,6 +1253,9 @@ public class UdfpsController implements DozeReceiver, Dumpable {
             for (Callback cb : mCallbacks) {
                 cb.onFingerUp();
             }
+        }
+        if (mUdfpsAnimation != null) {
+            mUdfpsAnimation.hide();
         }
         mOnFingerDown = false;
         unconfigureDisplay(view);