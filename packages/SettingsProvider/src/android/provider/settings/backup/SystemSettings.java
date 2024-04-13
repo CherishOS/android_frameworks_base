8590fabf2f6fb6e1284fe7f6de5e8592001895f2
Hưng Phan <phandinhhungvp2001@gmail.com>
2024-04-13 20:35:11 +0700

    Revert "Show Ambient instead of lockscreen on wake gestures [1/2]"
    Revert "Disable Ambient on wake gesture when incall"
    
    This reverts commit fb53d736007cf7e93e2a0d11bd402c37cebea580.
    
    Revert "Show Ambient instead of lockscreen on wake gestures [1/2]"
    
    This reverts commit 55cab5e2b2339c58235750de8efb65e32b3c7e13.
    
    Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>

--- packages/SettingsProvider/src/android/provider/settings/backup/SystemSettings.java
+++ packages/SettingsProvider/src/android/provider/settings/backup/SystemSettings.java
@@ -107,7 +107,6 @@ public class SystemSettings {
         Settings.System.NOTIFICATION_COOLDOWN_ENABLED,
         Settings.System.NOTIFICATION_COOLDOWN_ALL,
         Settings.System.NOTIFICATION_COOLDOWN_VIBRATE_UNLOCKED,
-        Settings.System.AMBIENT_WAKE_GESTURES,
         Settings.System.QS_PANEL_STYLE,
         Settings.System.NAVIGATION_BAR_IME_SPACE,
     };
