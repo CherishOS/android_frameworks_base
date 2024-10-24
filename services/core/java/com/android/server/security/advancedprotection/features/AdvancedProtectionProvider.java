18e4bda6eb8bbb8c4eab75d9b662a1b82547016d
Hani Kazmi <hanikazmi@google.com>
2024-10-24 16:03:20 +0000

    [AAPM] Add Feature class
    The settings screen for advanced protection will list the protection
    features available on the device during onboarding. This list is dynamic,
    and provided by the service. The list can be added to be Hooks, which
    are features living directly in the advanced protection package, and by
    Providers, where the feature lives elsewhere and returns the available
    protections.
    
    Bug: 352420507
    Test: atest AdvancedProtectionServiceTest AdvancedProtectionManagerTest
    Flag: android.security.aapm_api
    Change-Id: Iaa3e8fd77a78582c7676b59c7c47d3dd7db50027

--- services/core/java/com/android/server/security/advancedprotection/features/AdvancedProtectionProvider.java
+++ services/core/java/com/android/server/security/advancedprotection/features/AdvancedProtectionProvider.java
@@ -16,20 +16,12 @@
 
 package com.android.server.security.advancedprotection.features;
 
-import android.annotation.NonNull;
-import android.content.Context;
 import android.security.advancedprotection.AdvancedProtectionFeature;
 
+import java.util.List;
+
 /** @hide */
-public abstract class AdvancedProtectionHook {
-    /** Called on boot phase PHASE_SYSTEM_SERVICES_READY */
-    public AdvancedProtectionHook(@NonNull Context context, boolean enabled) {}
-    /** The feature this hook provides */
-    @NonNull
-    public abstract AdvancedProtectionFeature getFeature();
-    /** Whether this feature is relevant on this device. If false, onAdvancedProtectionChanged will
-     * not be called, and the feature will not be displayed in the onboarding UX. */
-    public abstract boolean isAvailable();
-    /** Called whenever advanced protection state changes */
-    public void onAdvancedProtectionChanged(boolean enabled) {}
+public abstract class AdvancedProtectionProvider {
+    /** The list of features provided */
+    public abstract List<AdvancedProtectionFeature> getFeatures();
 }
